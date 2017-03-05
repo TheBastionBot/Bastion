2ba8f9d5613627802a4cfa6db0ba7b127e1ee65a
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-03-05 13:33:06 +0000

    Added zalgolize command

---
+++ modules/queries/zalgolize.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+const zalgolize = require('zalgolize');
+
+exports.run = function(Bastion, message, args) {
+  if (args.length < 1) return;
+
+  message.channel.sendMessage('', {embed: {
+    color: 6651610,
+    title: 'Zalgolized Text:',
+    description: zalgo(args.join(' '))
+  }}).catch(e => {
+    Bastion.log.error(e.stack);
+  });
+};
+
+exports.conf = {
+  aliases: ['zalgo']
+};
+
+exports.help = {
+  name: 'zalgolize',
+  description: 'Zalgolizes a given text.',
+  permission: '',
+  usage: ['zaloglize It looks clumsy, but it\'s cool!']
+};
