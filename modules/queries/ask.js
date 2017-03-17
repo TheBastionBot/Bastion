9f80ac07258943ee691ead7fe77f260c78998940
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-03-17 16:14:09 +0000

    Fixed ask command

--- modules/queries/ask.js
+++ modules/queries/ask.js
@@ -31,28 +31,35 @@ exports.run = function(Bastion, message, args) {
     title: args.join(' '),
     description: 'React to this message with :one: to vote **YES**\n\nReact to this message with :zero: to vote **NO**'
   }}).then(m => {
-    for (var i = 0; i < opt.length; i++) m.react(opt[i])
-    setTimeout(function () {
-      let res = [];
-      for (var i = 0; i < opt.length; i++)
-        res.push(m.reactions.get(opt[i]).users.size - 1);
-      message.channel.sendMessage('', {embed: {
-        color: 5088314,
-        title: args.join(' '),
-        fields: [
-          {
-            name: 'Yes',
-            value: res[1],
-            inline: true
-          },
-          {
-            name: 'No',
-            value: res[0],
-            inline: true
-          }
-        ]
-      }});
-    }, 60000);
+    try {
+      for (var i = 0; i < opt.length; i++) m.react(opt[i]).catch(e => {
+        Bastion.log.error(e.stack);
+      });
+    } finally {
+      setTimeout(function () {
+        let res = [];
+        for (var i = 0; i < opt.length; i++)
+          res.push(m.reactions.get(opt[i]).users.size - 1);
+        message.channel.sendMessage('', {embed: {
+          color: 5088314,
+          title: args.join(' '),
+          fields: [
+            {
+              name: 'Yes',
+              value: res[1],
+              inline: true
+            },
+            {
+              name: 'No',
+              value: res[0],
+              inline: true
+            }
+          ]
+        }}).catch(e => {
+          Bastion.log.error(e.stack);
+        });
+      }, 60000);
+    }
   }).catch(e => {
     Bastion.log.error(e.stack);
   });
