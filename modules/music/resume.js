30624036433dfb57994f00062a71cdfcc923c9e5
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-03-27 19:06:33 +0100

    Fixed some music commands

--- modules/music/resume.js
+++ modules/music/resume.js
@@ -2,7 +2,6 @@ exports.run = (Bastion, message, args) => {
   message.delete().catch(e => {
     Bastion.log.error(e.stack);
   });
-  if (Bastion.credentials.ownerId.indexOf(msg.author.id) < 0 && !voiceChannel.permissionsFor(msg.author).hasPermission('MUTE_MEMBERS')) return Bastion.log.info('You don\'t have permissions to use this command.');
 };
 
 exports.config = {
