0cca6e7afeb1ae85a8803754eb237f14321e4155
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-03-30 13:40:03 +0100

    Added setNick command

---
+++ modules/owner/setNick.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+exports.run = (Bastion, message, args) => {
+  if (Bastion.credentials.ownerId.indexOf(message.author.id) < 0) return Bastion.log.info('You don\'t have permissions to use this command.');
+
+  if (args.length < 1) return;
+  message.guild.members.get(Bastion.user.id).setNickname(args.join(' ')).then(() => {
+    message.channel.sendMessage('', {embed: {
+      color: 14211540,
+      description: `${Bastion.user.username}'s nick is now set to **${args.join(' ')}**`
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }).catch(e => {
+    Bastion.log.error(e.stack);
+  });
+};
+
+exports.config = {
+  aliases: ['setn']
+};
+
+exports.help = {
+  name: 'setnick',
+  description: 'Sets the nick of the bot in the current guild.',
+  permission: '',
+  usage: 'setNick <text>',
+  example: ['setNick NewNick']
+};
