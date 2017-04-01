f1695edfe4cd8ed6f18dd84652519b4d510fa308
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-04-01 17:16:36 +0100

    Paginated listTriggers command

--- modules/owner/listTriggers.js
+++ modules/owner/listTriggers.js
@@ -26,10 +26,25 @@ exports.run = (Bastion, message, args) => {
   if (Bastion.credentials.ownerId.indexOf(message.author.id) < 0) return Bastion.log.info('You don\'t have permissions to use this command.');
 
   sql.all('SELECT trigger FROM triggers').then(triggers => {
+    if (triggers == '') return message.channel.sendMessage('', {embed: {
+      color: 13380644,
+      description: 'You don\'t have any triggers.',
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+
+    triggers = triggers.map((t, i) => `${i+1}. ${t.trigger}`);
+    let i = 0;
+    if (isNaN(args = parseInt(args[0]))) i = 1;
+    else i = args < triggers.length/10+1 ? args : 1;
+    i = i - 1;
     message.channel.sendMessage('', {embed: {
-      color: 5088314,
+      color: 6651610,
       title: 'List of triggers',
-      description: triggers.map(t => t.trigger).join('\n\n')
+      description: triggers.slice(i*10, (i*10)+10).join('\n'),
+      footer: {
+        text: `Page: ${i+1} of ${parseInt(triggers.length/10+1)}`
+      }
     }}).catch(e => {
       Bastion.log.error(e.stack);
     });
@@ -44,8 +59,8 @@ exports.config = {
 
 exports.help = {
   name: 'listtriggers',
-  description: 'Lists all the triggers you have added.',
+  description: 'Lists all the triggers you have added. It takes page number as an optional argument.',
   permission: '',
-  usage: 'listTriggers',
-  example: []
+  usage: 'listTriggers [page_no]',
+  example: ['listTriggers', 'listTriggers 2']
 };
