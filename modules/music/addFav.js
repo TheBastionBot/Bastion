9cbfa1c89e24efb59dc12d41cc489f564de5936c
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-04-01 13:26:01 +0100

    Added addFav command

---
+++ modules/music/addFav.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+const jsonDB = require('node-json-db');
+const db = new jsonDB('./data/favouriteSongs', true, true);
+
+exports.run = (Bastion, message, args) => {
+  if (Bastion.credentials.ownerId.indexOf(message.author.id) < 0) return Bastion.log.info('You don\'t have permissions to use this command.');
+  if (args.length < 1) return;
+
+  args = args.join(' ');
+  try {
+    db.reload();
+    db.push('[]', [args], false);
+  } catch (e) {
+    Bastion.log.error(e.stack);
+  } finally {
+    message.channel.sendMessage('', {embed: {
+      color: 5088314,
+      title: 'Added song to favourites',
+      description: args
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+};
+
+exports.config = {
+  aliases: []
+};
+
+exports.help = {
+  name: 'addfav',
+  description: 'Adds a song to your favourite list specified by name/link.',
+  permission: '',
+  usage: 'addfav <song name | song link>',
+  example: []
+};
