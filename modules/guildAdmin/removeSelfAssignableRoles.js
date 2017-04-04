942f00122fb90c9b9c4a4142eaf5201f0a5ca92f
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-04-04 13:27:14 +0100

    Added removeSelfAssignableRoles command

---
+++ modules/guildAdmin/removeSelfAssignableRoles.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+const sql = require('sqlite');
+sql.open('./data/Bastion.sqlite');
+
+exports.run = (Bastion, message, args) => {
+  if (Bastion.credentials.ownerId.indexOf(message.author.id) < 0) return Bastion.log.info('You don\'t have permissions to use this command.');
+  if (!(index = parseInt(args[0])) || index <= 0) return;
+  index -= 1;
+
+  sql.get(`SELECT selfAssignableRoles FROM guildSettings WHERE guildID=${message.guild.id}`).then(row => {
+    if (!row)
+      message.channel.sendMessage('', {embed: {
+        color: 13380644,
+        description: 'No self assignable roles found.'
+      }}).catch(e => {
+        Bastion.log.error(e.stack);
+      });
+    else {
+      let roles = JSON.parse(row.selfAssignableRoles);
+      if (index >= roles.length) return;
+      let deletedRoleID = roles[parseInt(args[0]) - 1];
+      roles.splice(parseInt(args[0]) - 1, 1);
+      sql.run(`UPDATE guildSettings SET selfAssignableRoles='${JSON.stringify(roles)}' WHERE guildID=${message.guild.id}`).then(() => {
+        message.channel.sendMessage('', {embed: {
+          color: 13380644,
+          description: `I've deleted **${message.guild.roles.get(deletedRoleID).name}** from self assignable roles.`
+        }}).catch(e => {
+          Bastion.log.error(e.stack);
+        });
+      }).catch(e => {
+        Bastion.log.error(e.stack);
+      });
+    }
+  }).catch(() => {
+    Bastion.log.error(e.stack);
+  });
+};
+
+exports.config = {
+  aliases: ['rsar']
+};
+
+exports.help = {
+  name: 'removeselfassignableroles',
+  description: 'Deletes an role from the self assignable roles by it\'s index number.',
+  permission: '',
+  usage: 'removeSelfAssignableRoles <index>',
+  example: ['removeSelfAssignableRoles 3']
+};
