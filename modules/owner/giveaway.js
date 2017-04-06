2ad805d5f239da151184d450fb3396bdbe1400c3
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-04-06 03:34:33 +0100

    Only one giveaway at a time, per channel

--- modules/owner/giveaway.js
+++ modules/owner/giveaway.js
@@ -22,6 +22,7 @@
 const sql = require('sqlite');
 const getRandomInt = require('../../functions/getRandomInt');
 sql.open('./data/Bastion.sqlite');
+let activeChannels = [];
 let winners = [];
 
 exports.run = (Bastion, message, args) => {
@@ -29,50 +30,63 @@ exports.run = (Bastion, message, args) => {
   if (args.length < 1) return;
   if (isNaN(args = parseInt(args[0])) || args < 0) return;
 
-  let reactions = ['🎈', '🎊', '🎉', '🎃', '🎁', '🎁'];
-  let reaction = reactions[getRandomInt(0, reactions.length-1)]
-  message.channel.sendMessage('', {embed: {
-    color: 6651610,
-    title: 'GIVEAWAY! 🎉',
-    description: `Giveaway event started. React to this message with ${reaction} to get **${args}** Bastion Currencies.`,
-    footer: {
-      text: 'Event stops in 1 hour. You will get your reward after the event has concluded.'
-    }
-  }}).then(msg => {
-    setTimeout(function () {
-      msg.edit('', {embed: {
-        color: 6651610,
-        title: 'Giveaway event ended',
-        description: `Giveaway event has been ended. Thank you for participating. All the participants are being rewarded with **${args}** Bastion Currencies.`
-      }}).catch(e => {
-        Bastion.log.error(e.stack);
-      });
-      if (msg.reactions.get(reaction)) winners = msg.reactions.get(reaction).users.map(u => u.id);
-      winners.forEach((user, i) => {
-        sql.get(`SELECT bastionCurrencies FROM profiles WHERE userID=${user}`).then(receiver => {
-          if (!receiver)
-            sql.run('INSERT INTO profiles (userID, bastionCurrencies) VALUES (?, ?)', [user, args]).catch(e => {
-              Bastion.log.error(e.stack);
-            });
-          else
-            sql.run(`UPDATE profiles SET bastionCurrencies=${parseInt(receiver.bastionCurrencies)+args} WHERE userID=${user}`).catch(e => {
-              Bastion.log.error(e.stack);
-            });
-        }).then(() => {
-          Bastion.users.get(user).sendMessage('', {embed: {
-            color: 5088314,
-            description: `You have been awarded **${args}** Bastion Currencies for your participation in the giveaway event.`
-          }}).catch(e => {
-            Bastion.log.error(e.stack);
-          });
+  if(!activeChannels.includes(message.channel.id)) {
+    let reactions = ['🎈', '🎊', '🎉', '🎃', '🎁', '🎁'];
+    let reaction = reactions[getRandomInt(0, reactions.length-1)]
+    message.channel.sendMessage('', {embed: {
+      color: 6651610,
+      title: 'GIVEAWAY! 🎉',
+      description: `Giveaway event started. React to this message with ${reaction} to get **${args}** Bastion Currencies.`,
+      footer: {
+        text: 'Event stops in 1 hour. You will get your reward after the event has concluded.'
+      }
+    }}).then(msg => {
+      activeChannels.push(message.channel.id);
+      setTimeout(function () {
+        msg.edit('', {embed: {
+          color: 6651610,
+          title: 'Giveaway event ended',
+          description: `Giveaway event has been ended. Thank you for participating. All the participants are being rewarded with **${args}** Bastion Currencies.`
+        }}).then(() => {
+          activeChannels.splice(activeChannels.indexOf(message.channel.id), 1);
         }).catch(e => {
           Bastion.log.error(e.stack);
         });
-      });
-    }, 60 * 60 * 1000);
-  }).catch(e => {
-    Bastion.log.error(e.stack);
-  });
+        if (msg.reactions.get(reaction)) winners = msg.reactions.get(reaction).users.map(u => u.id);
+        winners.forEach((user, i) => {
+          sql.get(`SELECT bastionCurrencies FROM profiles WHERE userID=${user}`).then(receiver => {
+            if (!receiver)
+              sql.run('INSERT INTO profiles (userID, bastionCurrencies) VALUES (?, ?)', [user, args]).catch(e => {
+                Bastion.log.error(e.stack);
+              });
+            else
+              sql.run(`UPDATE profiles SET bastionCurrencies=${parseInt(receiver.bastionCurrencies)+args} WHERE userID=${user}`).catch(e => {
+                Bastion.log.error(e.stack);
+              });
+          }).then(() => {
+            Bastion.users.get(user).sendMessage('', {embed: {
+              color: 5088314,
+              description: `You have been awarded **${args}** Bastion Currencies for your participation in the giveaway event.`
+            }}).catch(e => {
+              Bastion.log.error(e.stack);
+            });
+          }).catch(e => {
+            Bastion.log.error(e.stack);
+          });
+        });
+      }, 60 * 60 * 1000);
+    }).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+  else {
+    message.channel.sendMessage('', {embed: {
+      color: 13380644,
+      description: 'Can\'t start another giveaway event now. Another giveaway event is already active in this channel. Wait a for it to end.'
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
 };
 
 exports.config = {
