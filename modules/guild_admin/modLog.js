2450aa5b9143765b7eee1948b5a84b9310fed787
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-04-19 16:59:36 +0100

    Added modLog command

---
+++ modules/guild_admin/modLog.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+// This feature is absolutely useless after Discord releases audit logs feature to the public. I'll probably remove logging module after that.
+const sql = require('sqlite');
+sql.open('./data/Bastion.sqlite');
+
+exports.run = (Bastion, message, args) => {
+  if (!message.member.hasPermission("ADMINISTRATOR")) return Bastion.log.info('You don\'t have permissions to use this command.');
+
+  sql.get(`SELECT modLog, modLogChannelID FROM guildSettings WHERE guildID=${message.guild.id}`).then(row => {
+    if (row.modLog == 'false') {
+      sql.run(`UPDATE guildSettings SET modLog='true', modLogChannelID=${message.channel.id} WHERE guildID=${message.guild.id}`).catch(e => {
+        Bastion.log.error(e.stack);
+      });
+      color = Bastion.colors.green;
+      modLogStats = 'Moderation audit logging is now enabled in this channel.';
+    }
+    else {
+      sql.run(`UPDATE guildSettings SET modLog='false', modLogChannelID=null WHERE guildID=${message.guild.id}`).catch(e => {
+        Bastion.log.error(e.stack);
+      });
+      color = Bastion.colors.red;
+      modLogStats = 'Moderation audit logging is now disabled.';
+    }
+    message.channel.sendMessage('', {embed: {
+      color: color,
+      description: modLogStats
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }).catch(e => {
+    Bastion.log.error(e.stack);
+  });
+};
+
+exports.config = {
+  aliases: []
+};
+
+exports.help = {
+  name: 'modlog',
+  description: 'Toggle logging of various moderation events in the server.',
+  permission: '',
+  usage: 'modLog',
+  example: []
+};
