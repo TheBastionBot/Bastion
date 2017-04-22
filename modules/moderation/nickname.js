4d6ee1cf783968e72681d9c9a24e4e37eb9dd8c0
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-04-22 13:45:39 +0100

    Added nickname command

---
+++ modules/moderation/nickname.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+exports.run = (Bastion, message, args) => {
+  if (!message.member.hasPermission("MANAGE_NICKNAMES")) return Bastion.log.info('You don\'t have permissions to use this command.');
+  if (!(user = message.mentions.users.first())) {
+    return message.channel.sendMessage('', {embed: {
+      color: Bastion.colors.yellow,
+      title: 'Usage',
+      description: `\`${Bastion.config.prefix}${this.help.usage}\``
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+
+  args = args.slice(1);
+  let color;
+  let nickStat = '';
+  if (args.length < 1) {
+    color = Bastion.colors.red;
+    nickStat = `${user}'s nickname removed.`;
+  }
+  else {
+    color = Bastion.colors.green;
+    nickStat = `${user}'s nickname changed.`;
+  }
+  message.guild.members.get(user.id).setNickname(args.join(' ')).then(member => {
+    message.channel.sendMessage('', {embed: {
+      color: color,
+      description: nickStat
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  });
+};
+
+exports.config = {
+  aliases: ['nick']
+};
+
+exports.help = {
+  name: 'nickname',
+  description: 'Change the nickname of the mentioned user in the server to a specified nick. If no nick is specified, it resets the user\'s nickname.',
+  permission: 'Manage Nicknames',
+  usage: 'nickname <@user-mention> [nick]',
+  example: ['nickname @user#0001 The Legend', 'nickname @user#0001']
+};
