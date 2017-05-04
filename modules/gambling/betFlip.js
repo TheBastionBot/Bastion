d509cbc34714d240b82c63815a74b93d73cd9d3e
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-04 13:52:04 +0530

    Reduced the reward in betFlip command

--- modules/gambling/betFlip.js
+++ modules/gambling/betFlip.js
@@ -60,7 +60,7 @@ exports.run = (Bastion, message, args) => {
       recentUsers.push(message.author.id);
 
       if (outcome.toLowerCase() === args[1].toLowerCase()) {
-        prize = args[0] < 50 ? parseInt(args[0]) + outcomes.length : args[0] < 100 ? parseInt(args[0]) : parseInt(args[0]) * 2;
+        prize = args[0] < 50 ? parseInt(args[0]) + outcomes.length : args[0] < 100 ? parseInt(args[0]) : parseInt(args[0]) * 1.5;
         result = `Congratulations! You won the bet.\nYou won **${prize}** Bastion Currencies.`;
         sql.run(`UPDATE profiles SET bastionCurrencies=${parseInt(profile.bastionCurrencies) + parseInt(prize)} WHERE userID=${message.author.id}`).catch(e => {
           Bastion.log.error(e.stack);
