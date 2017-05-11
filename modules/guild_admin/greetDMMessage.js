5718904e9eeb3f489de93b05ac7fa5e6cfd0987d
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-11 13:36:36 +0530

    Added greetDMMessage command

---
+++ modules/guild_admin/greetDMMessage.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+const sql = require('sqlite');
+sql.open('./data/Bastion.sqlite');
+
+exports.run = (Bastion, message, args) => {
+  if (!message.member.hasPermission('ADMINISTRATOR')) return Bastion.log.info('User doesn\'t have permission to use this command.');
+  if (args.length < 1) {
+    sql.get(`SELECT greetDMMessage FROM guildSettings WHERE guildID=${message.guild.id}`).then(guild => {
+      message.channel.send({embed: {
+        color: Bastion.colors.dark_grey,
+        title: 'DM Greeting message:',
+        description: guild.greetDMMessage
+      }}).catch(e => {
+        Bastion.log.error(e.stack);
+      });
+    }).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+  else {
+    sql.run(`UPDATE guildSettings SET greetDMMessage="${args.join(' ').replace(/"/g, '\'')}" WHERE guildID=${message.guild.id}`).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+
+    message.channel.send({embed: {
+      color: Bastion.colors.green,
+      title: 'DM Greeting message set to:',
+      description: args.join(' ')
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+};
+
+exports.config = {
+  aliases: ['gmsg'],
+  enabled: true
+};
+
+exports.help = {
+  name: 'greetdmmessage',
+  description: 'Edit the greeting message that is sent as direct message when a new member is joined in the server.',
+  botPermission: '',
+  userPermission: 'Administrator',
+  usage: 'greetDMMessage [Message]',
+  example: ['greetDMMessage Hello $user! Welcome to $server.']
+};
