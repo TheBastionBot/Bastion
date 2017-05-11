cb0475ef6507fb2098eb6c4d0e8009ae6e79362d
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-12 02:16:11 +0530

    Added sendEmbed command

---
+++ modules/fun/sendEmbed.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+exports.run = (Bastion, message, args) => {
+  if (args.length < 1) {
+    return message.channel.send({embed: {
+      color: Bastion.colors.yellow,
+      title: 'Usage',
+      description: `\`${Bastion.config.prefix}${this.help.usage}\``
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+
+  try {
+    args = JSON.parse(args.join(' '));
+  } catch (e) {
+    return message.channel.send({embed: {
+      color: Bastion.colors.red,
+      description: 'Invalid embed object. Please check that it\'s an valid embed object or create one [here](https://bastion.js.org/tools/embed_builder/).'
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+
+  message.channel.send({embed: args}).catch(e => {
+    Bastion.log.error(e.stack);
+  });
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true
+};
+
+exports.help = {
+  name: 'sendembed',
+  description: 'Sends an embed message created from the specified embed JavaScript object. To create an embed object, graphically, [click here](https://bastion.js.org/tools/embed_builder/).',
+  botPermission: '',
+  userPermission: '',
+  usage: 'sendEmbed <embedObject>',
+  example: ['sendEmbed {title: "Hello", description: "Isn\'t it cool?"}']
+};
