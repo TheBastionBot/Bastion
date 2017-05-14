f06cb37d7f36cd4d81493924c33061c03cfde5b0
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-14 11:28:27 +0530

    betRoll command's cooldown increased to 60 seconds

--- modules/gambling/betRoll.js
+++ modules/gambling/betRoll.js
@@ -86,7 +86,7 @@ exports.run = (Bastion, message, args) => {
       }}).then(msg => {
         setTimeout(function () {
           recentUsers.splice(recentUsers.indexOf(message.author.id), 1);
-        }, 30 * 1000);
+        }, 60 * 1000);
       }).catch(e => {
         Bastion.log.error(e.stack);
       });
@@ -97,7 +97,7 @@ exports.run = (Bastion, message, args) => {
   else {
     message.channel.send({embed: {
       color: Bastion.colors.red,
-      description: `${message.author} you have gambled recently for this game, please wait at least 30 seconds before gambling again.`
+      description: `${message.author} you have gambled recently for this game, please wait at least 60 seconds before gambling again.`
     }}).catch(e => {
       Bastion.log.error(e.stack);
     });
