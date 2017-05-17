e3634197980a55a44ceb18634f32176f5cf0d359
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-17 19:25:21 +0530

    Improved callOfDuty4 command

--- modules/game_stats/cod4.js
+++ modules/game_stats/cod4.js
@@ -19,7 +19,7 @@
  * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
  */
 
-const cod4 = require('gamequery');
+const COD4 = require('gamequery');
 
 exports.run = (Bastion, message, args) => {
   if (args.length < 1 || !/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9]))?$/.test(args = args[0])) {
@@ -42,112 +42,112 @@ exports.run = (Bastion, message, args) => {
     port = 28960;
   }
 
-  cod4.query({
+  COD4.query({
     type: 'cod4',
     host: host,
     port: port
   }).then(data => {
-      let gametype = '';
-      if (data.raw.g_gametype === 'war') {
-        gametype = 'Team Deathmatch';
-      }
-      else if (data.raw.g_gametype === 'dm') {
-        gametype = 'Free for All';
-      }
-      else if (data.raw.g_gametype === 'sd') {
-        gametype = 'Search and Destroy';
-      }
-      else if (data.raw.g_gametype === 'dom') {
-        gametype = 'Domination';
-      }
-      else if (data.raw.g_gametype === 'koth') {
-        gametype = 'Headquarters';
-      }
-      else if (data.raw.g_gametype === 'sab') {
-        gametype = 'Sabotage';
-      }
-      else {
-        gametype = data.row.g_gametype;
-      }
+    let gametype = '';
+    if (data.raw.g_gametype === 'war') {
+      gametype = 'Team Deathmatch';
+    }
+    else if (data.raw.g_gametype === 'dm') {
+      gametype = 'Free for All';
+    }
+    else if (data.raw.g_gametype === 'sd') {
+      gametype = 'Search and Destroy';
+    }
+    else if (data.raw.g_gametype === 'dom') {
+      gametype = 'Domination';
+    }
+    else if (data.raw.g_gametype === 'koth') {
+      gametype = 'Headquarters';
+    }
+    else if (data.raw.g_gametype === 'sab') {
+      gametype = 'Sabotage';
+    }
+    else {
+      gametype = data.row.g_gametype;
+    }
 
-      let stats = [];
+    let stats = [];
+    stats.push(
+      {
+        name: 'Server IP',
+        value: `[${host}:${port}](cod4://${host}:${port})`,
+        inline: true
+      },
+      {
+        name: 'Players',
+        value: `${data.players.length}/${data.maxplayers}`,
+        inline: true
+      },
+      {
+        name: 'Private',
+        value: data.password,
+        inline: true
+      },
+      {
+        name: 'Map/Gametype',
+        value: `${data.map.replace('mp_', '').split('_').map(e => e.charAt(0).toUpperCase() + e.slice(1))} - ${gametype}`
+      }
+    );
+
+    if (data.players.length > 0) {
+      let players = [];
+      let scores = [];
+      for (i = 0; i < data.players.length; i++) {
+        players.push(data.players[i].name);
+      }
+      for (i = 0; i < data.players.length; i++) {
+        scores.push(data.players[i].frags);
+      }
       stats.push(
         {
-          name: 'Server IP',
-          value: `[${host}:${port}](cod4://${host}:${port})`,
+          name: 'Player',
+          value: players.join('\n'),
           inline: true
         },
         {
-          name: 'Players',
-          value: `${data.players.length}/${data.maxplayers}`,
+          name: 'Score',
+          value: scores.join('\n'),
           inline: true
-        },
-        {
-          name: 'Private',
-          value: data.password,
-          inline: true
-        },
-        {
-          name: 'Map/Gametype',
-          value: `${data.map.replace('mp_', '').split('_').map(e => e.charAt(0).toUpperCase() + e.slice(1))} - ${gametype}`
         }
       );
+    }
 
-      if (data.players.length > 0) {
-        let players = [];
-        let scores = [];
-        for (i = 0; i < stats.players.length; i++) {
-          players.push(stats.players[i].name);
-        }
-        for (i = 0; i < stats.players.length; i++) {
-          scores.push(stats.players[i].frags);
-        }
-        stats.push(
-          {
-            name: 'Player',
-            value: players.join('\n'),
-            inline: true
-          },
-          {
-            name: 'Score',
-            value: scores.join('\n'),
-            inline: true
-          }
-        );
+    message.channel.send({embed: {
+      color: Bastion.colors.blue,
+      title: data.name,
+      description: '[Call of Duty 4®: Modern Warfare®](https://store.steampowered.com/app/7940)',
+      fields: stats,
+      footer: {
+        text: `Server Uptime: ${data.raw.uptime}`
       }
-
-      message.channel.send({embed: {
-        color: Bastion.colors.blue,
-        title: data.name,
-        description: 'Call of Duty 4: Modern Warfare',
-        fields: stats,
-        footer: {
-          text: `Server Uptime: ${data.raw.uptime}`
-        }
-      }}).catch(e => {
-        Bastion.log.error(e.stack);
-      });
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
   }).catch(e => {
-      console.log(e);
-      message.channel.send({embed: {
-        color: Bastion.colors.red,
-        description: 'Can\'t get stats from the specified server. Please check the IP address and PORT number and if the server is online before trying again.',
-      }}).catch(e => {
-        Bastion.log.error(e.stack);
-      });
+    Bastion.log.error(e);
+    message.channel.send({embed: {
+      color: Bastion.colors.red,
+      description: 'Can\'t get stats from the specified server. Please check the IP address and PORT number and if the server is online before trying again.',
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
   });
 };
 
 exports.config = {
-  aliases: [],
+  aliases: ['cod4'],
   enabled: true
 };
 
 exports.help = {
-  name: 'cod4',
-  description: 'Get status of any Call of Duty 4: Modern Warfare game server by it\'s IP address and optional PORT number.',
+  name: 'callofduty4',
+  description: 'Get status of any Call of Duty 4®: Modern Warfare® game server by it\'s IP address and optional PORT number.',
   botPermission: '',
   userPermission: '',
-  usage: 'cod4 <COD_SERVER_IP>[:PORT]',
-  example: ['cod4 139.59.31.128', 'cod4 139.59.31.128:27016']
+  usage: 'callOfDuty4 <COD_SERVER_IP>[:PORT]',
+  example: ['callOfDuty4 139.59.31.128', 'callOfDuty4 139.59.31.128:27016']
 };
