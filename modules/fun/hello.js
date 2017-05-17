8ced2497429d1b7819c566b133824cabeea90958
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-18 01:16:22 +0530

    Added hello command

---
+++ modules/fun/hello.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+exports.run = (Bastion, message, args) => {
+  message.channel.send({embed: {
+    color: Bastion.colors.grey,
+    description: 'Hi! I\'m **Bastion**. \u{1F609}\n' +
+                 'I\'m a BOT that is going to make your time at this Discord Server amazing!',
+    footer: {
+      text: `Type ${Bastion.config.prefix}help to find out more about me.`
+    }
+  }}).catch(e => {
+    Bastion.log.error(e.stack);
+  });
+};
+
+exports.config = {
+  aliases: ['hi'],
+  enabled: true
+};
+
+exports.help = {
+  name: 'hello',
+  description: 'Get to know Bastion!',
+  botPermission: '',
+  userPermission: '',
+  usage: 'hello',
+  example: []
+};
