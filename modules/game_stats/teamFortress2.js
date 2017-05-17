306c8c322eecea216f22c751dac81a947fc3cb86
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-17 19:25:59 +0530

    Added teamFortress2 command

---
+++ modules/game_stats/teamFortress2.js
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2017 Sankarsan Kampa
+ *                    https://sankarsankampa.com/contact
+ *
+ * This file is a part of Bastion Discord BOT.
+ *                        https://github.com/snkrsnkampa/Bastion
+ *
+ * This code is licensed under the SNKRSN Shared License. It is free to
+ * download, copy, compile, use, study and refer under the terms of the
+ * SNKRSN Shared License. You can modify the code only for personal or
+ * internal use only. However, you can not redistribute the code without
+ * explicitly getting permission fot it.
+ *
+ * Bastion BOT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
+ * more details.
+ *
+ * You should have received a copy of the SNKRSN Shared License along
+ * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+ */
+
+const TF2 = require('gamequery');
+
+exports.run = (Bastion, message, args) => {
+  if (args.length < 1 || !/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9]))?$/.test(args = args[0])) {
+    return message.channel.send({embed: {
+      color: Bastion.colors.yellow,
+      title: 'Usage',
+      description: `\`${Bastion.config.prefix}${this.help.usage}\``
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+
+  args = args.split(':');
+  let host = args[0];
+  let port;
+  if (args[1]) {
+    port = parseInt(args[1]);
+  }
+  else {
+    port = 27015;
+  }
+
+  TF2.query({
+    type: 'tf2',
+    host: host,
+    port: port
+  }).then(data => {
+    let stats = [];
+    stats.push(
+      {
+        name: 'Server IP',
+        value: `[${host}:${port}](steam://connect/${host}:${port})`,
+        inline: true
+      },
+      {
+        name: 'Players',
+        value: `${data.players.length}/${data.maxplayers}`,
+        inline: true
+      },
+      {
+        name: 'Private',
+        value: data.password,
+        inline: true
+      },
+      {
+        name: 'Map',
+        value: data.map
+      }
+    );
+
+    if (data.players.length > 0) {
+      let players = [];
+      let scores = [];
+      for (i = 0; i < data.players.length; i++) {
+        players.push(data.players[i].name);
+      }
+      for (i = 0; i < data.players.length; i++) {
+        scores.push(data.players[i].score);
+      }
+      stats.push(
+        {
+          name: 'Player',
+          value: players.join('\n'),
+          inline: true
+        },
+        {
+          name: 'Score',
+          value: scores.join('\n'),
+          inline: true
+        }
+      );
+    }
+
+    message.channel.send({embed: {
+      color: Bastion.colors.blue,
+      title: data.name,
+      description: '[Team Fortress 2](https://store.steampowered.com/app/440/)',
+      fields: stats
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }).catch(e => {
+    Bastion.log.error(e);
+    message.channel.send({embed: {
+      color: Bastion.colors.red,
+      description: 'Can\'t get stats from the specified server. Please check the IP address and PORT number and if the server is online before trying again.',
+    }}).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  });
+};
+
+exports.config = {
+  aliases: ['tf2'],
+  enabled: true
+};
+
+exports.help = {
+  name: 'teamfortress2',
+  description: 'Get status of any Team Fortress 2 game server by it\'s IP address and optional PORT number.',
+  botPermission: '',
+  userPermission: '',
+  usage: 'teamFortress2 <TF2_SERVER_IP>[:PORT]',
+  example: ['teamFortress2 139.59.31.129', 'teamFortress2 139.59.31.129:27019']
+};
