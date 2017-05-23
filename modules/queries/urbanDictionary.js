2b27615a6ef0952e0442c93817add05184a2f958
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-23 19:18:12 +0530

    Improved urbanDictionary command

--- modules/queries/urbanDictionary.js
+++ modules/queries/urbanDictionary.js
@@ -19,7 +19,7 @@
  * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
  */
 
-const urllib = require('urllib');
+const request = require('request');
 
 exports.run = (Bastion, message, args) => {
   if (args.length < 1) {
@@ -34,37 +34,59 @@ exports.run = (Bastion, message, args) => {
     });
   }
 
-  urllib.request(`https://api.urbandictionary.com/v0/define?term=${args.join(' ')}`, function (err, data) {
-    try {
-      data = JSON.parse(data).list;
+  request(`https://api.urbandictionary.com/v0/define?term=${args.join(' ')}`, function (err, response, body) {
+    let color, description = '', data = [];
+
+    if (err) {
+      color = Bastion.colors.red;
+      description = 'Some error has occured while getting data from the server. Please try again later.';
     }
-    catch (e) {
-      return Bastion.log.error(e.stack);
-    }
-    let embed = {};
-    if (data.length !== 0) {
-      embed = {
-        embed: {
-          color: Bastion.colors.blue,
-          title: data[0].word,
-          url: `http://www.urbandictionary.com/define.php?term=${data[0].word}`,
-          description: data[0].definition,
-          footer: {
-            text: 'Powered by Urban Dictionary'
-          }
+    else if (response.statusCode === 200) {
+      color = Bastion.colors.blue;
+      try {
+        body = JSON.parse(body).list[0];
+      }
+      catch (e) {
+        color = Bastion.colors.red;
+        description = 'Some error has occured while parsing the received data. Please try again later.';
+      }
+      data = [
+        {
+          name: 'Word',
+          value: body.word || args.join(' ')
+        },
+        {
+          name: 'Definition',
+          value: body.definition || '-'
+        },
+        {
+          name: 'Example',
+          value: body.example || '-'
         }
-      };
+      ];
     }
     else {
-      embed = {
-        embed: {
-          color: Bastion.colors.red,
-          description: `No definition found for the term **${args.join(' ')}**`
+      color = Bastion.colors.red;
+      description = 'Some error has occured while getting data from the server.';
+      data = [
+        {
+          name: `${response.statusCode}`,
+          value: response.statusMessage
         }
-      };
+      ];
     }
 
-    message.channel.send(embed).catch(e => {
+    message.channel.send({
+      embed: {
+        color: color,
+        title: 'Urban Dictionary',
+        description: description,
+        fields: data,
+        footer: {
+          text: 'Powered by Urban Dictionary'
+        }
+      }
+    }).catch(e => {
       Bastion.log.error(e.stack);
     });
   });
