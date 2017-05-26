bcc8a8b5e6562b3c499a02e266a2190be5993504
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-26 11:40:22 +0530

    Added comments to bastion.js

--- bastion.js
+++ bastion.js
@@ -1,22 +1,7 @@
-/*
- * Copyright (C) 2017 Sankarsan Kampa
- *                    https://sankarsankampa.com/contact
- *
- * This file is a part of Bastion Discord BOT.
- *                        https://github.com/snkrsnkampa/Bastion
- *
- * This code is licensed under the SNKRSN Shared License. It is free to
- * download, copy, compile, use, study and refer under the terms of the
- * SNKRSN Shared License. You can modify the code only for personal or
- * internal use only. However, you can not redistribute the code without
- * explicitly getting permission fot it.
- *
- * Bastion BOT is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
- * more details.
- *
- * You should have received a copy of the SNKRSN Shared License along
- * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+/**
+ * @file The starting point of Bastion
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
  */
 
 const Discord = require('discord.js');
@@ -36,8 +21,11 @@ BASTION.colors = require('./settings/colors.json');
 BASTION.commands = new Discord.Collection();
 BASTION.aliases = new Discord.Collection();
 
+/** Log handler */
 require('./handlers/logHandler')(BASTION);
+/** Event handler */
 require('./handlers/eventHandler')(BASTION);
+/** Module handler */
 require('./handlers/moduleHandler')(BASTION);
 
 // Will use after updating to `discord.js v11.2.0+` as `discord.js v11.1.0` has problems with send() when using array prototypes
