c110b887819ed4b760950068bebb45b6a166cde4
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-26 11:41:10 +0530

    Added comment to handler modules

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,43 +1,111 @@
-/*
- * Copyright (C) 2017 Sankarsan Kampa
- *                    https://sankarsankampa.com/contact
- *
- * This file is a part of Bastion Discord BOT.
- *                        https://github.com/snkrsnkampa/Bastion
- *
- * This code is licensed under the SNKRSN Shared License. It is free to
- * download, copy, compile, use, study and refer under the terms of the
- * SNKRSN Shared License. You can modify the code only for personal or
- * internal use only. However, you can not redistribute the code without
- * explicitly getting permission fot it.
- *
- * Bastion BOT is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
- * more details.
- *
- * You should have received a copy of the SNKRSN Shared License along
- * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+/**
+ * @file The starting point of Bastion
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
  */
 
+/**
+ * @function LOAD_EVENTS
+ * @param {string} event Name of the event
+ * @returns {function} The event's function
+ */
 const LOAD_EVENTS = event => require(`../events/${event}`);
 
+/**
+ * Handles/Loads all the events.
+ * @module eventHandler
+ * @param {object} Bastion The Bastion Object
+ * @returns {void}
+ */
 module.exports = Bastion => {
+  /**
+   * Emitted whenever a channel is created.
+   * @listens channelCreate
+   */
   Bastion.on('channelCreate', LOAD_EVENTS('channelCreate'));
+  /**
+   * Emitted whenever a channel is deleted.
+   * @listens channelDelete
+   */
   Bastion.on('channelDelete', LOAD_EVENTS('channelDelete'));
+  /**
+   * Emitted whenever a channel is updated - e.g. name change, topic change.
+   * @listens channelUpdate
+   */
   Bastion.on('channelUpdate', LOAD_EVENTS('channelUpdate'));
+  /**
+   * Emitted whenever Bastion's WebSocket encounters a connection error.
+   * @listens error
+   */
   Bastion.on('error', LOAD_EVENTS('error'));
+  /**
+   * Emitted whenever a member is banned from a guild.
+   * @listens guildBanAdd
+   */
   Bastion.on('guildBanAdd', LOAD_EVENTS('guildBanAdd'));
+  /**
+   * Emitted whenever a member is unbanned from a guild.
+   * @listens guildBanRemove
+   */
   Bastion.on('guildBanRemove', LOAD_EVENTS('guildBanRemove'));
+  /**
+   * Emitted whenever Bastion joins a guild.
+   * @listens guildCreate
+   */
   Bastion.on('guildCreate', LOAD_EVENTS('guildCreate'));
+  /**
+   * Emitted whenever a guild is deleted/left.
+   * @listens guildDelete
+   */
   Bastion.on('guildDelete', LOAD_EVENTS('guildDelete'));
+  /**
+   * Emitted whenever a user joins a guild.
+   * @listens guildMemberAdd
+   */
   Bastion.on('guildMemberAdd', LOAD_EVENTS('guildMemberAdd'));
+  /**
+   * Emitted whenever a member leaves a guild, or is kicked.
+   * @listens guildMemberRemove
+   */
   Bastion.on('guildMemberRemove', LOAD_EVENTS('guildMemberRemove'));
+  /**
+   * Emitted whenever a guild is updated - e.g. name change.
+   * @listens guildUpdate
+   */
   Bastion.on('guildUpdate', LOAD_EVENTS('guildUpdate'));
+  /**
+   * Emitted whenever a message is created.
+   * @listens message
+   */
   Bastion.on('message', LOAD_EVENTS('message'));
+  /**
+   * Emitted whenever a message is updated - e.g. embed or content change.
+   * @listens messageUpdate
+   */
   Bastion.on('messageUpdate', LOAD_EVENTS('messageUpdate'));
+  /**
+   * Emitted when Bastion becomes ready to start working.
+   * @listens ready
+   */
   Bastion.on('ready', () => LOAD_EVENTS('ready')(Bastion));
+  /**
+   * Emitted whenever a role is created.
+   * @listens roleCreate
+   */
   Bastion.on('roleCreate', LOAD_EVENTS('roleCreate'));
+  /**
+   * Emitted whenever a guild role is deleted.
+   * @listens roleDelete
+   */
   Bastion.on('roleDelete', LOAD_EVENTS('roleDelete'));
+  /**
+   * Emitted whenever a guild role is updated.
+   * @listens roleUpdate
+   */
   Bastion.on('roleUpdate', LOAD_EVENTS('roleUpdate'));
+  /**
+   * Emitted for general warnings.
+   * @listens warn
+   */
   Bastion.on('warn', LOAD_EVENTS('warn'));
 };
--- handlers/logHandler.js
+++ handlers/logHandler.js
@@ -1,36 +1,39 @@
-/*
- * Copyright (C) 2017 Sankarsan Kampa
- *                    https://sankarsankampa.com/contact
- *
- * This file is a part of Bastion Discord BOT.
- *                        https://github.com/snkrsnkampa/Bastion
- *
- * This code is licensed under the SNKRSN Shared License. It is free to
- * download, copy, compile, use, study and refer under the terms of the
- * SNKRSN Shared License. You can modify the code only for personal or
- * internal use only. However, you can not redistribute the code without
- * explicitly getting permission fot it.
- *
- * Bastion BOT is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
- * more details.
- *
- * You should have received a copy of the SNKRSN Shared License along
- * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+/**
+ * @file The starting point of Bastion
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
  */
 
 const COLOR = require('chalk');
 
+/**
+ * Handles/Loads all the funcions for logging messages in console.
+ * @module logHandler
+ * @param {object} Bastion The Bastion Object
+ * @returns {void}
+ */
 module.exports = Bastion => {
   Bastion.log = {};
 
   /* eslint-disable no-console*/
+  /**
+   * Used to display the warning messages.
+   * @function Bastion.log.warn
+   * @param {...string} message Message(s) to be shown in the warn log.
+   * @returns {void}
+   */
   Bastion.log.warn = (...message) => {
     console.log(COLOR.yellow('[WARNING]'));
     console.warn(...message);
     console.log(COLOR.yellow('[/WARNING]'));
   };
 
+  /**
+   * Used to display the error messages.
+   * @function Bastion.log.error
+   * @param {...string} message Message(s) to be shown in the error log.
+   * @returns {void}
+   */
   Bastion.log.error = (...message) => {
     console.log(COLOR.red('[ERROR]'));
     console.log(...message);
@@ -38,14 +41,32 @@ module.exports = Bastion => {
     console.log(COLOR.red('[/ERROR]'));
   };
 
+  /**
+   * Used to display the information/notes in logs.
+   * @function Bastion.log.info
+   * @param {...string} message Message(s) to be shown in the log as information/notes.
+   * @returns {void}
+   */
   Bastion.log.info = (...message) => {
     console.log(COLOR.cyan('[Bastion]: ') + COLOR.yellow(...message));
   };
 
+  /**
+   * Used to display a single messages in the log.
+   * @function Bastion.log.message
+   * @param {string} message Message to be shown in the log.
+   * @returns {void}
+   */
   Bastion.log.message = message => {
     console.log(COLOR.cyan('[Bastion]: ') + message);
   };
 
+  /**
+   * Used to display the error messages.
+   * @function Bastion.log.console
+   * @param {...string} message Message(s) to be logged in the console.
+   * @returns {void}
+   */
   Bastion.log.console = (...message) => {
     console.log(...message);
   };
