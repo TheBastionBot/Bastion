1b52dbd02f0afe2bf1cb52c737b00845f9281b04
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-28 19:16:14 +0530

    Added comments

--- bastion.js
+++ bastion.js
@@ -14,6 +14,9 @@ const BASTION = new Discord.Client({
   ]
 });
 
+/**
+ * Add necessary files as a global object.
+ */
 BASTION.package = require('./package.json');
 BASTION.credentials = require('./settings/credentials.json');
 BASTION.config = require('./settings/config.json');
@@ -21,20 +24,32 @@ BASTION.colors = require('./settings/colors.json');
 BASTION.commands = new Discord.Collection();
 BASTION.aliases = new Discord.Collection();
 
-/** Log handler */
+/**
+ * Log handler
+ */
 require('./handlers/logHandler')(BASTION);
-/** Event handler */
+/**
+ * Event handler
+ */
 require('./handlers/eventHandler')(BASTION);
-/** Module handler */
+/**
+ * Module handler
+ */
 require('./handlers/moduleHandler')(BASTION);
 
 // Will use after updating to `discord.js v11.2.0+` as `discord.js v11.1.0` has problems with send() when using array prototypes
 // require('./functions/Array.prototype');
 
+/**
+ * Log Bastion in as a Discord client.
+ */
 BASTION.login(BASTION.credentials.token).catch(e => {
   BASTION.log.error(e.stack);
 });
 
+/**
+ * Handle unhandled rejections
+ */
 process.on('unhandledRejection', rejection => {
   // eslint-disable-next-line no-console
   console.warn('\n[unhandledRejection]\n\n', rejection, '\n\n[/unhandledRejection]\n');
