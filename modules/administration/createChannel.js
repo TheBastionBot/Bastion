04014d0e120739fb4ae0300512eb3ae8b9600c0a
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-05-31 03:51:29 +0530

    Added createChannel command

---
+++ modules/administration/createChannel.js
@@ -0,0 +1,92 @@
+/**
+ * @file createTextChannel command
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+exports.run = (Bastion, message, args) => {
+  if (!message.member.hasPermission(this.help.userPermission)) return Bastion.log.info('User doesn\'t have permission to use this command.');
+  if (!message.guild.me.hasPermission(this.help.botPermission)) {
+    return message.channel.send({
+      embed: {
+        color: Bastion.colors.red,
+        description: `I need **${this.help.botPermission}** permission to use this command.`
+      }
+    }).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+
+  if (!args.name) {
+    return message.channel.send({
+      embed: {
+        color: Bastion.colors.yellow,
+        title: 'Usage',
+        description: `\`${Bastion.config.prefix}${this.help.usage}\``
+      }
+    }).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+
+  args.name = args.name.join('-');
+  if (args.length < 2 && args.name.length > 100) {
+    return message.channel.send({
+      embed: {
+        color: Bastion.colors.red,
+        description: 'Channel name should be between 2 and 100 characters.'
+      }
+    }).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }
+
+  let channelType = 'text';
+  if (args.voice) {
+    channelType = 'voice';
+  }
+
+  message.guild.createChannel(args.name, channelType).then(channel => {
+    message.channel.send({
+      embed: {
+        color: Bastion.colors.green,
+        title: `${channelType.charAt(0).toUpperCase() + channelType.substr(1)} Channel Created`,
+        fields: [
+          {
+            name: 'Channel Name',
+            value: channel.name,
+            inline: true
+          },
+          {
+            name: 'Channel ID',
+            value: channel.id,
+            inline: true
+          }
+        ]
+      }
+    }).catch(e => {
+      Bastion.log.error(e.stack);
+    });
+  }).catch(e => {
+    Bastion.log.error(e.stack);
+  });
+};
+
+exports.config = {
+  aliases: [ 'cc' ],
+  enabled: true,
+  argsDefinitions: [
+    { name: 'name', alias: 'n', multiple: true, defaultOption: true },
+    { name: 'text', type: Boolean, alias: 't' },
+    { name: 'voice', type: Boolean, alias: 'v' }
+  ]
+};
+
+exports.help = {
+  name: 'createchannel',
+  description: 'Creates a new text/voice channel with a given name.',
+  botPermission: 'MANAGE_CHANNELS',
+  userPermission: 'MANAGE_CHANNELS',
+  usage: 'createChannel [-t | -v] <Channel Name>',
+  example: [ 'createChannel -t Text Channel Name', 'createChannel -v Voice Channel Name', 'createChannel Text Channel Name' ]
+};
