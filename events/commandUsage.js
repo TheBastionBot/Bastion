9c4944deb3a84a1b386f071694f6fb1812eda073
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-06 17:14:24 +0530

    Command usage now shows guild specific prefix instead of default prefix

--- events/commandUsage.js
+++ events/commandUsage.js
@@ -5,25 +5,29 @@
  */
 
 module.exports = (message, command) => {
-  message.channel.send({
-    embed: {
-      color: message.client.colors.yellow,
-      title: 'Invalid Use!',
-      description: `See the usage below to know how to use \`${command.name}\` command.`,
-      fields: [
-        {
-          name: 'Usage',
-          value: `\`${command.usage}\``
-        },
-        {
-          name: 'Example',
-          value: `\`${command.example.join('`\n`') || '-'}\``
+  message.client.db.get(`SELECT prefix FROM guildSettings WHERE guildID=${message.guild.id}`).then(guild => {
+    message.channel.send({
+      embed: {
+        color: message.client.colors.yellow,
+        title: 'Invalid Use!',
+        description: `See the usage below to know how to use \`${command.name}\` command.`,
+        fields: [
+          {
+            name: 'Usage',
+            value: `\`${command.usage}\``
+          },
+          {
+            name: 'Example',
+            value: `\`${command.example.join('`\n`') || '-'}\``
+          }
+        ],
+        footer: {
+          text: `Try ${guild.prefix}help ${command.name} for more information.`
         }
-      ],
-      footer: {
-        text: `Try ${message.client.config.prefix}help ${command.name} for more information.`
       }
-    }
+    }).catch(e => {
+      message.client.log.error(e.stack);
+    });
   }).catch(e => {
     message.client.log.error(e.stack);
   });
