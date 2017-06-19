301192617672ae1172f487b3de182f217a0b37ec
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-19 06:58:21 +0530

    Paginated roles command

--- modules/info/roles.js
+++ modules/info/roles.js
@@ -4,12 +4,26 @@
  * @license MIT
  */
 
-exports.run = (Bastion, message) => {
+exports.run = (Bastion, message, args) => {
+  let roles = message.guild.roles.filter(r => r.position !== 0).map(r => r.name).map((r, i) => `${i + 1}. ${r}`);
+
+  let i = 0, noOfPages = roles.length / 10;
+  if (isNaN(args = parseInt(args[0]))) {
+    i = 1;
+  }
+  else {
+    i = (args > 0 && args < noOfPages + 1) ? args : 1;
+  }
+  i = i - 1;
+
   message.channel.send({
     embed: {
       color: Bastion.colors.blue,
       title: 'Roles',
-      description: message.guild.roles.size > 10 ? `${message.guild.roles.map(r => r.name).splice(1, 10).join('\n')}\nand ${message.guild.roles.size - 10 - 1} roles.` :  message.guild.roles.map(r => r.name).splice(1).join('\n')
+      description: roles.slice(i * 10, (i * 10) + 10).join('\n'),
+      footer: {
+        text: `Page: ${i + 1} of ${noOfPages > parseInt(noOfPages) ? parseInt(noOfPages) + 1 : parseInt(noOfPages)}`
+      }
     }
   }).catch(e => {
     Bastion.log.error(e.stack);
