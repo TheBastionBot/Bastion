dd77cc6ba844267f0838c869db6cc98a485af165
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-19 19:31:41 +0530

    Added function handler

--- bastion.js
+++ bastion.js
@@ -15,7 +15,7 @@ const BASTION = new Discord.Client({
 });
 
 /**
- * Add necessary files as a global object.
+ * Add necessary files as a global objects.
  */
 BASTION.package = require('./package.json');
 BASTION.credentials = require('./settings/credentials.json');
@@ -23,9 +23,14 @@ BASTION.config = require('./settings/config.json');
 BASTION.colors = require('./settings/colors.json');
 BASTION.commands = new Discord.Collection();
 BASTION.aliases = new Discord.Collection();
+BASTION.functions = {};
 BASTION.db = require('sqlite');
 BASTION.db.open('./data/Bastion.sqlite');
 
+/**
+* Function handler
+*/
+require('./handlers/functionHandler')(BASTION);
 /**
  * Log handler
  */
