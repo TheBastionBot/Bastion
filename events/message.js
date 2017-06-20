dc4fb7ff1097f044aab5015d3439f96beb402aad
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-21 05:00:24 +0530

    Implemented command handler

--- events/message.js
+++ events/message.js
@@ -5,19 +5,18 @@
  */
 
 const CLEVERBOT = require('cleverbot-node');
-const parseArgs = require('command-line-args');
 const CREDENTIALS = require('../settings/credentials.json');
 const BOT = new CLEVERBOT;
 BOT.configure({
   botapi: CREDENTIALS.cleverbotAPIkey
 });
-const COLOR = require('chalk');
 const credentialsFilter = require('../utils/credentialsFilter');
 const wordFilter = require('../utils/wordFilter');
 const linkFilter = require('../utils/linkFilter');
 const inviteFilter = require('../utils/inviteFilter');
 const handleTrigger = require('../handlers/triggerHandler');
 const handleUserLevel = require('../handlers/levelHandler');
+const handleCommand = require('../handlers/commandHandler');
 
 module.exports = message => {
   /**
@@ -56,35 +55,10 @@ module.exports = message => {
        */
       handleUserLevel(message);
 
-      message.client.db.get(`SELECT prefix FROM guildSettings WHERE guildID=${message.guild.id}`).then(guild => {
-        if (message.content.startsWith(guild.prefix)) {
-          let args = message.content.split(' ');
-          let command = args.shift().slice(guild.prefix.length).toLowerCase();
-
-          let cmd;
-          if (message.client.commands.has(command)) {
-            cmd = message.client.commands.get(command);
-          }
-          else if (message.client.aliases.has(command)) {
-            cmd = message.client.commands.get(message.client.aliases.get(command));
-          }
-          else return;
-
-          message.client.log.console(`\n[${new Date()}]`);
-          message.client.log.console(COLOR.green('[COMMAND]: ') + guild.prefix + command);
-          message.client.log.console(COLOR.green('[ARGUMENTs]: ') + (args.join(' ') || COLOR.yellow('No arguments to execute')));
-          message.client.log.console(`${COLOR.green('[SERVER]:')} ${message.guild} ${COLOR.cyan(`<#${message.guild.id}>`)}`);
-          message.client.log.console(`${COLOR.green('[CHANNEL]:')} #${message.channel.name} ${COLOR.cyan(message.channel)}`);
-          message.client.log.console(`${COLOR.green('[USER]:')} ${message.author.tag} ${COLOR.cyan(`${message.author}`)}`);
-
-          if (!cmd.config.enabled) {
-            return message.client.log.info('This command is disabled.');
-          }
-          cmd.run(message.client, message, parseArgs(cmd.config.argsDefinitions, { argv: args, partial: true }));
-        }
-      }).catch(e => {
-        message.client.log.error(e.stack);
-      });
+      /**
+       * Handles Bastion's commands
+       */
+      handleCommand(message);
 
       if (message.content.startsWith(`<@${message.client.credentials.botId}>`) || message.content.startsWith(`<@!${message.client.credentials.botId}>`)) {
         message.client.db.get(`SELECT chat FROM guildSettings WHERE guildID=${message.guild.id}`).then(guild => {
