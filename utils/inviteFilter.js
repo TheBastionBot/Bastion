241747efb37efa49823f9868dd0e8a2b8284ab8d
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-20 09:38:28 +0530

    Added inviteFilter function

---
+++ utils/inviteFilter.js
@@ -0,0 +1,26 @@
+/**
+ * @file inviteFilter
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+/**
+ * Handles filtering of Discord server invites in messages
+ * @param {Message} message Discord.js message object
+ * @returns {void}
+ */
+module.exports = message => {
+  message.client.db.get(`SELECT filterInvite FROM guildSettings WHERE guildID=${message.guild.id}`).then(guild => {
+    if (guild.filterInvite === 'true' && !message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) {
+      if (/(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite\/)\/?([a-z0-9-.]+)?/i.test(message.content)) {
+        if (message.deletable) {
+          message.delete().catch(e => {
+            message.client.log.error(e.stack);
+          });
+        }
+      }
+    }
+  }).catch(e => {
+    message.client.log.error(e.stack);
+  });
+};
