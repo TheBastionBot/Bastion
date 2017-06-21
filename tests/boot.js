5b1c8dbeefaabafb82bf41430b48cb72bd505e35
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-21 15:33:57 +0530

    Updated test scripts

--- tests/boot.js
+++ tests/boot.js
@@ -1,22 +1,7 @@
-/*
- * Copyright (C) 2017 Sankarsan Kampa
- *                    https://sankarsankampa.com/contact
- *
- * This file is a part of Bastion Discord BOT.
- *                        https://github.com/snkrsnkampa/Bastion
- *
- * This code is licensed under the SNKRSN Shared License. It is free to
- * download, copy, compile, use, study and refer under the terms of the
- * SNKRSN Shared License. You can modify the code only for personal or
- * internal use only. However, you can not redistribute the code without
- * explicitly getting permission fot it.
- *
- * Bastion BOT is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY. See the SNKRSN Shared License for
- * more details.
- *
- * You should have received a copy of the SNKRSN Shared License along
- * with this program. If not, see <https://github.com/snkrsnkampa/Bastion/LICENSE>.
+/**
+ * @file Test script to test Bastion's successful booting
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
  */
 
 const Discord = require('discord.js');
@@ -33,10 +18,12 @@ BASTION.package = require('../package.json');
 BASTION.credentials = require('../settings/credentials.json');
 BASTION.config = require('../settings/config.json');
 BASTION.colors = require('../settings/colors.json');
+BASTION.functions = {};
 BASTION.commands = new Discord.Collection();
 BASTION.aliases = new Discord.Collection();
 
 try {
+  require('../handlers/functionHandler')(BASTION);
   require('../handlers/logHandler')(BASTION);
   require('../handlers/eventHandler')(BASTION);
   require('../handlers/moduleHandler')(BASTION);
