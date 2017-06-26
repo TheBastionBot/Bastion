f7f1596dc3aa1cd2d086deda1a29cdcc4763a414
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-27 02:35:27 +0530

    Improvements to followURL function

--- functions/followURL.js
+++ functions/followURL.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 module.exports = (url) => {
-  if (!/(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/i.test(url)) return;
+  if (!/^(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)$/i.test(url)) return;
 
   return new Promise((resolve, reject) => {
     request( { method: 'HEAD', url: url, followAllRedirects: true },
