039426cd590b7dc71778c33415fcc53acec901bb
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-27 02:27:24 +0530

    Added followURL function

---
+++ functions/followURL.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+module.exports = (url) => {
+  if (!/(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/i.test(url)) return;
+
+  return new Promise((resolve, reject) => {
+    request( { method: 'HEAD', url: url, followAllRedirects: true },
+      function (error, response) {
+        if (error) {
+          reject(error);
+        }
+        else {
+          resolve(response.request.href);
+        }
+      });
+  });
+};
