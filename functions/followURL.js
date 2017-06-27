2d15f74fd2c01548e42a0f434450b054b62f5700
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-06-27 13:23:42 +0530

    Improved followURL function

--- functions/followURL.js
+++ functions/followURL.js
@@ -1,9 +1,11 @@
 const request = require('request');
 
 module.exports = (url) => {
-  if (!/^(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)$/i.test(url)) return;
-
   return new Promise((resolve, reject) => {
+    if (!/^(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)$/i.test(url)) {
+      resolve('Invalid URI');
+    }
+
     request( { method: 'HEAD', url: url, followAllRedirects: true },
       function (error, response) {
         if (error) {
