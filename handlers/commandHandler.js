51943c92d5c7292763a664b2b1748259541e403f
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-07-22 14:02:23 +0530

    Implemented async/await in handlers

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -12,38 +12,41 @@ const COLOR = require('chalk');
  * @param {Message} message Discord.js message object
  * @returns {void}
  */
-module.exports = message => {
-  message.client.db.get(`SELECT prefix FROM guildSettings WHERE guildID=${message.guild.id}`).then(guild => {
+module.exports = async message => {
+  try {
+    let guild = await message.client.db.get(`SELECT prefix FROM guildSettings WHERE guildID=${message.guild.id}`);
+
     if (!message.guild.prefix || message.guild.prefix !== guild.prefix) {
       message.guild.prefix = guild.prefix;
     }
 
-    if (message.content.startsWith(guild.prefix)) {
-      let args = message.content.split(' ');
-      let command = args.shift().slice(guild.prefix.length).toLowerCase();
+    if (!message.content.startsWith(guild.prefix)) return;
 
-      let cmd;
-      if (message.client.commands.has(command)) {
-        cmd = message.client.commands.get(command);
-      }
-      else if (message.client.aliases.has(command)) {
-        cmd = message.client.commands.get(message.client.aliases.get(command));
-      }
-      else return;
+    let args = message.content.split(' ');
+    let command = args.shift().slice(guild.prefix.length).toLowerCase();
 
-      message.client.log.console(`\n[${new Date()}]`);
-      message.client.log.console(COLOR.green('[COMMAND]: ') + guild.prefix + command);
-      message.client.log.console(COLOR.green('[ARGUMENTs]: ') + (args.join(' ') || COLOR.yellow('No arguments to execute')));
-      message.client.log.console(`${COLOR.green('[SERVER]:')} ${message.guild} ${COLOR.cyan(`<#${message.guild.id}>`)}`);
-      message.client.log.console(`${COLOR.green('[CHANNEL]:')} #${message.channel.name} ${COLOR.cyan(message.channel)}`);
-      message.client.log.console(`${COLOR.green('[USER]:')} ${message.author.tag} ${COLOR.cyan(`${message.author}`)}`);
-
-      if (!cmd.config.enabled) {
-        return message.client.log.info('This command is disabled.');
-      }
-      cmd.run(message.client, message, parseArgs(cmd.config.argsDefinitions, { argv: args, partial: true }));
+    let cmd;
+    if (message.client.commands.has(command)) {
+      cmd = message.client.commands.get(command);
     }
-  }).catch(e => {
+    else if (message.client.aliases.has(command)) {
+      cmd = message.client.commands.get(message.client.aliases.get(command));
+    }
+    else return;
+
+    message.client.log.console(`\n[${new Date()}]`);
+    message.client.log.console(COLOR.green('[COMMAND]: ') + guild.prefix + command);
+    message.client.log.console(COLOR.green('[ARGUMENTs]: ') + (args.join(' ') || COLOR.yellow('No arguments to execute')));
+    message.client.log.console(`${COLOR.green('[SERVER]:')} ${message.guild} ${COLOR.cyan(`<#${message.guild.id}>`)}`);
+    message.client.log.console(`${COLOR.green('[CHANNEL]:')} #${message.channel.name} ${COLOR.cyan(message.channel)}`);
+    message.client.log.console(`${COLOR.green('[USER]:')} ${message.author.tag} ${COLOR.cyan(`${message.author}`)}`);
+
+    if (!cmd.config.enabled) {
+      return message.client.log.info('This command is disabled.');
+    }
+    cmd.run(message.client, message, parseArgs(cmd.config.argsDefinitions, { argv: args, partial: true }));
+  }
+  catch (e) {
     message.client.log.error(e);
-  });
+  }
 };
