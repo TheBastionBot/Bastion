0e77ff8bf2383b98da75ca073a32838a2db98803
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-08-04 16:29:07 +0530

    Added manga command

---
+++ modules/queries/manga.js
@@ -0,0 +1,60 @@
+/**
+ * @file manga command
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+const string = require('../../handlers/languageHandler');
+const Kitsu = require('kitsu.js');
+const kitsu = new Kitsu();
+
+exports.run = async (Bastion, message, args) => {
+  let manga = await kitsu.searchManga(args.name);
+  manga = manga[0];
+
+  message.channel.send({
+    embed: {
+      color: Bastion.colors.blue,
+      title: manga.titles.en || manga.titles.enJp || manga.titles.canonicalTitle,
+      url: `https://kitsu.io/manga/${manga.slug}`,
+      description: manga.synopsis,
+      fields: [
+        {
+          name: 'Status',
+          value: manga.endDate ? 'Finished' : 'Publishing',
+          inline: true
+        },
+        {
+          name: 'Published',
+          value: manga.endDate ? `${manga.startDate} - ${manga.endDate}` : `${manga.startDate} - Present`,
+          inline: true
+        }
+      ],
+      image: {
+        url: manga.posterImage.original
+      },
+      footer: {
+        text: 'Powered by Kitsu'
+      }
+    }
+  }).catch(e => {
+    Bastion.log.error(e);
+  });
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true,
+  argsDefinitions: [
+    { name: 'name', type: String, multiple: true, defaultOption: true }
+  ]
+};
+
+exports.help = {
+  name: 'manga',
+  description: string('manga', 'commandDescription'),
+  botPermission: '',
+  userPermission: '',
+  usage: 'manga <Manga Name>',
+  example: [ 'manga Death Note' ]
+};
