8b7bf09ae4d3b11aa39a41b836557a76e37d09f3
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-08-04 12:23:43 +0530

    Added punch command

---
+++ modules/fun/punch.js
@@ -0,0 +1,52 @@
+/**
+ * @file punch command
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+const string = require('../../handlers/languageHandler');
+
+exports.run = (Bastion, message) => {
+  let user = message.mentions.users.first();
+  if (!user) {
+    /**
+     * The command was ran with invalid parameters.
+     * @fires commandUsage
+     */
+    return Bastion.emit('commandUsage', message, this.help);
+  }
+
+  let punches = [
+    'https://i.giphy.com/media/iWEIxgPiAq58c/giphy.gif',
+    'https://i.giphy.com/media/DViGV8rfVjw6Q/giphy.gif',
+    'https://i.giphy.com/media/GoN89WuFFqb2U/giphy.gif',
+    'https://i.giphy.com/media/xT0BKiwgIPGShJNi0g/giphy.gif',
+    'https://i.giphy.com/media/Lx8lyPHGfdNjq/giphy.gif'
+  ];
+
+  message.channel.send({
+    embed: {
+      color: Bastion.colors.blue,
+      description: `${message.author.username} punched ${user.username}! :punch:`,
+      image: {
+        url: punches[Math.floor(Math.random() * punches.length)]
+      }
+    }
+  }).catch(e => {
+    Bastion.log.error(e);
+  });
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true
+};
+
+exports.help = {
+  name: 'punch',
+  description: string('punch', 'commandDescription'),
+  botPermission: '',
+  userPermission: '',
+  usage: 'punch <@USER_MENTION>',
+  example: [ 'punch @user#0001' ]
+};
