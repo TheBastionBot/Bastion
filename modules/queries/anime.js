60235ce9791341c4d638d786dc9658bcb65b6b70
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-08-04 16:08:59 +0530

    Added anime command

---
+++ modules/queries/anime.js
@@ -0,0 +1,65 @@
+/**
+ * @file anime command
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+const string = require('../../handlers/languageHandler');
+const Kitsu = require('kitsu.js');
+const kitsu = new Kitsu();
+
+exports.run = async (Bastion, message, args) => {
+  let anime = await kitsu.searchAnime(args.name);
+  anime = anime[0];
+
+  message.channel.send({
+    embed: {
+      color: Bastion.colors.blue,
+      title: anime.titles.english,
+      url: `https://kitsu.io/anime/${anime.slug}`,
+      description: anime.synopsis,
+      fields: [
+        {
+          name: 'Status',
+          value: anime.endDate ? 'Finished' : 'Airing',
+          inline: true
+        },
+        {
+          name: 'Aired',
+          value: anime.endDate ? `${anime.startDate} - ${anime.endDate}` : `${anime.startDate} - Present`,
+          inline: true
+        },
+        {
+          name: 'Rating',
+          value: `${anime.ageRating} - ${anime.ageRatingGuide} ${anime.nsfw ? '[NSFW]' : ''}`,
+          inline: true
+        }
+      ],
+      image: {
+        url: anime.posterImage.original
+      },
+      footer: {
+        text: 'Powered by Kitsu'
+      }
+    }
+  }).catch(e => {
+    Bastion.log.error(e);
+  });
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true,
+  argsDefinitions: [
+    { name: 'name', type: String, multiple: true, defaultOption: true }
+  ]
+};
+
+exports.help = {
+  name: 'anime',
+  description: string('anime', 'commandDescription'),
+  botPermission: '',
+  userPermission: '',
+  usage: 'anime <Anime Name>',
+  example: [ 'anime One Piece' ]
+};
