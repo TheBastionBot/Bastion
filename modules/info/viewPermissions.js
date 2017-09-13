5f3aa315173dad870fd9d84a2130860c7ffa0335
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-09-13 21:29:10 +0530

    Added viewPermissions command

---
+++ modules/info/viewPermissions.js
@@ -0,0 +1,43 @@
+/**
+ * @file viewPermissions command
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+const string = require('../../handlers/languageHandler');
+
+exports.run = (Bastion, message) => {
+  let fields = [];
+  let permissions = message.member.permissions.serialize();
+  for (let permission in permissions) {
+    fields.push({
+      name: permission.replace(/_/g, ' ').toTitleCase(),
+      value: permissions[permission],
+      inline: true
+    });
+  }
+  message.channel.send({
+    embed: {
+      color: Bastion.colors.BLUE,
+      title: `Permissions for ${message.author.tag}`,
+      description: 'Permissions you have in this channel and server.',
+      fields: fields
+    }
+  }).catch(e => {
+    Bastion.log.error(e);
+  });
+};
+
+exports.config = {
+  aliases: [ 'viewPerms' ],
+  enabled: true
+};
+
+exports.help = {
+  name: 'viewPermissions',
+  description: string('viewPermissions', 'commandDescription'),
+  botPermission: '',
+  userPermission: '',
+  usage: 'viewPermissions',
+  example: []
+};
