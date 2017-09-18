ab5884f9934f490cef41ca7a6a9df319a0e2e06f
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-09-18 11:03:18 +0530

    Improved guildCreate event * Removed redundant code

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,46 +5,9 @@
  */
 
 module.exports = guild => {
-  guild.client.db.run('CREATE TABLE IF NOT EXISTS guildSettings' +
-    '(guildID TEXT NOT NULL UNIQUE,' +
-    `prefix TEXT NOT NULL DEFAULT '${guild.client.config.prefix}',` +
-    'greet TEXT UNIQUE,' +
-    'greetMessage BLOB,' +
-    'greetTimeout INTEGER NOT NULL DEFAULT 30,' +
-    'greetPrivate INTEGER NOT NULL DEFAULT 0,' +
-    'greetPrivateMessage BLOB,' +
-    'farewell TEXT UNIQUE,' +
-    'farewellMessage BLOB,' +
-    'farewellTimeout INTEGER NOT NULL DEFAULT 15,' +
-    'log TEXT UNIQUE,' +
-    'musicTextChannel TEXT UNIQUE,' +
-    'musicVoiceChannel TEXT UNIQUE,' +
-    'musicMasterRole TEXT UNIQUE,' +
-    'filterInvite INTEGER NOT NULL DEFAULT 0,' +
-    'filterLink INTEGER NOT NULL DEFAULT 0,' +
-    'whitelistDomains TEXT NOT NULL DEFAULT \'[]\',' +
-    'filterWord INTEGER NOT NULL DEFAULT 0,' +
-    'filteredWords TEXT,' +
-    'slowMode INTEGER NOT NULL DEFAULT 0,' +
-    'announcementChannel TEXT,' +
-    'chat INTEGER NOT NULL DEFAULT 0,' +
-    'levelUpMessage INTEGER NOT NULL DEFAULT 0,' +
-    'selfAssignableRoles TEXT,' +
-    'autoAssignableRoles TEXT' +
-    'streamerRole TEXT,' +
-    'warnAction TEXT,' +
-    'ignoredChannels TEXT,' +
-    'ignoredRoles TEXT,' +
-    'starboard TEXT UNIQUE,' +
-    'modLog TEXT UNIQUE,' +
-    'modCaseNo TEXT NOT NULL DEFAULT \'1\',' +
-    'PRIMARY KEY(guildID))').then(() => {
-      guild.client.db.run('INSERT OR IGNORE INTO guildSettings (guildID) VALUES (?)', [ guild.id ]).catch(e => {
-        guild.client.log.error(e);
-      });
-    }).catch(e => {
-      guild.client.log.error(e);
-    });
+  guild.client.db.run('INSERT OR IGNORE INTO guildSettings (guildID) VALUES (?)', [ guild.id ]).catch(e => {
+    guild.client.log.error(e);
+  });
 
   /**
    * bastion join/leave log events, if enabled
