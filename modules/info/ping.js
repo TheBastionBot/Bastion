aa6093fb5b28aba524f6621acce9e59a05e4425f
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-14 21:46:23 +0530

    Improvements to ping command ping now shows response time as well as the websocket ping

--- modules/info/ping.js
+++ modules/info/ping.js
@@ -4,15 +4,35 @@
  * @license MIT
  */
 
-exports.run = (Bastion, message) => {
-  message.channel.send({
-    embed: {
-      color: Bastion.colors.BLUE,
-      description: `${parseInt(Bastion.ping)}ms`
-    }
-  }).catch(e => {
+exports.run = async (Bastion, message) => {
+  try {
+    let responseMessage = await message.channel.send({
+      embed: {
+        color: Bastion.colors.BLUE,
+        description: 'PINGing...'
+      }
+    });
+    await responseMessage.edit({
+      embed: {
+        color: Bastion.colors.BLUE,
+        fields: [
+          {
+            name: 'Response Time',
+            value: `${responseMessage.createdTimestamp - message.createdTimestamp}ms`,
+            inline: true
+          },
+          {
+            name: 'WebSocket PING',
+            value: `${Bastion.ping}ms`,
+            inline: true
+          }
+        ]
+      }
+    });
+  }
+  catch (e) {
     Bastion.log.error(e);
-  });
+  }
 };
 
 exports.config = {
