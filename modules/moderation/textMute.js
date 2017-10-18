ca746bdefc0e32bef7fcb9b8237ad2f6d2c5734a
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-18 22:10:04 +0530

    Improvements to textMute command * textMute can now mute users globally across the server * Logic improvements

--- modules/moderation/textMute.js
+++ modules/moderation/textMute.js
@@ -33,16 +33,36 @@ exports.run = async (Bastion, message, args) => {
   if (message.author.id !== message.guild.ownerID && message.member.highestRole.comparePositionTo(message.guild.members.get(user.id).highestRole) <= 0) return Bastion.log.info(Bastion.strings.error(message.guild.language, 'lowerRole', true));
 
   try {
-    await message.channel.overwritePermissions(user, {
-      SEND_MESSAGES: false,
-      ADD_REACTIONS: false
-    });
-
     if (args.reason) {
       args.reason = args.reason.filter(str => !str.startsWith('<@') || !str.endsWith('>'));
     }
-
     args.reason = args.reason && args.reason.length ? args.reason.join(' ') : 'No reason given';
+
+    if (args.server) {
+      let mutedRole = message.guild.roles.find('name', 'Bastion:mute');
+      if (!mutedRole) {
+        mutedRole = await message.guild.createRole({ name:'Bastion:mute' });
+      }
+
+      let member = message.guild.members.get(user.id);
+      await member.addRole(mutedRole, args.reason);
+
+      for (let channel of message.guild.channels.filter(channel => channel.type === 'text')) {
+        channel = channel[1];
+        if (!channel.permissionOverwrites.get(mutedRole.id)) {
+          await channel.overwritePermissions(mutedRole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        }
+      }
+    }
+    else {
+      await message.channel.overwritePermissions(user, {
+        SEND_MESSAGES: false,
+        ADD_REACTIONS: false
+      });
+    }
 
     message.channel.send({
       embed: {
@@ -87,7 +107,8 @@ exports.config = {
   aliases: [ 'tm' ],
   enabled: true,
   argsDefinitions: [
-    { name: 'reason', type: String, multiple: true, defaultOption: true }
+    { name: 'reason', type: String, multiple: true, defaultOption: true },
+    { name: 'server', type: Boolean, alias: 's' }
   ]
 };
 
@@ -95,6 +116,6 @@ exports.help = {
   name: 'textMute',
   botPermission: 'MANAGE_ROLES',
   userPermission: 'MANAGE_ROLES',
-  usage: 'textMute @user-mention [Reason]',
-  example: [ 'textMute @user#0001 Reason for the mute.' ]
+  usage: 'textMute @user-mention [Reason] [--server]',
+  example: [ 'textMute @user#0001 off topic discussions', 'textMute @user#0001 misbehaving others --server' ]
 };
