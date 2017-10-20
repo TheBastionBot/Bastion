0f56ba55c9cb560ba84c83b5497426e5332093ce
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-20 20:43:21 +0530

    Improvements to command handler * Command log will now show the module of the command * Commands can now be enabled/disabled specific to guilds by parsing bastion meta data in channel topics

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -50,10 +50,12 @@ module.exports = async message => {
       cmd = message.client.commands.get(message.client.aliases.get(command).toLowerCase());
     }
     else return;
+    let mdl = cmd.config.module;
 
     message.client.log.console(`\n[${new Date()}]`);
     message.client.log.console(COLOR.green('[COMMAND]: ') + usedPrefix + command);
     message.client.log.console(COLOR.green('[ARGUMENTs]: ') + (args.join(' ') || COLOR.yellow('No arguments to execute')));
+    message.client.log.console(COLOR.green('[MODULE]: ') + mdl);
     message.client.log.console(`${COLOR.green('[SERVER]:')} ${message.guild} ${COLOR.cyan(`<#${message.guild.id}>`)}`);
     message.client.log.console(`${COLOR.green('[CHANNEL]:')} #${message.channel.name} ${COLOR.cyan(message.channel)}`);
     message.client.log.console(`${COLOR.green('[USER]:')} ${message.author.tag} ${COLOR.cyan(`${message.author}`)}`);
@@ -61,6 +63,46 @@ module.exports = async message => {
     if (!cmd.config.enabled) {
       return message.client.log.info('This command is disabled.');
     }
+
+    if (message.channel.topic) {
+      let parsedTopic = message.channel.topic.split('\n').filter(str => str.length);
+      parsedTopic = parsedTopic[parsedTopic.length - 1];
+
+      let topicSyntax = new RegExp(`${message.client.user.id}:(?:(?:enableCommands|disableCommands):[a-z ]+|(?:enableModules|disableModules):[a-z ]+)`, 'i');
+
+      if (topicSyntax.test(parsedTopic)) {
+        parsedTopic = parsedTopic.toLowerCase().split(':');
+        if (parsedTopic.length >= 3) {
+          let filteredCommands = parsedTopic[2].split(' ');
+
+          switch (parsedTopic[1]) {
+            case 'enablecommands':
+              if (!filteredCommands.includes(command)) {
+                return message.client.log.info('This command is disabled via channel topic.');
+              }
+              break;
+            case 'disablecommands':
+              if (filteredCommands.includes(command) || filteredCommands.includes('all')) {
+                return message.client.log.info('This command is disabled via channel topic.');
+              }
+              break;
+            case 'enablemodules':
+              if (!filteredCommands.includes(mdl.toLowerCase())) {
+                return message.client.log.info('This module is disabled via channel topic.');
+              }
+              break;
+            case 'disablemodules':
+              if (filteredCommands.includes(mdl.toLowerCase()) || filteredCommands.includes('all')) {
+                return message.client.log.info('This module is disabled via channel topic.');
+              }
+              break;
+            default:
+              break;
+          }
+        }
+      }
+    }
+
     cmd.run(message.client, message, parseArgs(cmd.config.argsDefinitions, { argv: args, partial: true }));
   }
   catch (e) {
