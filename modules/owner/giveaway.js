0a8145e2a514109ec054a579e65a91a487ecc99b
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-21 13:18:35 +0530

    Minor code improvements

--- modules/owner/giveaway.js
+++ modules/owner/giveaway.js
@@ -36,7 +36,7 @@ exports.run = async (Bastion, message, args) => {
     reaction = reaction[Math.floor(Math.random() * reaction.length)];
 
     try {
-      let giveawayMessage = await message.channel.send({
+      await message.channel.send({
         embed: {
           color: Bastion.colors.BLUE,
           title: 'GIVEAWAY! 🎉',
@@ -46,42 +46,45 @@ exports.run = async (Bastion, message, args) => {
           }
         }
       });
-      let giveawayMessageID = giveawayMessage.id;
+
       activeChannel = message.channel.id;
 
       setTimeout(async () => {
-        let giveawayMessage = await message.channel.fetchMessage(giveawayMessageID).catch(e => {
+        try {
+          let giveawayMessage = await message.channel.fetchMessage(giveawayMessage.id);
+
+          // reaction = encodeURIComponent(reaction);
+
+          let winners = [];
+          if (giveawayMessage.reactions.get(reaction)) {
+            winners = giveawayMessage.reactions.get(reaction).users.map(u => u.id);
+          }
+
+          let winner = winners[Math.floor(Math.random() * winners.length)];
+          winner = Bastion.users.get(winner);
+          if (winner) {
+            /**
+            * User's account is debited with `args.amount` Bastion Currencies
+            * @fires userDebit
+            */
+            Bastion.emit('userDebit', winner, args.amount);
+
+            giveawayMessage.edit('', {
+              embed: {
+                color: Bastion.colors.BLUE,
+                title: 'Giveaway event ended',
+                description: `${winner.tag} won the giveaway! And has been awarded with **${args.amount}** Bastion Currencies.\nThank you everyone for participating. Better luck next time.`
+              }
+            }).catch(e => {
+              Bastion.log.error(e);
+            });
+          }
+
+          activeChannel = null;
+        }
+        catch (e) {
           Bastion.log.error(e);
-        });
-
-        // reaction = encodeURIComponent(reaction);
-
-        let winners = [];
-        if (giveawayMessage.reactions.get(reaction)) {
-          winners = giveawayMessage.reactions.get(reaction).users.map(u => u.id);
         }
-
-        let winner = winners[Math.floor(Math.random() * winners.length)];
-        winner = Bastion.users.get(winner);
-        if (winner) {
-          /**
-          * User's account is debited with `args.amount` Bastion Currencies
-          * @fires userDebit
-          */
-          Bastion.emit('userDebit', winner, args.amount);
-
-          giveawayMessage.edit('', {
-            embed: {
-              color: Bastion.colors.BLUE,
-              title: 'Giveaway event ended',
-              description: `${winner.tag} won the giveaway! And has been awarded with **${args.amount}** Bastion Currencies.\nThank you everyone for participating. Better luck next time.`
-            }
-          }).catch(e => {
-            Bastion.log.error(e);
-          });
-        }
-
-        activeChannel = null;
       }, TIMEOUT * 60 * 60 * 1000);
     }
     catch (e) {
