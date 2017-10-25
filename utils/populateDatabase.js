9648034d1140db3ad667689fff51d3fae1fb17f5
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-25 20:54:19 +0530

    Fixed an issue with default prefix that caused the deafult prefix to be NULL (commands worked without any prefix) if the prefix was not configured in config.json before running Bastion for the first time

--- utils/populateDatabase.js
+++ utils/populateDatabase.js
@@ -9,7 +9,7 @@ const config = require('../settings/config.json');
 module.exports = async db => {
   await db.run('CREATE TABLE IF NOT EXISTS guildSettings' +
     '(guildID TEXT NOT NULL UNIQUE,' +
-    `prefix TEXT NOT NULL DEFAULT '${config.prefix}',` +
+    `prefix TEXT NOT NULL DEFAULT '${config.prefix || '!'}',` +
     'language TEXT DEFAULT \'en\',' +
     'greet TEXT,' +
     'greetMessage BLOB,' +
