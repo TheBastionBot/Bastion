fbfcdefbbca10989b8a4e031716754cdad69bf73
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-26 22:03:59 +0530

    Improvements to stats command It will omit the wrong user IDs instead of preventing the command from running

--- modules/info/stats.js
+++ modules/info/stats.js
@@ -5,104 +5,109 @@
  */
 
 exports.run = async (Bastion, message) => {
-  let owners = [];
-  for (let userID of Bastion.credentials.ownerId) {
-    let user = await Bastion.fetchUser(userID);
-    owners.push(user.tag);
-  }
+  try {
+    let owners = [];
+    for (let userID of Bastion.credentials.ownerId) {
+      let user = await Bastion.fetchUser(userID).catch(() => {});
+      owners.push(user.tag);
+    }
 
-  let uptime = Bastion.uptime;
-  let seconds = uptime / 1000;
-  let days = parseInt(seconds / 86400);
-  seconds = seconds % 86400;
-  let hours = parseInt(seconds / 3600);
-  seconds = seconds % 3600;
-  let minutes = parseInt(seconds / 60);
-  seconds = parseInt(seconds % 60);
+    let uptime = Bastion.uptime;
+    let seconds = uptime / 1000;
+    let days = parseInt(seconds / 86400);
+    seconds = seconds % 86400;
+    let hours = parseInt(seconds / 3600);
+    seconds = seconds % 3600;
+    let minutes = parseInt(seconds / 60);
+    seconds = parseInt(seconds % 60);
 
-  uptime = `${seconds}s`;
-  if (days) {
-    uptime = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-  }
-  else if (hours) {
-    uptime = `${hours}h ${minutes}m ${seconds}s`;
-  }
-  else if (minutes) {
-    uptime = `${minutes}m ${seconds}s`;
-  }
+    uptime = `${seconds}s`;
+    if (days) {
+      uptime = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+    else if (hours) {
+      uptime = `${hours}h ${minutes}m ${seconds}s`;
+    }
+    else if (minutes) {
+      uptime = `${minutes}m ${seconds}s`;
+    }
 
-  message.channel.send({
-    embed: {
-      color: Bastion.colors.BLUE,
-      author: {
-        name: `Bastion ${Bastion.package.version}`
-      },
-      url: Bastion.package.url,
-      fields: [
-        {
-          name: 'Author',
-          value: `[${Bastion.package.author}](${Bastion.package.authorUrl})`,
-          inline: true
+    message.channel.send({
+      embed: {
+        color: Bastion.colors.BLUE,
+        author: {
+          name: `Bastion ${Bastion.package.version}`
         },
-        {
-          name: 'Library',
-          value: Bastion.package.library,
-          inline: true
-        },
-        {
-          name: 'BOT ID',
-          value: Bastion.credentials.botId,
-          inline: true
-        },
-        {
-          name: 'Default Prefix',
-          value: Bastion.config.prefix,
-          inline: true
-        },
-        {
-          name: `Owner${Bastion.credentials.ownerId.length > 1 ? 's' : ''}`,
-          value: owners.join('\n'),
-          inline: true
-        },
-        {
-          name: `Owner ID${Bastion.credentials.ownerId.length > 1 ? 's' : ''}`,
-          value: Bastion.credentials.ownerId.join('\n'),
-          inline: true
-        },
-        {
-          name: 'Presence',
-          value: `${Bastion.guilds.size.toHumanString()} Servers\n`
+        url: Bastion.package.url,
+        fields: [
+          {
+            name: 'Author',
+            value: `[${Bastion.package.author}](${Bastion.package.authorUrl})`,
+            inline: true
+          },
+          {
+            name: 'Library',
+            value: Bastion.package.library,
+            inline: true
+          },
+          {
+            name: 'BOT ID',
+            value: Bastion.credentials.botId,
+            inline: true
+          },
+          {
+            name: 'Default Prefix',
+            value: Bastion.config.prefix,
+            inline: true
+          },
+          {
+            name: `Owner${Bastion.credentials.ownerId.length > 1 ? 's' : ''}`,
+            value: owners.join('\n'),
+            inline: true
+          },
+          {
+            name: `Owner ID${Bastion.credentials.ownerId.length > 1 ? 's' : ''}`,
+            value: Bastion.credentials.ownerId.join('\n'),
+            inline: true
+          },
+          {
+            name: 'Presence',
+            value: `${Bastion.guilds.size.toHumanString()} Servers\n`
             + `${Bastion.channels.filter(channel => channel.type === 'text').size.toHumanString()} Text Channels\n`
             + `${Bastion.channels.filter(channel => channel.type === 'voice').size.toHumanString()} Voice Channels`,
-          inline: true
+            inline: true
+          },
+          {
+            name: 'Uptime',
+            value: uptime,
+            inline: true
+          },
+          {
+            name: 'Heap',
+            value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+            inline: true
+          },
+          {
+            name: 'Memory',
+            value: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`,
+            inline: true
+          }
+        ],
+        thumbnail: {
+          url: Bastion.user.displayAvatarURL
         },
-        {
-          name: 'Uptime',
-          value: uptime,
-          inline: true
+        footer: {
+          text: `WebSocket PING: ${parseInt(Bastion.ping)}ms`
         },
-        {
-          name: 'Heap',
-          value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
-          inline: true
-        },
-        {
-          name: 'Memory',
-          value: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`,
-          inline: true
-        }
-      ],
-      thumbnail: {
-        url: Bastion.user.displayAvatarURL
-      },
-      footer: {
-        text: `WebSocket PING: ${parseInt(Bastion.ping)}ms`
-      },
-      timestamp: new Date()
-    }
-  }).catch(e => {
+        timestamp: new Date()
+      }
+    }).catch(e => {
+      Bastion.log.error(e);
+    });
+  }
+  catch (e) {
     Bastion.log.error(e);
-  });
+  }
 };
 
 exports.config = {
