43bdd7135fae33925f48da76fc0cb37cf5ebb488
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-29 09:38:21 +0530

    Removed bastionLog from event emitter

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,10 +8,4 @@ module.exports = guild => {
   guild.client.db.run('INSERT OR IGNORE INTO guildSettings (guildID) VALUES (?)', [ guild.id ]).catch(e => {
     guild.client.log.error(e);
   });
-
-  /**
-   * bastion join/leave log events, if enabled
-   * @fires bastionLog
-   */
-  guild.client.emit('bastionLog', guild.client, 'guildCreate', guild);
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -8,10 +8,4 @@ module.exports = guild => {
   guild.client.db.run(`DELETE FROM guildSettings WHERE guildID=${guild.id}`).catch(e => {
     guild.client.log.error(e);
   });
-
-  /**
-   * bastion join/leave log events, if enabled
-   * @fires bastionLog
-   */
-  guild.client.emit('bastionLog', guild.client, 'guildDelete', guild);
 };
