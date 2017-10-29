4df0f22501c607edaefa0cc8352d5f17148ad6f0
Sankarsan Kampa <me@sankarsankampa.com>
2017-10-29 09:12:20 +0530

    Added Bastion's Webhook handler

---
+++ handlers/webhookHandler.js
@@ -0,0 +1,48 @@
+/**
+ * @file webhookHandler
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+const Discord = require('discord.js');
+
+module.exports = class WebhookHandler {
+  /**
+   * Handles the webhooks sent by Bastion.
+   * @constructor
+   * @param {Object} webhooks The webhooks object in the `credentials.json` file.
+   */
+  constructor(webhooks) {
+    if (!webhooks) return null;
+
+    for (let hook of Object.keys(webhooks)) {
+      let webhookCredentials = webhooks[hook].split('/').slice(-2);
+      this[hook] = {
+        url: webhooks[hook],
+        id: webhookCredentials[0],
+        token: webhookCredentials[1]
+      };
+      this[hook].client = new Discord.WebhookClient(this[hook].id, this[hook].token);
+    }
+  }
+
+  /**
+   * Sends the specified message as a webhook to the specified webhook channel.
+   * @function send
+   * @param {String} webhook The name of the webhook where the message is to be sent.
+   * @param {String|Number|Object} content The message content to send with the webhook. A string, number or Discord.js embed object.
+   * @returns {void}
+   */
+  send(webhook, content) {
+    if (webhook === 'bastionLog') {
+      if (typeof content === 'object' && !(content instanceof Array)) {
+        content = {
+          embeds: [ content ]
+        };
+      }
+      if (content) {
+        this.bastionLog.client.send(content);
+      }
+    }
+  }
+};
