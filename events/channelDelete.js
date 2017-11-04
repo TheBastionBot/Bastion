d1a2a981840f43de54b4224ea944664577d944b1
Sankarsan Kampa <me@sankarsankampa.com>
2017-11-04 08:23:38 +0530

    Added channelDelete log code to event listner

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -4,10 +4,47 @@
  * @license MIT
  */
 
-module.exports = channel => {
+module.exports = async channel => {
   if (!channel.guild) return;
 
-  channel.client.emit('serverLog', channel.client, channel.guild, 'channelDelete', {
-    channel: channel
-  });
+  try {
+    let guildSettings = await channel.client.db.get(`SELECT log FROM guildSettings WHERE guildID=${channel.guild.id}`);
+    if (!guildSettings || !guildSettings.log) return;
+
+    let logChannel = channel.guild.channels.get(guildSettings.log);
+    if (!logChannel) return;
+
+    let title = 'Channel Deleted';
+    if (channel.type === 'text') {
+      title = 'Text Channel Deleted';
+    }
+    else if (channel.type === 'voice') {
+      title = 'Voice Channel Deleted';
+    }
+
+    logChannel.send({
+      embed: {
+        color: channel.client.colors.RED,
+        title: title,
+        fields: [
+          {
+            name: 'Channel Name',
+            value: channel.name,
+            inline: true
+          },
+          {
+            name: 'Channel ID',
+            value: channel.id,
+            inline: true
+          }
+        ],
+        timestamp: new Date()
+      }
+    }).catch(e => {
+      channel.client.log.error(e);
+    });
+  }
+  catch (e) {
+    channel.client.log.error(e);
+  }
 };
