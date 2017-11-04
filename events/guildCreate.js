25f40b28bd890b66b9d954e51a2c138bda570b26
Sankarsan Kampa <me@sankarsankampa.com>
2017-11-04 15:50:09 +0530

    Implemented bastionLog to webhook

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,4 +8,35 @@ module.exports = guild => {
   guild.client.db.run('INSERT OR IGNORE INTO guildSettings (guildID) VALUES (?)', [ guild.id ]).catch(e => {
     guild.client.log.error(e);
   });
+
+  guild.client.webhook.send('bastionLog', {
+    color: guild.client.colors.GREEN,
+    title: guild.client.strings.events(guild.language, 'guildCreate'),
+    fields: [
+      {
+        name: 'Server Name',
+        value: guild.name,
+        inline: true
+      },
+      {
+        name: 'Server ID',
+        value: guild.id,
+        inline: true
+      },
+      {
+        name: 'Server Owner',
+        value: guild.owner.user.tag,
+        inline: true
+      },
+      {
+        name: 'Server Owner ID',
+        value: guild.ownerID,
+        inline: true
+      }
+    ],
+    thumbnail: {
+      url: guild.iconURL || 'https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png'
+    },
+    timestamp: new Date()
+  });
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -8,4 +8,35 @@ module.exports = guild => {
   guild.client.db.run(`DELETE FROM guildSettings WHERE guildID=${guild.id}`).catch(e => {
     guild.client.log.error(e);
   });
+
+  guild.client.webhook.send('bastionLog', {
+    color: guild.client.colors.RED,
+    title: guild.client.strings.events(guild.language, 'guildDelete'),
+    fields: [
+      {
+        name: 'Server Name',
+        value: guild.name,
+        inline: true
+      },
+      {
+        name: 'Server ID',
+        value: guild.id,
+        inline: true
+      },
+      {
+        name: 'Server Owner',
+        value: guild.owner.user.tag,
+        inline: true
+      },
+      {
+        name: 'Server Owner ID',
+        value: guild.ownerID,
+        inline: true
+      }
+    ],
+    thumbnail: {
+      url: guild.iconURL || 'https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png'
+    },
+    timestamp: new Date()
+  });
 };
