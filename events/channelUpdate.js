274ee0a76266d54ef36b9a6d411473bc1375d1fa
Sankarsan Kampa <me@sankarsankampa.com>
2017-11-04 08:23:59 +0530

    Added channelDelete log code to event listner

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -4,12 +4,53 @@
  * @license MIT
  */
 
-module.exports = (oldChannel, newChannel) => {
+module.exports = async (oldChannel, newChannel) => {
   if (!oldChannel.guild) return;
   if (oldChannel.name === newChannel.name) return;
 
-  newChannel.client.emit('serverLog', newChannel.client, newChannel.guild, 'channelUpdate', {
-    oldChannel: oldChannel,
-    newChannel: newChannel
-  });
+  try {
+    let guildSettings = await newChannel.client.db.get(`SELECT log FROM guildSettings WHERE guildID=${newChannel.guild.id}`);
+    if (!guildSettings || !guildSettings.log) return;
+
+    let logChannel = newChannel.guild.channels.get(guildSettings.log);
+    if (!logChannel) return;
+
+    let title = 'Channel Renamed';
+    if (newChannel.type === 'text') {
+      title = 'Text Channel Renamed';
+    }
+    else if (newChannel.type === 'voice') {
+      title = 'Voice Channel Renamed';
+    }
+
+    logChannel.send({
+      embed: {
+        color: newChannel.client.colors.ORANGE,
+        title: title,
+        fields: [
+          {
+            name: 'Old Channel Name',
+            value: oldChannel.name,
+            inline: true
+          },
+          {
+            name: 'New Channel Name',
+            value: newChannel.name,
+            inline: true
+          },
+          {
+            name: 'Channel ID',
+            value: newChannel.id,
+            inline: true
+          }
+        ],
+        timestamp: new Date()
+      }
+    }).catch(e => {
+      newChannel.client.log.error(e);
+    });
+  }
+  catch (e) {
+    newChannel.client.log.error(e);
+  }
 };
