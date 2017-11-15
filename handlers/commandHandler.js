661f82fa68b9fa37cb7b51ee57ba8b0010519786
Sankarsan Kampa <me@sankarsankampa.com>
2017-11-15 18:13:04 +0530

    Fixed a typo

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -156,7 +156,7 @@ module.exports = async message => {
 
     // Checks if Bastion has the required permission
     if (cmd.help.botPermission) {
-      if (!message.channel.permissionsFor(message.guild.me).has(this.help.botPermission)) {
+      if (!message.channel.permissionsFor(message.guild.me).has(cmd.help.botPermission)) {
         /**
         * Bastion has missing permissions.
         * @fires bastionMissingPermissions
