33a6822843430d1a03d60e2fd1b7449f43c934a7
Sankarsan Kampa <me@sankarsankampa.com>
2017-11-23 02:07:29 +0530

    Improvements to some RegEx

--- modules/administration/createEmoji.js
+++ modules/administration/createEmoji.js
@@ -13,7 +13,7 @@ exports.run = async (Bastion, message, args) => {
     return Bastion.emit('userMissingPermissions', this.help.userPermission);
   }
 
-  if (!args.url || !/^(https?:\/\/)((([a-z0-9]{1,})?(-?)+[a-z0-9]{1,})(\.))+([a-z]{1,63})\/((([a-z0-9_\-~#%])+\/)+)?([a-z0-9_\-~#%]+)\.(jpg|jpeg|gif|png|bmp)$/i.test(args.url) || !args.name) {
+  if (!args.url || !/^(https?:\/\/)((([-a-z0-9]{1,})?(-?)+[-a-z0-9]{1,})(\.))+([a-z]{1,63})\/((([a-z0-9._\-~#%])+\/)+)?([a-z0-9._\-~#%]+)\.(jpg|jpeg|gif|png|bmp)$/i.test(args.url) || !args.name) {
     /**
      * The command was ran with invalid parameters.
      * @fires commandUsage
