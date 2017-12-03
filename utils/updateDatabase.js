bc7fdf24c05411e6ce55ba3abd88ffc1cf0a80fc
Sankarsan Kampa <me@sankarsankampa.com>
2017-12-03 12:56:55 +0530

    Fixed some database updater issues

--- utils/updateDatabase.js
+++ utils/updateDatabase.js
@@ -8,8 +8,8 @@ module.exports = async db => {
   await db.get('SELECT language FROM guildSettings').catch(() => {
     db.run('ALTER TABLE guildSettings ADD language TEXT DEFAULT \'en\'').catch(() => {});
   });
-  await db.get('SELECT birthdate, location FROM guildSettings').catch(() => {
-    db.run('ALTER TABLE guildSettings ADD birthDate INTEGER, location TEXT').catch(() => {});
+  await db.get('SELECT birthDate, location FROM profiles').catch(() => {
+    db.run('ALTER TABLE profiles ADD birthDate INTEGER location TEXT').catch(() => {});
   });
   await db.run('DROP TABLE IF EXISTS bastionSettings').catch(() => {});
 };
