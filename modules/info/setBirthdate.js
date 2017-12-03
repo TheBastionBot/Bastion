6f60f5041447cbab0897dc907ccd8062fb86e28b
Sankarsan Kampa <me@sankarsankampa.com>
2017-12-03 13:07:25 +0530

    Fixed broken usermention in setBirthdate

--- modules/info/setBirthdate.js
+++ modules/info/setBirthdate.js
@@ -46,7 +46,7 @@ exports.exec = async (Bastion, message, args) => {
   if (!user) {
     return message.channel.send({
       embed: {
-        description: `<@${args.id}> you didn't had a profile yet. I've now created your profile. Now you can use the command again to set your birth date.`
+        description: `<@${message.author.id}> you didn't had a profile yet. I've now created your profile. Now you can use the command again to set your birth date.`
       }
     }).catch(e => {
       Bastion.log.error(e);
