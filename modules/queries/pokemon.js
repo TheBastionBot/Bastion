1a30457e5eac0347d9d222eff67636294cb620c5
Sankarsan Kampa <me@sankarsankampa.com>
2017-12-27 04:19:36 +0530

    Better error handling on pokemon command

--- modules/queries/pokemon.js
+++ modules/queries/pokemon.js
@@ -11,103 +11,117 @@ const pokedex = new Pokedex({
 });
 
 exports.exec = async (Bastion, message, args) => {
-  if (!args.number) {
-    /**
-     * The command was ran with invalid parameters.
-     * @fires commandUsage
-     */
-    return Bastion.emit('commandUsage', message, this.help);
-  }
-
-  let pokemon = await pokedex.getPokemonByNumber(args.number);
-  pokemon = pokemon[0];
-
-  let fields = [
-    {
-      name: 'Pokédex Number',
-      value: pokemon.number,
-      inline: true
-    },
-    {
-      name: 'Species',
-      value: pokemon.species,
-      inline: true
-    },
-    {
-      name: 'Types',
-      value: pokemon.types.join(', '),
-      inline: true
-    },
-    {
-      name: 'Abilities',
-      value: `Normal: ${pokemon.abilities.normal.join(', ') || '-'}\nHidden: ${pokemon.abilities.hidden.join(', ') || '-'}`,
-      inline: true
-    },
-    {
-      name: 'Egg Groups',
-      value: pokemon.eggGroups.join(', '),
-      inline: true
-    },
-    {
-      name: 'Gender',
-      value: pokemon.gender.length ? `Male: ${pokemon.gender[0]}%\nFemale: ${pokemon.gender[1]}%` : 'Genderless',
-      inline: true
-    },
-    {
-      name: 'Height',
-      value: pokemon.height,
-      inline: true
-    },
-    {
-      name: 'Weight',
-      value: pokemon.weight,
-      inline: true
-    },
-    {
-      name: 'Evolution Line',
-      value: pokemon.family.evolutionLine.join(' -> ')
+  try {
+    if (!args.number) {
+      /**
+      * The command was ran with invalid parameters.
+      * @fires commandUsage
+      */
+      return Bastion.emit('commandUsage', message, this.help);
     }
-  ];
 
-  let note = '';
-  if (pokemon.starter) {
-    note = note.concat('Is a starter pokemon\n');
-  }
-  if (pokemon.legendary) {
-    note = note.concat('Is a legendary pokemon\n');
-  }
-  if (pokemon.mythical) {
-    note = note.concat('Is a mythical pokemon\n');
-  }
-  if (pokemon.ultraBeast) {
-    note = note.concat('Is an ultra beast\n');
-  }
-  if (pokemon.mega) {
-    note = note.concat('Can mega evolve\n');
-  }
+    let pokemon = await pokedex.getPokemonByNumber(args.number);
+    pokemon = pokemon[0];
 
-  fields.push({
-    name: 'Notes',
-    value: note.length ? note : '-'
-  });
-
-  message.channel.send({
-    embed: {
-      color: Bastion.colors.BLUE,
-      title: pokemon.name,
-      description: `Discovered in generation ${pokemon.gen}`,
-      fields: fields,
-      thumbnail: {
-        url: pokemon.sprite
+    let fields = [
+      {
+        name: 'Pokédex Number',
+        value: pokemon.number,
+        inline: true
       },
-      footer: {
-        icon_url: 'https://pokedevs.bastionbot.org/favicon.png',
-        text: 'Powered by Pokedex API by PokéDevs'
+      {
+        name: 'Species',
+        value: pokemon.species,
+        inline: true
+      },
+      {
+        name: 'Types',
+        value: pokemon.types.join(', '),
+        inline: true
+      },
+      {
+        name: 'Abilities',
+        value: `Normal: ${pokemon.abilities.normal.join(', ') || '-'}\nHidden: ${pokemon.abilities.hidden.join(', ') || '-'}`,
+        inline: true
+      },
+      {
+        name: 'Egg Groups',
+        value: pokemon.eggGroups.join(', '),
+        inline: true
+      },
+      {
+        name: 'Gender',
+        value: pokemon.gender.length ? `Male: ${pokemon.gender[0]}%\nFemale: ${pokemon.gender[1]}%` : 'Genderless',
+        inline: true
+      },
+      {
+        name: 'Height',
+        value: pokemon.height,
+        inline: true
+      },
+      {
+        name: 'Weight',
+        value: pokemon.weight,
+        inline: true
+      },
+      {
+        name: 'Evolution Line',
+        value: pokemon.family.evolutionLine.join(' -> ')
       }
+    ];
+
+    let note = '';
+    if (pokemon.starter) {
+      note = note.concat('Is a starter pokemon\n');
     }
-  }).catch(e => {
+    if (pokemon.legendary) {
+      note = note.concat('Is a legendary pokemon\n');
+    }
+    if (pokemon.mythical) {
+      note = note.concat('Is a mythical pokemon\n');
+    }
+    if (pokemon.ultraBeast) {
+      note = note.concat('Is an ultra beast\n');
+    }
+    if (pokemon.mega) {
+      note = note.concat('Can mega evolve\n');
+    }
+
+    fields.push({
+      name: 'Notes',
+      value: note.length ? note : '-'
+    });
+
+    message.channel.send({
+      embed: {
+        color: Bastion.colors.BLUE,
+        title: pokemon.name,
+        description: `Discovered in generation ${pokemon.gen}`,
+        fields: fields,
+        thumbnail: {
+          url: pokemon.sprite
+        },
+        footer: {
+          icon_url: 'https://pokedevs.bastionbot.org/favicon.png',
+          text: 'Powered by Pokedex API by PokéDevs'
+        }
+      }
+    }).catch(e => {
+      Bastion.log.error(e);
+    });
+  }
+  catch (e) {
+    if (e.statusCode) {
+      if (e.statusCode === 404) {
+        return Bastion.emit('error', e.error.error, e.error.message.replace('You should head home: https://pokedevs.bastionbot.org', ''), message.channel);
+      }
+      else if (e.statusCode === 429) {
+        return Bastion.emit('error', e.error.error, e.error.message, message.channel);
+      }
+      return Bastion.emit('error', e.statusCode, e.message, message.channel);
+    }
     Bastion.log.error(e);
-  });
+  }
 };
 
 exports.config = {
