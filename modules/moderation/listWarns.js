36d54caaf441c951c89a54b29b56025ccc199e04
Sankarsan Kampa <me@sankarsankampa.com>
2017-12-30 15:50:12 +0000

    Fixed listWarns command - errored when warned user had left the server

--- modules/moderation/listWarns.js
+++ modules/moderation/listWarns.js
@@ -19,15 +19,24 @@ exports.exec = async (Bastion, message) => {
 
     let warnedUsers = [];
     for (let userID of Object.keys(message.guild.warns)) {
-      let member = await message.guild.fetchMember(userID);
-      warnedUsers.push(`${member.user.tag} - ${message.guild.warns[userID]} Warnings`);
+      let member = await message.guild.fetchMember(userID).catch((e) => {
+        if (e.code !== 10007) {
+          Bastion.log.error(e);
+        }
+      });
+      if (member) {
+        warnedUsers.push(`${member.user.tag} - ${message.guild.warns[userID]} Warnings`);
+      }
+      else {
+        // TODO: Remove userID from warn list
+      }
     }
 
     message.channel.send({
       embed: {
         color: Bastion.colors.ORANGE,
         title: 'Warning List',
-        description: warnedUsers.join('\n')
+        description: warnedUsers.length ? warnedUsers.join('\n') : 'No one has been warned yet.'
       }
     }).catch(e => {
       Bastion.log.error(e);
