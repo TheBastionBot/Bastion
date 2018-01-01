ab2e9d3f46230ecb14248420b85b46790ae6f225
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-01 17:13:00 +0000

    Improvements to setTopic command * Simplified success message * Code improvements

--- modules/administration/setTopic.js
+++ modules/administration/setTopic.js
@@ -5,57 +5,50 @@
  */
 
 exports.exec = async (Bastion, message, args) => {
-  let channel = message.mentions.channels.first();
-  let topic;
-  if (!channel) {
-    channel = message.channel;
-    topic = args.join(' ');
-  }
-  else {
-    topic = args.slice(1).join(' ').trim();
-  }
-
-  if (!channel.permissionsFor(message.member).has(this.help.userTextPermission)) {
-    /**
-     * User has missing permissions.
-     * @fires userMissingPermissions
-     */
-    return Bastion.emit('userMissingPermissions', this.help.userTextPermission);
-  }
-  if (!channel.permissionsFor(message.guild.me).has(this.help.botPermission)) {
-    /**
-     * Bastion has missing permissions.
-     * @fires bastionMissingPermissions
-     */
-    return Bastion.emit('bastionMissingPermissions', this.help.botPermission, message);
-  }
-
-  let color = Bastion.colors.GREEN;
-  let title = 'Channel Topic Set';
-  if (topic.length < 2) {
-    topic = ' ';
-    title = 'Channel Topic Removed';
-    color = Bastion.colors.RED;
-  }
-
   try {
+    let channel = message.mentions.channels.first();
+    let topic;
+    if (!channel) {
+      channel = message.channel;
+      topic = args.join(' ');
+    }
+    else {
+      topic = args.slice(1).join(' ').trim();
+    }
+
+    if (!channel.permissionsFor(message.member).has(this.help.userTextPermission)) {
+      /**
+      * User has missing permissions.
+      * @fires userMissingPermissions
+      */
+      return Bastion.emit('userMissingPermissions', this.help.userTextPermission);
+    }
+    if (!channel.permissionsFor(message.guild.me).has(this.help.botPermission)) {
+      /**
+      * Bastion has missing permissions.
+      * @fires bastionMissingPermissions
+      */
+      return Bastion.emit('bastionMissingPermissions', this.help.botPermission, message);
+    }
+
+    let color;
+    let action;
+    if (topic.length < 2) {
+      topic = ' ';
+      action = 'removed';
+      color = Bastion.colors.RED;
+    }
+    else {
+      action = 'changed';
+      color = Bastion.colors.ORANGE;
+    }
+
     await channel.setTopic(topic);
+
     await message.channel.send({
       embed: {
         color: color,
-        title: title,
-        fields: [
-          {
-            name: 'Channel Name',
-            value: `#${channel.name}`,
-            inline: true
-          },
-          {
-            name: 'Topic',
-            value: channel.topic.length > 1 ? channel.topic : '-',
-            inline: true
-          }
-        ]
+        description: `${message.author.tag} ${action} the topic of the text channel **${channel.name}** ${channel.topic.length > 1 ? `to\n\`\`\`${channel.topic}\`\`\`` : ''}`
       }
     });
   }
