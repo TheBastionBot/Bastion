46960a3ea5c67717306939f99440f0d2ab97052d
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-01 16:18:15 +0000

    Improvements to createChannel command * Won't force use of - instead of space character for voice channels * Simplified success message

--- modules/administration/createChannel.js
+++ modules/administration/createChannel.js
@@ -5,51 +5,43 @@
  */
 
 exports.exec = async (Bastion, message, args) => {
-  if (!args.name) {
-    /**
-     * The command was ran with invalid parameters.
-     * @fires commandUsage
-     */
-    return Bastion.emit('commandUsage', message, this.help);
-  }
-
-  let minLength = 2, maxLength = 100;
-  args.name = args.name.join('-');
-  if (args.name.length < minLength || args.name.length > maxLength) {
-    /**
-     * Error condition is encountered.
-     * @fires error
-     */
-    return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'invalidInput'), Bastion.strings.error(message.guild.language, 'channelNameLength', true, minLength, maxLength), message.channel);
-  }
-
-  let channelType = 'text';
-  if (!args.text && args.voice) {
-    channelType = 'voice';
-  }
-  else {
-    args.name = args.name.replace(' ', '-');
-  }
-
   try {
+    if (!args.name) {
+      /**
+      * The command was ran with invalid parameters.
+      * @fires commandUsage
+      */
+      return Bastion.emit('commandUsage', message, this.help);
+    }
+
+    let minLength = 2, maxLength = 100;
+    args.name = args.name.join(' ');
+    if (args.name.length < minLength || args.name.length > maxLength) {
+      /**
+      * Error condition is encountered.
+      * @fires error
+      */
+      return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'invalidInput'), Bastion.strings.error(message.guild.language, 'channelNameLength', true, minLength, maxLength), message.channel);
+    }
+
+    let channelType;
+    if (!args.text && args.voice) {
+      channelType = 'voice';
+    }
+    else {
+      channelType = 'text';
+      args.name = args.name.replace(' ', '-');
+    }
+
     let channel = await message.guild.createChannel(args.name, channelType);
 
     await message.channel.send({
       embed: {
         color: Bastion.colors.GREEN,
-        title: `${channelType.charAt(0).toUpperCase() + channelType.substr(1)} Channel Created`,
-        fields: [
-          {
-            name: 'Channel Name',
-            value: channel.name,
-            inline: true
-          },
-          {
-            name: 'Channel ID',
-            value: channel.id,
-            inline: true
-          }
-        ]
+        description: `${message.author.tag} created a ${channelType} channel **${channel.name}**`,
+        footer: {
+          text: `ID: ${channel.id}`
+        }
       }
     });
   }
