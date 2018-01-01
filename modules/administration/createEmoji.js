98d69782a263c0310bc6f61d73f8441e8087b5f2
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-01 16:29:36 +0000

    Improvements to createEmoji command * Simplified success message * Code improvements

--- modules/administration/createEmoji.js
+++ modules/administration/createEmoji.js
@@ -5,36 +5,21 @@
  */
 
 exports.exec = async (Bastion, message, args) => {
-  if (!args.url || !/^(https?:\/\/)((([-a-z0-9]{1,})?(-?)+[-a-z0-9]{1,})(\.))+([a-z]{1,63})\/((([a-z0-9._\-~#%])+\/)+)?([a-z0-9._\-~#%]+)\.(jpg|jpeg|gif|png|bmp)$/i.test(args.url) || !args.name) {
-    /**
-     * The command was ran with invalid parameters.
-     * @fires commandUsage
-     */
-    return Bastion.emit('commandUsage', message, this.help);
-  }
-
   try {
+    if (!args.url || !/^(https?:\/\/)((([-a-z0-9]{1,})?(-?)+[-a-z0-9]{1,})(\.))+([a-z]{1,63})\/((([a-z0-9._\-~#%])+\/)+)?([a-z0-9._\-~#%]+)\.(jpg|jpeg|gif|png|bmp)$/i.test(args.url) || !args.name) {
+      /**
+      * The command was ran with invalid parameters.
+      * @fires commandUsage
+      */
+      return Bastion.emit('commandUsage', message, this.help);
+    }
+
     let emoji = await message.guild.createEmoji(args.url, args.name.join('_'));
 
     await message.channel.send({
       embed: {
         color: Bastion.colors.GREEN,
-        title: 'Emoji Created',
-        fields: [
-          {
-            name: 'Emoji Name',
-            value: emoji.name,
-            inline: true
-          },
-          {
-            name: 'Creator',
-            value: message.author.tag,
-            inline: true
-          }
-        ],
-        thumbnail: {
-          url: emoji.url
-        }
+        description: `${message.author.tag} created the emoji **${emoji.name}**`
       }
     });
   }
