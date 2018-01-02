ed90be856d00fe2992c3d5b574bfdd12cc783f1a
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-02 17:33:20 +0000

    Added genre & rating to tvShow command

--- modules/queries/tvShow.js
+++ modules/queries/tvShow.js
@@ -45,6 +45,9 @@ exports.exec = (Bastion, message, args) => {
           return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'notFound'), Bastion.strings.error(message.guild.language, 'notFound', true, 'TV show'), message.channel);
         }
 
+        // Hard coded genre IDs because they are not likely to change for v3 and dynamically getting them would mean sending another request, since it's a seperate endpoint.
+        let genre_list = { '10759': 'Action & Adventure', '16': 'Animation', '35': 'Comedy', '80': 'Crime', '99': 'Documentary', '18': 'Drama', '10751': 'Family', '10762': 'Kids', '9648': 'Mystery', '10763': 'News', '10764': 'Reality', '10765': 'Sci-Fi & Fantasy', '10766': 'Soap', '10767': 'Talk', '10768': 'War & Politics', '37': 'Western' };
+
         message.channel.send({
           embed: {
             color: Bastion.colors.BLUE,
@@ -53,8 +56,18 @@ exports.exec = (Bastion, message, args) => {
             description: tvShow.overview,
             fields: [
               {
+                name: 'Genre',
+                value: tvShow.genre_ids.map(id => genre_list[id]).join('\n'),
+                inline: true
+              },
+              {
                 name: 'Language',
                 value: tvShow.original_language.toUpperCase(),
+                inline: true
+              },
+              {
+                name: 'Rating',
+                value: `${tvShow.vote_average}`,
                 inline: true
               },
               {
