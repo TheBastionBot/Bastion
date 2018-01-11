d04685b0219ead2509c0eaa633e7d90fb7ecaf2a
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-01-11 12:00:16 +0000

    Improvements to tf2 command
    * Fixed linting issues
    * Design changes
    * Code improvements

--- modules/game_stats/teamFortress2.js
+++ modules/game_stats/teamFortress2.js
@@ -4,137 +4,130 @@
  * @license MIT
  */
 
-const TF2 = require('gamedig');
+const source = require('gamedig');
 
-exports.exec = (Bastion, message, args) => {
-  if (args.length < 1 || !/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9]))?$/.test(args = args[0])) {
-    /**
-     * The command was ran with invalid parameters.
-     * @fires commandUsage
-     */
-    return Bastion.emit('commandUsage', message, this.help);
-  }
+exports.exec = async (Bastion, message, args) => {
+  try {
+    if (!/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9]))?$/.test(args.address)) {
+      /**
+      * The command was ran with invalid parameters.
+      * @fires commandUsage
+      */
+      return Bastion.emit('commandUsage', message, this.help);
+    }
 
-  args = args.split(':');
-  let host = args[0];
+    args.address = args.address.split(':');
+    let host = args.address[0];
 
-  if (host === '127.0.0.1') {
-    return message.channel.send({
-      embed: {
-        description: 'There is no place like `127.0.0.1`'
-      }
+    if (host === '127.0.0.1') {
+      return message.channel.send({
+        embed: {
+          description: 'There is no place like `127.0.0.1`'
+        }
+      });
+    }
+
+    let port;
+    if (args.address[1]) {
+      port = parseInt(args.address[1]);
+    }
+    else {
+      port = 27015;
+    }
+
+    let data = await source.query({
+      type: 'tf2',
+      host: host,
+      port: port
     });
-  }
 
-  let port;
-  if (args[1]) {
-    port = parseInt(args[1]);
-  }
-  else {
-    port = 27015;
-  }
-
-  TF2.query({
-    type: 'tf2',
-    host: host,
-    port: port
-  }).then(data => {
     let stats = [
       {
-        name: 'Address',
-        value: '`' + host + ':' + port + '`',
+        name: 'Server IP',
+        value: `\`${host}:${port}\``,
         inline: true
       },
       {
         name: 'Players',
-        value: '`' + data.players.length + '/' + data.maxplayers + '`',
+        value: `${data.players.length}/${data.maxplayers}`,
         inline: true
       },
       {
         name: 'Map',
-        value: '`' + data.map + '`',
-        inline: true
+        value: data.map
       }
     ];
 
     if (data.players.length > 0) {
       let players = [];
       let scores = [];
-      let times = [];
+      let playtimes = [];
       for (let i = 0; i < data.players.length; i++) {
-        players.push(data.players[i].name.substring(0, 12));
+        players.push(data.players[i].name);
       }
       for (let i = 0; i < data.players.length; i++) {
         scores.push(data.players[i].score);
       }
       for (let i = 0; i < data.players.length; i++) {
-        times.push(new Date(data.players[i].time * 1000).toISOString().substr(11, 8));
+        playtimes.push(`${parseInt(data.players[i].time)}s`);
       }
       stats.push(
         {
-          name: 'Player Name',
-          value: '```http\n' + players.join('\n') + '\n```',
+          name: 'Player',
+          value: `\`\`\`http\n${players.join('\n')}\`\`\``,
           inline: true
         },
         {
           name: 'Score',
-          value: '```http\n' + scores.join('\n') + '\n```',
+          value: `\`\`\`http\n${scores.join('\n')}\`\`\``,
           inline: true
         },
         {
-          name: 'Time',
-          value: '```http\n' + times.join('\n') + '\n```',
+          name: 'Playtime',
+          value: `\`\`\`http\n${playtimes.join('\n')}\`\`\``,
           inline: true
+        },
+        {
+          name: 'Join',
+          value: `<steam://connect/${host}:${port}>`
         }
-        
       );
     }
-    
-    stats.push(
-      {
-        name: 'Join Server',
-        value: 'steam://connect/' + host + ':' + port,
-        inline: false
-      }  
-    );
-    
-    let lock = data.password;
-    let lock_icon = '';
-    if (lock == true) {
-      lock = 'Password required to join server';
-      lock_icon = 'https://resources.bastionbot.org/images/lock.png';
-    } 
-    else {
-       lock = '';
-       lock_icon = '';
+
+    let footer;
+    if (data.password) {
+      footer = {
+        text: 'Private Server',
+        icon_url: 'https://resources.bastionbot.org/images/lock.png'
+      };
     }
-    
+
     message.channel.send({
       embed: {
         color: Bastion.colors.BLUE,
         title: data.name,
-        description: '[' + data.raw.game + '](https://store.steampowered.com/app/' + data.raw.gameid + '/)',
+        description: '[Team Fortress 2](https://store.steampowered.com/app/440/)',
         fields: stats,
-        footer: {
-          text: lock,
-          icon_url: lock_icon
-        }
+        footer: footer
       }
     }).catch(e => {
       Bastion.log.error(e);
     });
-  }).catch(() => {
-    /**
-     * Error condition is encountered.
-     * @fires error
-     */
-    return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'connection'), Bastion.strings.error(message.guild.language, 'invalidIPPort', true), message.channel);
-  });
+  }
+  catch (e) {
+    if (e.toString() === 'UDP Watchdog Timeout') {
+      return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'connection'), Bastion.strings.error(message.guild.language, 'invalidIPPort', true), message.channel);
+    }
+    Bastion.log.error(e);
+  }
 };
 
 exports.config = {
   aliases: [ 'tf2' ],
-  enabled: true
+  enabled: true,
+  argsDefinitions: [
+    { name: 'address', type: String, defaultOption: true }
+  ]
 };
 
 exports.help = {
@@ -142,6 +135,6 @@ exports.help = {
   botPermission: '',
   userTextPermission: '',
   userVoicePermission: '',
-  usage: 'teamFortress2 <TF2_SERVER_IP>[:PORT]',
-  example: [ 'teamFortress2 139.59.31.129', 'teamFortress2 139.59.31.129:27019' ]
+  usage: 'teamFortress2 <TF2_SERVER_IP:PORT>',
+  example: [ 'teamFortress2 139.59.31.129:27019' ]
 };
