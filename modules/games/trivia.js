83ee289f0efc60ebecdd2a836a171a928b54d2d0
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-18 16:23:02 +0000

    Upgraded to request-promise-native in games module

--- modules/games/trivia.js
+++ modules/games/trivia.js
@@ -4,127 +4,110 @@
  * @license MIT
  */
 
-const request = require('request');
+const request = require('request-promise-native');
 let activeChannels = [];
 
-exports.exec = (Bastion, message) => {
-  if (activeChannels.includes(message.channel.id))  {
-    /**
-     * Error condition is encountered.
-     * @fires error
-     */
-    return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'busy'), Bastion.strings.error(message.guild.language, 'isGameInUse', true, 'trivia'), message.channel);
+exports.exec = async (Bastion, message) => {
+  try {
+    if (activeChannels.includes(message.channel.id))  {
+      /**
+      * Error condition is encountered.
+      * @fires error
+      */
+      return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'busy'), Bastion.strings.error(message.guild.language, 'isGameInUse', true, 'trivia'), message.channel);
+    }
+
+    let options = {
+      method: 'GET',
+      url: 'https://opentdb.com/api.php?amount=1&type=boolean&encode=url3986',
+      json: true
+    };
+    let response = await request(options);
+
+    response = response.results[0];
+
+    let question = await message.channel.send({
+      embed: {
+        color: Bastion.colors.BLUE,
+        title: 'Trivia - True/False',
+        description: decodeURIComponent(response.question),
+        fields: [
+          {
+            name: 'Category',
+            value: decodeURIComponent(response.category),
+            inline: true
+          },
+          {
+            name: 'Difficulty',
+            value: response.difficulty.toTitleCase(),
+            inline: true
+          }
+        ],
+        footer: {
+          text: 'Reply with either True/False within 60 seconds.'
+        }
+      }
+    });
+
+    activeChannels.push(message.channel.id);
+
+    let validAnswers = [
+      'true',
+      'false'
+    ];
+
+    const trivia = message.channel.createMessageCollector(
+      msg => !msg.author.bot && validAnswers.includes(msg.content.toLowerCase()),
+      { maxMatches: 1, time: 60 * 1000 }
+    );
+
+    trivia.on('collect', ans => {
+      let color, description;
+      if (ans.content.toLowerCase() === response.correct_answer.toLowerCase()) {
+        color = Bastion.colors.BLUE;
+        description = `${ans.author.tag} you're absolutely right.`;
+      }
+      else {
+        color = Bastion.colors.RED;
+        description = `Unfortunately, you're wrong ${ans.author.tag}`;
+      }
+
+      message.channel.send({
+        embed: {
+          color: color,
+          description: description
+        }
+      }).catch(e => {
+        Bastion.log.error(e);
+      });
+    });
+
+    trivia.on('end', (answers, reason) => {
+      activeChannels.splice(activeChannels.indexOf(message.channel.id), 1);
+
+      if (reason === 'time') {
+        message.channel.send({
+          embed: {
+            color: Bastion.colors.RED,
+            title: 'Trivia Ended',
+            description: 'Trivia was ended as no one was able to answer within 60 seconds.'
+          }
+        }).then(() => {
+          question.delete().catch(e => {
+            Bastion.log.error(e);
+          });
+        }).catch(e => {
+          Bastion.log.error(e);
+        });
+      }
+    });
   }
-
-  let options = {
-    method: 'GET',
-    url: 'https://opentdb.com/api.php?amount=1&type=boolean&encode=url3986'
-  };
-
-  request(options, async (error, response, body) => {
-    try {
-      if (error) {
-        /**
-        * Error condition is encountered.
-        * @fires error
-        */
-        return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'connection'), Bastion.strings.error(message.guild.language, 'connection', true), message.channel);
-      }
-
-      if (response) {
-        if (response.statusCode === 200) {
-          body = JSON.parse(body);
-          body = body.results[0];
-
-          let question = await message.channel.send({
-            embed: {
-              color: Bastion.colors.BLUE,
-              title: 'Trivia - True/False',
-              description: decodeURIComponent(body.question),
-              fields: [
-                {
-                  name: 'Category',
-                  value: decodeURIComponent(body.category),
-                  inline: true
-                },
-                {
-                  name: 'Difficulty',
-                  value: body.difficulty.toTitleCase(),
-                  inline: true
-                }
-              ],
-              footer: {
-                text: 'Reply with either True/False within 60 seconds.'
-              }
-            }
-          });
-
-          activeChannels.push(message.channel.id);
-
-          let validAnswers = [
-            'true',
-            'false'
-          ];
-
-          const trivia = message.channel.createMessageCollector(
-            msg => !msg.author.bot && validAnswers.includes(msg.content.toLowerCase()),
-            { maxMatches: 1, time: 60 * 1000 }
-          );
-
-          trivia.on('collect', ans => {
-            let color, description;
-            if (ans.content.toLowerCase() === body.correct_answer.toLowerCase()) {
-              color = Bastion.colors.BLUE;
-              description = `${ans.author.tag} you're absolutely right.`;
-            }
-            else {
-              color = Bastion.colors.RED;
-              description = `Unfortunately, you're wrong ${ans.author.tag}`;
-            }
-
-            message.channel.send({
-              embed: {
-                color: color,
-                description: description
-              }
-            }).catch(e => {
-              Bastion.log.error(e);
-            });
-          });
-
-          trivia.on('end', (answers, reason) => {
-            activeChannels.splice(activeChannels.indexOf(message.channel.id), 1);
-
-            if (reason === 'time') {
-              message.channel.send({
-                embed: {
-                  color: Bastion.colors.RED,
-                  title: 'Trivia Ended',
-                  description: 'Trivia was ended as no one was able to answer within 60 seconds.'
-                }
-              }).then(() => {
-                question.delete().catch(e => {
-                  Bastion.log.error(e);
-                });
-              }).catch(e => {
-                Bastion.log.error(e);
-              });
-            }
-          });
-        }
-        else {
-          /**
-          * Error condition is encountered.
-          * @fires error
-          */
-          return Bastion.emit('error', response.statusCode, response.statusMessage, message.channel);
-        }
-      }
+  catch (e) {
+    if (e.response) {
+      return Bastion.emit('error', e.response.statusCode, e.response.statusMessage, message.channel);
     }
-    catch (e) {
-      Bastion.log.error(e);
-    }
-  });
+    Bastion.log.error(e);
+  }
 };
 
 exports.config = {
