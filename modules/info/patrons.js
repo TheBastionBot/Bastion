328e04449b9bb44c4343a70cb0b66e6340270122
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-25 09:24:53 +0000

    Improvements to patrons command * Will now show the list of Patrons of the Patreon creator whose API key is used.

--- modules/info/patrons.js
+++ modules/info/patrons.js
@@ -6,17 +6,25 @@
 
 exports.exec = async (Bastion, message, args) => {
   try {
-    let patrons = await Bastion.functions.getPatrons(754397);
+    let patrons = await Bastion.functions.getPatrons();
     patrons = patrons.map(patron => patron.full_name);
 
     let noOfPages = patrons.length / 50;
     let i = (args.page > 0 && args.page < noOfPages + 1) ? args.page : 1;
     i = i - 1;
 
+    let description;
+    if (Bastion.user.id === '267035345537728512') {
+      description = 'These are the awesome people who continuously support the development of the Bastion bot project, by being my patron, on [Patreon](https://patreon.com/snkrsnkampa).\nIf you want to support the development of Bastion too, [be my Patron](https://patreon.com/bePatron?c=754397)';
+    }
+    else {
+      description = 'These are the awesome people who continuously support us, by being our patron, on Patreon.';
+    }
+
     message.channel.send({
       embed: {
         color: 16345172,
-        description: 'These are the awesome people who continuously support the development of the Bastion Bot project by being my patron on [Patreon](https://patreon.com/snkrsnkampa).',
+        description: description,
         fields: [
           {
             name: 'Patrons',
@@ -24,7 +32,7 @@ exports.exec = async (Bastion, message, args) => {
           }
         ],
         footer: {
-          text: `Page: ${i + 1} of ${noOfPages > parseInt(noOfPages) ? parseInt(noOfPages) + 1 : parseInt(noOfPages)} • https://patreon.com/snkrsnkampa`
+          text: `Page: ${i + 1} of ${noOfPages > parseInt(noOfPages) ? parseInt(noOfPages) + 1 : parseInt(noOfPages)}`
         }
       }
     }).catch(e => {
