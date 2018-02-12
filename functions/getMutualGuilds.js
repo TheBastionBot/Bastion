63d17b62a4d32c6f43cf5e2c24ff42358983aa2f
Sankarsan Kampa <me@sankarsankampa.com>
2018-02-12 11:50:48 +0000

    Fixed getMututalGuilds function

--- functions/getMutualGuilds.js
+++ functions/getMutualGuilds.js
@@ -1,5 +1,5 @@
 module.exports = async (user) => {
-  // Gets the no. of guilds user share with Bastion per shard.
-  let mutualGuilds = await user.client.shard.broadcastEval(`let ids = Array.from(this.guilds.keys()); let count = 0; for (let id of ids) { count += this.guilds.get(id).members.has(${user.id}) ? 1 : 1; }; count;`);
+  // Gets the no. of guilds `user` share with Bastion per shard.
+  let mutualGuilds = await user.client.shard.broadcastEval(`let ids = Array.from(this.guilds.keys()); for (let id of ids) { if (!this.guilds.get(id).members.has('${user.id}')) ids.splice(ids.indexOf(id), 1); }; ids.length;`);
   return mutualGuilds.reduce((sum, val) => sum + val, 0);
 };
