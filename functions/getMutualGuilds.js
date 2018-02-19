126ee4b7e9b7c868a8db2dde7cb78f6df1e3f7c2
Sankarsan Kampa <me@sankarsankampa.com>
2018-02-19 10:54:19 +0000

    Fixed mutual server count in uinfo command; Fixed #132

--- functions/getMutualGuilds.js
+++ functions/getMutualGuilds.js
@@ -1,5 +1,5 @@
 module.exports = async (user) => {
   // Gets the no. of guilds `user` share with Bastion per shard.
-  let mutualGuilds = await user.client.shard.broadcastEval(`let ids = Array.from(this.guilds.keys()); for (let id of ids) { if (!this.guilds.get(id).members.has('${user.id}')) ids.splice(ids.indexOf(id), 1); }; ids.length;`);
+  let mutualGuilds = await user.client.shard.broadcastEval(`let ids = Array.from(this.guilds.keys()), count = 0; for (let id of ids) if (this.guilds.get(id).members.has('${user.id}')) ++count; count;`);
   return mutualGuilds.reduce((sum, val) => sum + val, 0);
 };
