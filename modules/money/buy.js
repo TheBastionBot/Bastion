5a6878065bab577d8a68c70ce5734817c9c5f0f0
Sankarsan Kampa <me@sankarsankampa.com>
2018-02-21 07:20:31 +0000

    Added buy command

---
+++ modules/money/buy.js
@@ -0,0 +1,71 @@
+/**
+ * @file buy command
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license MIT
+ */
+
+exports.exec = async (Bastion, message, args) => {
+  try {
+    if (!args.index) {
+      /**
+      * The command was ran with invalid parameters.
+      * @fires commandUsage
+      */
+      return Bastion.emit('commandUsage', message, this.help);
+    }
+
+    let guildShop = await message.client.db.get(`SELECT custom FROM guildShop WHERE guildID=${message.guild.id}`);
+
+    let itemsInShop;
+    if (guildShop && guildShop.custom) {
+      itemsInShop = await Bastion.functions.decodeString(guildShop.custom);
+      itemsInShop = JSON.parse(itemsInShop);
+    }
+    else {
+      itemsInShop = [];
+    }
+
+    args.index = Math.abs(args.index);
+    args.index = args.index - 1;
+
+    if (args.index > itemsInShop.length) {
+      return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'notFound'), Bastion.strings.error(message.guild.language, 'indexRange', true), message.channel);
+    }
+
+
+    Bastion.emit('userCredit', message.author, itemsInShop[args.index].value);
+
+    if (message.author.id !== message.guild.owner.id) {
+      Bastion.emit('userDebit', message.guild.owner, (0.9) * itemsInShop[args.index].value);
+    }
+
+    message.channel.send({
+      embed: {
+        color: Bastion.colors.GREEN,
+        description: `${message.author.tag} bought **${itemsInShop[args.index].name}** for **${itemsInShop[args.index].value}** Bastion Currencies.`
+      }
+    }).catch(e => {
+      Bastion.log.error(e);
+    });
+  }
+  catch (e) {
+    Bastion.log.error(e);
+  }
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true,
+  argsDefinitions: [
+    { name: 'index', type: Number, defaultOption: true }
+  ]
+};
+
+exports.help = {
+  name: 'buy',
+  botPermission: '',
+  userTextPermission: '',
+  userVoicePermission: '',
+  usage: 'buy <ITEM_INDEX>',
+  example: [ 'buy 3' ]
+};
