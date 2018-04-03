a6363b4cccdc53864d4ed9d27935a071b570f37f
Sankarsan Kampa <me@sankarsankampa.com>
2018-04-03 15:44:14 +0000

    Added support for slow mode ignored channels & roles

--- events/message.js
+++ events/message.js
@@ -54,65 +54,80 @@ module.exports = async message => {
         attributes: [ 'slowMode' ],
         where: {
           guildID: message.guild.id
-        }
+        },
+        include: [
+          {
+            model: message.client.database.models.textChannel,
+            attributes: [ 'channelID', 'ignoreSlowMode' ]
+          },
+          {
+            model: message.client.database.models.role,
+            attributes: [ 'roleID', 'ignoreSlowMode' ]
+          }
+        ]
       });
 
-      if (!message.channel.permissionsFor(message.member) || !message.channel.permissionsFor(message.member).has('MANAGE_ROLES')) {
-        if (guildModel && guildModel.dataValues.slowMode) {
-          if (recentUsers.hasOwnProperty(message.author.id)) {
-            let title, description;
+      let slowModeIgnoredChannels = guildModel.textChannels.length && guildModel.textChannels.filter(model => model.dataValues.ignoreSlowMode).map(model => model.dataValues.channelID);
+      let slowModeIgnoredRoles = guildModel.roles.length && guildModel.roles.filter(model => model.dataValues.ignoreSlowMode).map(model => model.dataValues.roleID);
 
-            ++recentUsers[message.author.id];
-            if (recentUsers[message.author.id] >= 8) {
-              title = 'Warned ya!.';
-              description = `${message.author} you have been muted for 15 minutes.`;
-              await message.channel.overwritePermissions(message.author, {
-                SEND_MESSAGES: false,
-                ADD_REACTIONS: false
-              });
+      if (!slowModeIgnoredChannels.includes(message.channel.id) || !message.member.roles.some(role => slowModeIgnoredRoles.includes(role))) {
+        if (!message.channel.permissionsFor(message.member) || !message.channel.permissionsFor(message.member).has('MANAGE_ROLES')) {
+          if (guildModel && guildModel.dataValues.slowMode) {
+            if (recentUsers.hasOwnProperty(message.author.id)) {
+              let title, description;
+
+              ++recentUsers[message.author.id];
+              if (recentUsers[message.author.id] >= 8) {
+                title = 'Warned ya!.';
+                description = `${message.author} you have been muted for 15 minutes.`;
+                await message.channel.overwritePermissions(message.author, {
+                  SEND_MESSAGES: false,
+                  ADD_REACTIONS: false
+                });
+
+                setTimeout(() => {
+                  let permissionOverwrites = message.channel.permissionOverwrites.get(message.author.id);
+                  if (permissionOverwrites) {
+                    if (permissionOverwrites.deny === 2112) {
+                      permissionOverwrites.delete();
+                    }
+                    else {
+                      message.channel.overwritePermissions(message.author, {
+                        SEND_MESSAGES: null,
+                        ADD_REACTIONS: null
+                      }).catch(e => {
+                        message.client.log.error(e);
+                      });
+                    }
+                  }
+                }, 15 * 60 * 1000);
+              }
+              else if (recentUsers[message.author.id] >= 6) {
+                title = 'Cooldown, dark lord.';
+                description = `${message.author} you are sending messages way too quickly.`;
+              }
+              else if (recentUsers[message.author.id] >= 4) {
+                title = 'Woah There. Way too Spicy.';
+                description = `${message.author} you are sending messages too quickly.`;
+              }
+
+              if (title && description) {
+                await message.channel.send({
+                  embed: {
+                    color: message.client.colors.ORANGE,
+                    title: title,
+                    description: description
+                  }
+                });
+              }
+            }
+            else {
+              recentUsers[message.author.id] = 1;
 
               setTimeout(() => {
-                let permissionOverwrites = message.channel.permissionOverwrites.get(message.author.id);
-                if (permissionOverwrites) {
-                  if (permissionOverwrites.deny === 2112) {
-                    permissionOverwrites.delete();
-                  }
-                  else {
-                    message.channel.overwritePermissions(message.author, {
-                      SEND_MESSAGES: null,
-                      ADD_REACTIONS: null
-                    }).catch(e => {
-                      message.client.log.error(e);
-                    });
-                  }
-                }
-              }, 15 * 60 * 1000);
+                delete recentUsers[message.author.id];
+              }, 5 * 1000);
             }
-            else if (recentUsers[message.author.id] >= 6) {
-              title = 'Cooldown, dark lord.';
-              description = `${message.author} you are sending messages way too quickly.`;
-            }
-            else if (recentUsers[message.author.id] >= 4) {
-              title = 'Woah There. Way too Spicy.';
-              description = `${message.author} you are sending messages too quickly.`;
-            }
-
-            if (title && description) {
-              await message.channel.send({
-                embed: {
-                  color: message.client.colors.ORANGE,
-                  title: title,
-                  description: description
-                }
-              });
-            }
-          }
-          else {
-            recentUsers[message.author.id] = 1;
-
-            setTimeout(() => {
-              delete recentUsers[message.author.id];
-            }, 5 * 1000);
           }
         }
       }
