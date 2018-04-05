bf04c56bb7a366cf5cbb38c49d3fed46dd5cf011
Sankarsan Kampa <me@sankarsankampa.com>
2018-04-05 03:38:38 +0000

    If streaming activity & music status is set in the config, Bastion should use the streaming activity after music is over.

--- modules/music/play.js
+++ modules/music/play.js
@@ -274,7 +274,10 @@ function startStreamDispatcher(guild, connection) {
   if ((connection.channel && connection.channel.members.size < 2) || guild.music.songs[0] === undefined) {
     if (guild.client.config.music && guild.client.config.music.status) {
       guild.client.user.setActivity(typeof guild.client.config.game.name === 'string' ? guild.client.config.game.name : guild.client.config.game.name.length ? guild.client.config.game.name[0] : null,
-        { type: guild.client.config.game.type }
+        {
+          type: guild.client.config.game.type,
+          url: guild.client.config.game.url.trim().length ? guild.client.config.game.url : null
+        }
       );
     }
 
