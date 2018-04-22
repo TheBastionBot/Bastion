a91efec093c01fe696845ae9cc95e22a8959a351
Sankarsan Kampa <me@sankarsankampa.com>
2018-04-22 14:30:45 +0000

    giveXP can now be used by guild owners

--- modules/money/giveXP.js
+++ modules/money/giveXP.js
@@ -59,7 +59,7 @@ exports.exec = async (Bastion, message, args) => {
 exports.config = {
   aliases: [],
   enabled: true,
-  ownerOnly: true,
+  guildOwnerOnly: true,
   argsDefinitions: [
     { name: 'id', type: String, defaultOption: true },
     { name: 'points', alias: 'n', type: String }
