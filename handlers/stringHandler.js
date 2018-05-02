e7b1493cf4e1235d1af5716ff7d6259b36f874e6
Sankarsan Kampa <me@sankarsankampa.com>
2018-05-02 16:47:02 +0000

    stringHandler: added event method to get event strings

--- handlers/stringHandler.js
+++ handlers/stringHandler.js
@@ -1,5 +1,17 @@
 const Locke = require('locke');
 
-const StringHandler = class StringHandler extends Locke {};
+const StringHandler = class StringHandler extends Locke {
+  /**
+   * Returns a event name for the specified event key in the specified locale.
+   * @method event
+   * @param {String} locale The locale of the event name to get.
+   * @param {String} event The event key of the event name to get.
+   * @returns {String} The event name for the specified key in the specified
+   * language.
+   */
+  event(locale, event) {
+    return super._getString('event', locale, event);
+  }
+};
 
 module.exports = StringHandler;
