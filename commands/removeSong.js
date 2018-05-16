391d754e14bcecd8f6a43cd4cea706ee171e5890
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-05-16 22:42:02 +0000

    commands: refactor removeSong command

--- commands/removeSong.js
+++ commands/removeSong.js
@@ -12,13 +12,11 @@ exports.exec = (Bastion, message, args) => {
     return Bastion.emit('error', '', Bastion.i18n.error(message.guild.language, 'musicDisabled'), message.channel);
   }
 
-  if (message.guild.music.textChannelID && message.channel.id !== message.guild.music.textChannelID) return Bastion.log.info('Music channels have been set, so music commands will only work in the music text channel.');
+  if (message.guild.music.textChannelID && message.guild.music.textChannelID !== message.channel.id) {
+    return Bastion.log.info('Music channels have been set, so music commands will only work in the Music Text Channel.');
+  }
 
   if (!message.guild.music.songs || !message.guild.music.songs.length) {
-    /**
-    * Error condition is encountered.
-    * @fires error
-    */
     return Bastion.emit('error', '', Bastion.i18n.error(message.guild.language, 'notPlaying'), message.channel);
   }
 
@@ -31,10 +29,6 @@ exports.exec = (Bastion, message, args) => {
   }
 
   if (args.index >= message.guild.music.songs.length || args.index < 1) {
-    /**
-     * Error condition is encountered.
-     * @fires error
-     */
     return Bastion.emit('error', '', Bastion.i18n.error(message.guild.language, 'indexRange'), message.channel);
   }
 
