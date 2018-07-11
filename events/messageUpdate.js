4e838587c8c22d6e8a815580d4c1fd1c5d96b452
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-11 12:11:35 +0000

    events: integrate server logging to messageUpdate event

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -9,30 +9,90 @@ const wordFilter = xrequire('./utils/wordFilter');
 const linkFilter = xrequire('./utils/linkFilter');
 const inviteFilter = xrequire('./utils/inviteFilter');
 
-module.exports = (oldMessage, newMessage) => {
-  // If message content hasn't been changed, do nothing
-  if (oldMessage.content === newMessage.content) return;
+module.exports = async  (oldMessage, newMessage) => {
+  try {
+    // If message content hasn't been changed, do nothing
+    if (oldMessage.content === newMessage.content) return;
 
-  /**
-   * Filter Bastion's credentials from the message
-   */
-  credentialsFilter(newMessage);
+    /**
+     * Filter Bastion's credentials from the message
+     */
+    credentialsFilter(newMessage);
 
-  if (!oldMessage.guild) return;
-  if (newMessage.author.bot) return;
+    if (!oldMessage.guild) return;
+    if (newMessage.author.bot) return;
 
-  /**
-   * Filter specific words from the message
-   */
-  wordFilter(newMessage);
+    /**
+     * Filter specific words from the message
+     */
+    wordFilter(newMessage);
 
-  /**
-   * Filter links from the message
-   */
-  linkFilter(newMessage);
+    /**
+     * Filter links from the message
+     */
+    linkFilter(newMessage);
 
-  /**
-   * Filter Discord server invites from the message
-   */
-  inviteFilter(newMessage);
+    /**
+     * Filter Discord server invites from the message
+     */
+    inviteFilter(newMessage);
+
+
+    let guildModel = await newMessage.client.database.models.guild.findOne({
+      attributes: [ 'serverLog' ],
+      where: {
+        guildID: newMessage.guild.id
+      }
+    });
+
+    if (!guildModel || !guildModel.dataValues.serverLog) return;
+
+    let logChannel = newMessage.guild.channels.get(guildModel.dataValues.serverLog);
+    if (!logChannel) return;
+
+    logChannel.send({
+      embed: {
+        color: newMessage.client.colors.ORANGE,
+        title: newMessage.client.i18n.event(newMessage.guild.language, 'messageUpdate'),
+        fields: [
+          {
+            name: 'Message Channel',
+            value: newMessage.channel.toString(),
+            inline: true
+          },
+          {
+            name: 'Message Channel ID',
+            value: newMessage.channel.id,
+            inline: true
+          },
+          {
+            name: 'Message ID',
+            value: newMessage.id,
+            inline: true
+          },
+          {
+            name: 'Message Author',
+            value: newMessage.author.tag,
+            inline: true
+          },
+          {
+            name: 'Message Author ID',
+            value: newMessage.author.id,
+            inline: true
+          },
+          {
+            name: 'Old Message Content',
+            value: '~~Deleted~~',
+            inline: true
+          }
+        ],
+        timestamp: new Date()
+      }
+    }).catch(e => {
+      newMessage.client.log.error(e);
+    });
+  }
+  catch (e) {
+    newMessage.client.log.error(e);
+  }
 };
