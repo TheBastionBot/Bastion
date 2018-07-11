81b6378a1e9a193bf4c0183d5985f545a6fb3997
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-11 12:12:55 +0000

    events: refactor messageUpdate event

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -14,30 +14,22 @@ module.exports = async  (oldMessage, newMessage) => {
     // If message content hasn't been changed, do nothing
     if (oldMessage.content === newMessage.content) return;
 
-    /**
-     * Filter Bastion's credentials from the message
-     */
+    // Filter Bastion's credentials from the message
     credentialsFilter(newMessage);
 
     if (!oldMessage.guild) return;
     if (newMessage.author.bot) return;
 
-    /**
-     * Filter specific words from the message
-     */
+    // Filter specific words from the message
     wordFilter(newMessage);
 
-    /**
-     * Filter links from the message
-     */
+    // Filter links from the message
     linkFilter(newMessage);
 
-    /**
-     * Filter Discord server invites from the message
-     */
+    // Filter Discord server invites from the message
     inviteFilter(newMessage);
 
-
+    // Server Logging
     let guildModel = await newMessage.client.database.models.guild.findOne({
       attributes: [ 'serverLog' ],
       where: {
