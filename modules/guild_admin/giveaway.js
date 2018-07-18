5b46863b51dfde11972895cf6db8f823327854a0
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-18 07:07:32 +0000

    commands: edit the giveaway message to show results

--- modules/guild_admin/giveaway.js
+++ modules/guild_admin/giveaway.js
@@ -62,9 +62,8 @@ exports.exec = async (Bastion, message, args) => {
 
           // If there're winners declare the result
           if (winners) {
-            giveawayMessage.delete().catch(() => {});
             // Declare the result in the channel
-            giveawayMessage.channel.send({
+            giveawayMessage.channel.edit({
               embed: {
                 color: Bastion.colors.BLUE,
                 title: 'Giveaway Event Ended',
