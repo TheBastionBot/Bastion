e492265d1e958dd34316d9d707e46723563aa80e
James Naylor <jamesnaylor85@gmail.com>
2018-09-13 14:00:36 +0100

    commands: add world of warcraft command
    Fixes: https://github.com/TheBastionBot/Bastion/issues/323

---
+++ commands/game_stats/wow.js
@@ -0,0 +1,166 @@
+/**
+ * @file wow command
+ * @author James Naylor (a.k.a euronay)
+ * @license GPL-3.0
+ */
+
+const request = xrequire('request-promise-native');
+
+const wowConstants = {
+  faction: [
+    'Alliance',
+    'Horde'
+  ],
+  gender: [
+    'None',
+    'Male',
+    'Female'
+  ],
+  race: [
+    'None',
+    'Human',
+    'Orc',
+    'Dwarf',
+    'Night Elf',
+    'Undead',
+    'Tauren',
+    'Gnome',
+    'Troll',
+    'Goblin',
+    'Blood Elf',
+    'Draenei',
+    'Fel Orc',
+    'Naga',
+    'Broken',
+    'Skeleton',
+    'Vrykul',
+    'Tuskarr',
+    'Forest Troll',
+    'Taunka',
+    'Northrend Skeleton',
+    'Ice Troll',
+    'Worgen',
+    'Gilnean',
+    'Pandaren',
+    'Pandaren',
+    'Pandaren',
+    'Nightborn',
+    'Highmountain Tauren',
+    'Void Elf',
+    'Lightforged Draenei',
+    'Zandalari Troll',
+    'Kul Tiran',
+    'Human',
+    'Dark Iron Dwarf',
+    'Vulpera',
+    'Mag\'har Orc'
+  ],
+  class: [
+    'None',
+    'Warrior',
+    'Paladin',
+    'Hunter',
+    'Rogue',
+    'Priest',
+    'Death Knight',
+    'Shaman',
+    'Mage',
+    'Warlock',
+    'Druid',
+    'Demon Hunter'
+  ]
+};
+
+exports.exec = async (Bastion, message, args) => {
+  try {
+    if (!args.character || !args.realm) {
+      /**
+       * The command was ran with invalid parameters.
+       * @fires commandUsage
+       */
+      return Bastion.emit('commandUsage', message, this.help);
+    }
+
+    let locale = args.region == 'us' ? 'en-us' : 'en-gb';
+
+    let requestUrl = `https://${args.region}.api.battle.net/wow/character/${args.realm}/${args.character}?fields=guild,items&locale=${locale}&apikey=${Bastion.credentials.battleNetApiKey}`;
+
+    options = {
+      url: requestUrl,
+      json: true
+    };
+    response = await request(options);
+
+    let factionIcon = `https://worldofwarcraft.akamaized.net/static/components/Logo/Logo-${response.faction == 0 ? 'alliance' : 'horde'}.png` + "";
+
+    let avatar = `https://render-${args.region}.worldofwarcraft.com/character/${response.thumbnail}` + "";
+
+    let stats = [{
+      name: 'Character',
+      value: `${response.level} ${wowConstants.race[response.race]} ${wowConstants.class[response.class]}`
+    },
+    {
+      name: 'Achievement Points',
+      value: response.achievementPoints
+    },
+    {
+      name: 'Item Level',
+      value: response.items.averageItemLevel
+    }];
+
+    if (response.guild) {
+      stats.push(
+        {
+          name: 'Guild',
+          value: response.guild.name
+        }
+      )
+    }
+
+    message.channel.send({
+      embed: {
+        author: {
+          name: response.name,
+          url: `https://worldofwarcraft.com/${locale}/character/${args.realm}/${args.character}`,
+          icon_url: factionIcon
+        },
+        fields: stats,
+        thumbnail: {
+          url: avatar
+        }
+      }
+    }).catch(e => {
+      Bastion.log.error(e);
+    });
+
+  }
+  catch (e) {
+    if (e.name === 'StatusCodeError') {
+      if (e.statusCode == '404') {
+        return Bastion.emit('error', '', Bastion.i18n.error(message.guild.language, 'notFound', 'player'), message.channel);
+      }
+      return Bastion.emit('error', e.statusCode, e.error.message, message.channel);
+    }
+    Bastion.log.error(e);
+  }
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true,
+  argsDefinitions: [
+    { name: 'character', type: String, defaultOption: true },
+    { name: 'realm', type: String, alias: 'r' },
+    { name: 'region', type: String, alias: 'e', defaultValue: 'us' },
+  ]
+};
+
+exports.help = {
+  name: 'wow',
+  description: 'Get stats of any World of Warcraft character from the Armory.',
+  botPermission: '',
+  userTextPermission: '',
+  userVoicePermission: '',
+  usage: 'wow <character> <realm> [-r <region>]',
+  example: ['wow Fallken -r Burning-Blade -e eu ']
+};
