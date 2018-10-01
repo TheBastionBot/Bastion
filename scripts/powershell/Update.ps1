d1dde9a4f34c7fc83d7cac31db8720f9f1285017
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-10-01 15:59:07 +0000

    scripts: use yarn in PowerShell scripts

--- scripts/powershell/Update.ps1
+++ scripts/powershell/Update.ps1
@@ -65,10 +65,10 @@ Write-Host "[Bastion]: Updating dependencies..."
 Write-Host
 
 Remove-Item -Path ".\node_modules", ".\package-lock.json" -Force -Recurse -ErrorAction SilentlyContinue
-npm install --only=production --no-package-lock
+yarn install --production --no-lockfile
 If (-Not ($?)) {
   Write-Host "[Bastion]: Unable to update Bastion, error while updating dependencies."
-  Write-Host "[Bastion]: Try updating it manually: npm i --only=production --no-package-lock"
+  Write-Host "[Bastion]: Try updating it manually: yarn install --production --no-lockfile"
   Write-Host "[Bastion]: Contact Bastion Support for further help."
 
   Exit-Bastion-Updater
