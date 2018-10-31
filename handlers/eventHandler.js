a1f378d38182b6cbd31ae48dad05ff666cfb2afc
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-10-31 18:12:28 +0000

    handlers: update eventHandler to load events from new location

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -14,20 +14,33 @@ const path = xrequire('path');
  * @returns {void}
  */
 module.exports = Bastion => {
+  const DISCORD_EVENTS_PATH = './events/discord/';
+  const SELF_EVENTS_PATH = './events/self/';
+
   /* eslint-disable no-sync */
-  let events = fs.readdirSync('./events/').
-    filter(file => !fs.statSync(path.resolve('./events/', file)).isDirectory()).
+  let DiscordEvents = fs.readdirSync(DISCORD_EVENTS_PATH).
+    filter(file => !fs.statSync(path.resolve(DISCORD_EVENTS_PATH, file)).isDirectory()).
+    filter(file => file.endsWith('.js'));
+
+  let SelfEvents = fs.readdirSync(SELF_EVENTS_PATH).
+    filter(file => !fs.statSync(path.resolve(SELF_EVENTS_PATH, file)).isDirectory()).
     filter(file => file.endsWith('.js'));
   /* eslint-enable no-sync */
 
-  for (let event of events) {
+  for (let event of DiscordEvents) {
     event = event.replace(/\.js$/i, '');
 
     if (event === 'ready') {
-      Bastion.on(event, () => xrequire('./events', event)(Bastion));
+      Bastion.on(event, () => xrequire(DISCORD_EVENTS_PATH, event)(Bastion));
     }
     else {
-      Bastion.on(event, xrequire('./events', event));
+      Bastion.on(event, xrequire(DISCORD_EVENTS_PATH, event));
     }
+  }
+
+  for (let event of SelfEvents) {
+    event = event.replace(/\.js$/i, '');
+
+    Bastion.on(event, xrequire(SELF_EVENTS_PATH, event));
   }
 };
