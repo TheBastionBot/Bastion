e0efaa1e6b77d442554650115fb5ea8f9b1016f7
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-10 15:11:20 +0000

    filters: promisify word filter

--- filters/wordFilter.js
+++ filters/wordFilter.js
@@ -7,62 +7,66 @@
 /**
  * Handles filtering of specific words in messages
  * @param {Message} message Discord.js message object
- * @returns {Promise<true>} If the message was filtered
+ * @returns {Promise<boolean>} Whether or not the message, by an unauthorized
+ * user, contains blacklisted words
  */
-module.exports = async message => {
-  try {
-    // If the user has Manage Server permission, return
-    if (message.member && message.member.hasPermission('MANAGE_GUILD')) return;
+module.exports = message => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      resolve(false);
 
-    // Fetch filter data from database
-    let guildModel = await message.client.database.models.guild.findOne({
-      attributes: [ 'guildID', 'filteredWords' ],
-      where: {
-        guildID: message.guild.id,
-        filterWords: true
-      },
-      include: [
-        {
-          model: message.client.database.models.textChannel,
-          attributes: [ 'channelID', 'ignoreWordFilter' ]
+      // If the user has Manage Server permission, we don't need to filter it
+      if (message.member.hasPermission('MANAGE_GUILD')) return;
+
+      // Fetch filter data from database
+      let guildModel = await message.client.database.models.guild.findOne({
+        attributes: [ 'guildID', 'filteredWords' ],
+        where: {
+          guildID: message.guild.id,
+          filterWords: true
         },
-        {
-          model: message.client.database.models.role,
-          attributes: [ 'roleID', 'ignoreWordFilter' ]
+        include: [
+          {
+            model: message.client.database.models.textChannel,
+            attributes: [ 'channelID', 'ignoreWordFilter' ]
+          },
+          {
+            model: message.client.database.models.role,
+            attributes: [ 'roleID', 'ignoreWordFilter' ]
+          }
+        ]
+      });
+
+      // Check if word filter is disabled
+      if (!guildModel) return;
+
+      // Check if the channel is whitelisted
+      if (guildModel.textChannels.
+        filter(channel => channel.dataValues.ignoreWordFilter).
+        map(channel => channel.dataValues.channelID).
+        includes(message.channel.id)) return;
+
+      // Check if the user is in a whitelisted role
+      let whitelistedRoles = guildModel.roles.
+        filter(role => role.dataValues.ignoreWordFilter).
+        map(role => role.dataValues.roleID);
+
+      for (let role of whitelistedRoles) {
+        if (message.member.roles.has(role)) return;
+      }
+
+      // If message contains a blacklisted word, filter it
+      let filteredWords = guildModel.dataValues.filteredWords ? guildModel.dataValues.filteredWords : [];
+
+      for (let word of filteredWords) {
+        if (message.content.toLowerCase().split(' ').includes(word.toLowerCase())) {
+          if (message.deletable) message.delete().catch(() => {});
+          return resolve(true);
         }
-      ]
-    });
-
-    // If word filter is disabled, return
-    if (!guildModel) return;
-
-    // If the channel is whitelisted, return
-    if (guildModel.textChannels.
-      filter(channel => channel.dataValues.ignoreWordFilter).
-      map(channel => channel.dataValues.channelID).
-      includes(message.channel.id)) return;
-
-    // If the user is in a whitelisted role, return
-    let whitelistedRoles = guildModel.roles.
-      filter(role => role.dataValues.ignoreWordFilter).
-      map(role => role.dataValues.roleID);
-
-    for (let role of whitelistedRoles) {
-      if (message.member.roles.has(role)) return;
-    }
-
-    let filteredWords = guildModel.dataValues.filteredWords ? guildModel.dataValues.filteredWords : [];
-
-    for (let word of filteredWords) {
-      if (message.content.toLowerCase().split(' ').includes(word.toLowerCase())) {
-        if (message.deletable) {
-          message.delete().catch(() => {});
-        }
-        return true;
       }
     }
-  }
-  catch (e) {
-    message.client.log.error(e);
-  }
+    catch (e) {
+      reject(e);
+    }
+  });
 };
