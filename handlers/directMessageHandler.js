f964c71025da35ef6bfc37ac6de243a85140637f
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-12-01 07:49:55 +0000

    handlers: implement DM relays
    Refs: https://github.com/TheBastionBot/Bastion/issues/419

--- handlers/directMessageHandler.js
+++ handlers/directMessageHandler.js
@@ -9,35 +9,63 @@
  * @param {Message} message Discord.js message object
  * @returns {void}
  */
-module.exports = message => {
-  if (!message.content) return;
+module.exports = async message => {
+  try {
+    if (!message.content) return;
 
-  if (message.content.toLowerCase().startsWith('help')) {
-    return message.channel.send({
-      embed: {
-        color: message.client.colors.BLUE,
-        title: 'The Bastion Bot',
-        url: 'https://bastionbot.org',
-        description: 'Join [**Bastion HQ**](https://discord.gg/fzx8fkt) to test Bastion and it\'s commands, for giveaway events, for chatting and for a lot of fun!',
-        fields: [
-          {
-            name: 'Bastion HQ Invite Link',
-            value: 'https://discord.gg/fzx8fkt'
+    if (message.content.toLowerCase().startsWith('help')) {
+      return message.channel.send({
+        embed: {
+          color: message.client.colors.BLUE,
+          title: 'The Bastion Bot',
+          url: 'https://bastionbot.org',
+          description: 'Join [**Bastion HQ**](https://discord.gg/fzx8fkt) to test Bastion and it\'s commands, for giveaway events, for chatting and for a lot of fun!',
+          fields: [
+            {
+              name: 'Bastion HQ Invite Link',
+              value: 'https://discord.gg/fzx8fkt'
+            },
+            {
+              name: 'Bastion Bot Invite Link',
+              value: `https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=2146958463`
+            }
+          ],
+          thumbnail: {
+            url: message.client.user.displayAvatarURL
           },
-          {
-            name: 'Bastion Bot Invite Link',
-            value: `https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=2146958463`
+          footer: {
+            text: '</> with ❤ by Sankarsan Kampa (a.k.a. k3rn31p4nic)'
           }
-        ],
-        thumbnail: {
-          url: message.client.user.displayAvatarURL
-        },
-        footer: {
-          text: '</> with ❤ by Sankarsan Kampa (a.k.a. k3rn31p4nic)'
         }
+      }).catch(e => {
+        message.client.log.error(e);
+      });
+    }
+
+    let settingsModel = await message.client.database.models.settings.findOne({
+      attributes: [ 'relayDirectMessages' ],
+      where: {
+        botID: message.client.user.id
       }
-    }).catch(e => {
-      message.client.log.error(e);
     });
+
+    if (settingsModel && settingsModel.dataValues.relayDirectMessages) {
+      // Find the application owner
+      let app = await message.client.fetchApplication();
+      let owner = await message.client.fetchUser(app.owner.id);
+
+      // Relay the message
+      await owner.send({
+        embed: {
+          author: {
+            name: message.author.tag
+          },
+          description: message.content
+        }
+      });
+    }
+  }
+  catch (e) {
+    message.client.log.error(e);
   }
 };
