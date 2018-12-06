d0dc515e535078c42b0bf2b149836ae3f1d9acd3
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-12-06 18:45:00 +0000

    prototypes: remove String#substitute method

--- prototypes/String.prototype.js
+++ prototypes/String.prototype.js
@@ -1,9 +1,3 @@
-String.prototype.substitute = function () {
-  let count = 0;
-  let args = arguments;
-  return this.replace(/%var%/g, () => args[count++]);
-};
-
 String.prototype.toTitleCase = function() {
   let newstr = this.split(' ');
   for (let i = 0; i < newstr.length; i++) {
