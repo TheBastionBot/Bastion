cc55d8d42711b4481b446b09272d9238d4e2da39
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-12-12 06:48:21 +0000

    commands: use BWAPI in capture command

--- commands/owner/capture.js
+++ commands/owner/capture.js
@@ -4,57 +4,36 @@
  * @license GPL-3.0
  */
 
-const capture = xrequire('webshot');
-
 exports.exec = async (Bastion, message, args) => {
-  if (!args.length) {
+  if (!args.url) {
     return Bastion.emit('commandUsage', message, this.help);
   }
 
-  if (!/^(http[s]?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)$/.test(args[0])) {
+  if (!/^(http[s]?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)$/.test(args.url)) {
     return Bastion.emit('error', '', Bastion.i18n.error(message.guild.language, 'invalidInput', 'URL'), message.channel);
   }
-  let options = {
-    windowSize: {
-      width: 1366,
-      height: 768
-    },
-    shotSize: {
-      width: 'all',
-      height: 'all'
-    },
-    timeout: 15000,
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
-  };
 
-  await capture(args[0], options, (err, renderStream) => {
-    if (err) {
-      return Bastion.emit('error', '', Bastion.i18n.error(message.guild.language, 'serverNotFound', args[0]), message.channel);
+  let webshot = await Bastion.methods.makeBWAPIRequest('/url/capture', {
+    encoding: null,
+    qs: {
+      url: args.url
     }
-    let imageBuffers = [];
-    renderStream.on('data', (data) => {
-      imageBuffers.push(data);
-    });
-    renderStream.on('end', () => {
-      let imageBuffer = Buffer.concat(imageBuffers);
-      if (imageBuffer.length > 0) {
-        message.channel.send({
-          file: {
-            attachment: imageBuffer,
-            name: 'capture.jpg'
-          }
-        }).catch(e => {
-          Bastion.log.error(e);
-        });
-      }
-    });
+  });
+
+  webshot = Buffer.from(webshot);
+
+  await message.channel.send({
+    files: [ { attachment: webshot, name: 'capture.png' } ]
   });
 };
 
 exports.config = {
   aliases: [],
   enabled: true,
-  ownerOnly: true
+  ownerOnly: true,
+  argsDefinitions: [
+    { name: 'url', type: String, defaultOption: true }
+  ]
 };
 
 exports.help = {
@@ -64,5 +43,5 @@ exports.help = {
   userTextPermission: '',
   userVoicePermission: '',
   usage: 'capture <url>',
-  example: [ 'capture BastionBot.org' ]
+  example: [ 'capture https://bastionbot.org' ]
 };
