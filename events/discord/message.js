e1df207c0ea9381ae304328540e9f0a074869502
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-12-19 06:25:21 +0000

    events: remove slow mode implementation from message event

--- events/discord/message.js
+++ events/discord/message.js
@@ -15,7 +15,6 @@ const handleCommand = xrequire('./handlers/commandHandler');
 const handleConversation = xrequire('./handlers/conversationHandler');
 const handleDirectMessage = xrequire('./handlers/directMessageHandler');
 let recentLevelUps = [];
-let recentUsers = {};
 
 module.exports = async message => {
   try {
@@ -51,91 +50,6 @@ module.exports = async message => {
        * Moderate mention spams in the message
        */
       if (await mentionSpamFilter(message)) return;
-
-      let guildModel = await message.client.database.models.guild.findOne({
-        attributes: [ 'slowMode' ],
-        where: {
-          guildID: message.guild.id
-        },
-        include: [
-          {
-            model: message.client.database.models.textChannel,
-            attributes: [ 'channelID', 'ignoreSlowMode' ]
-          },
-          {
-            model: message.client.database.models.role,
-            attributes: [ 'roleID', 'ignoreSlowMode' ]
-          }
-        ]
-      });
-
-      let slowModeIgnoredChannels = guildModel.textChannels.length && guildModel.textChannels.filter(model => model.dataValues.ignoreSlowMode).map(model => model.dataValues.channelID);
-      let slowModeIgnoredRoles = guildModel.roles.length && guildModel.roles.filter(model => model.dataValues.ignoreSlowMode).map(model => model.dataValues.roleID);
-
-      if (!slowModeIgnoredChannels) slowModeIgnoredChannels = [];
-      if (!slowModeIgnoredRoles) slowModeIgnoredRoles = [];
-
-      if (!slowModeIgnoredChannels.includes(message.channel.id) || !message.member.roles.some(role => slowModeIgnoredRoles.includes(role.id))) {
-        if (!message.channel.permissionsFor(message.member) || !message.channel.permissionsFor(message.member).has('MANAGE_ROLES')) {
-          if (guildModel && guildModel.dataValues.slowMode) {
-            if (recentUsers.hasOwnProperty(message.author.id)) {
-              let title, description;
-
-              ++recentUsers[message.author.id];
-              if (recentUsers[message.author.id] >= 8) {
-                title = 'Warned ya!.';
-                description = `${message.author} you have been muted for 15 minutes.`;
-                await message.channel.overwritePermissions(message.author, {
-                  SEND_MESSAGES: false,
-                  ADD_REACTIONS: false
-                });
-
-                setTimeout(() => {
-                  let permissionOverwrites = message.channel.permissionOverwrites.get(message.author.id);
-                  if (permissionOverwrites) {
-                    if (permissionOverwrites.deny === 2112) {
-                      permissionOverwrites.delete();
-                    }
-                    else {
-                      message.channel.overwritePermissions(message.author, {
-                        SEND_MESSAGES: null,
-                        ADD_REACTIONS: null
-                      }).catch(e => {
-                        message.client.log.error(e);
-                      });
-                    }
-                  }
-                }, 15 * 60 * 1000);
-              }
-              else if (recentUsers[message.author.id] >= 6) {
-                title = 'Cooldown, dark lord.';
-                description = `${message.author} you are sending messages way too quickly.`;
-              }
-              else if (recentUsers[message.author.id] >= 4) {
-                title = 'Woah There. Way too Spicy.';
-                description = `${message.author} you are sending messages too quickly.`;
-              }
-
-              if (title && description) {
-                await message.channel.send({
-                  embed: {
-                    color: message.client.colors.ORANGE,
-                    title: title,
-                    description: description
-                  }
-                });
-              }
-            }
-            else {
-              recentUsers[message.author.id] = 1;
-
-              setTimeout(() => {
-                delete recentUsers[message.author.id];
-              }, 5 * 1000);
-            }
-          }
-        }
-      }
 
       /**
        * Check if the message contains a trigger and respond to it
