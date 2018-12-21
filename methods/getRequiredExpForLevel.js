a40042ca1ecb478f499c179975f00714af38452f
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-12-21 15:10:28 +0000

    methods: minor refactor

--- methods/getRequiredExpForLevel.js
+++ methods/getRequiredExpForLevel.js
@@ -10,7 +10,7 @@ module.exports = (level = 0) => {
   if (level >= 0) {
     // Why's 0.15 used? Magic number, mate! MAGIC NUMBER!
     return Math.floor((level / 0.15) * (level / 0.15));
-
   }
+
   return null;
 };
