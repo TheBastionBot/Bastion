55216500e273f21337684d865662ac0089d1aa91
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-12-27 07:19:19 +0000

    prototypes: add Object class extended methods

---
+++ prototypes/Object.js
@@ -0,0 +1,11 @@
+Object.fromPairs = (pairs) => {
+  let index = -1;
+  let length = pairs === null ? 0 : pairs.length;
+  let result = {};
+
+  while (++index < length) {
+    let pair = pairs[index];
+    result[pair[0]] = pair[1];
+  }
+  return result;
+};
