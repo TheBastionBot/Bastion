0b5b4e26883e91e342359949a8dd8903cad90fa5
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-01-01 15:35:22 +0000

    commands(music): improvements to volume command

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -21,15 +21,8 @@ exports.exec = async (Bastion, message, args) => {
   }
 
   let color = Bastion.colors.GREEN;
-  if (args[0] === '+') {
-    message.guild.voiceConnection.dispatcher.setVolume((message.guild.voiceConnection.dispatcher.volume * 50 + 2) / 50);
-  }
-  else if (args[0] === '-') {
-    message.guild.voiceConnection.dispatcher.setVolume((message.guild.voiceConnection.dispatcher.volume * 50 - 2) / 50);
-  }
-  else if (/^\d+$/.test(args[0])) {
-    args = args[0] > 0 && args[0] < 100 ? args[0] : 100;
-    message.guild.voiceConnection.dispatcher.setVolume(args / 50);
+  if (args.value) {
+    message.guild.voiceConnection.dispatcher.setVolume(args.value / 50);
   }
   else {
     color = Bastion.colors.BLUE;
@@ -38,7 +31,7 @@ exports.exec = async (Bastion, message, args) => {
   await message.guild.music.textChannel.send({
     embed: {
       color: color,
-      description: `Volume: ${Math.round(message.guild.voiceConnection.dispatcher.volume * 50)}%`
+      description: `Volume: ${message.guild.voiceConnection.dispatcher.volume * 50}%`
     }
   }).catch(e => {
     Bastion.log.error(e);
@@ -48,15 +41,18 @@ exports.exec = async (Bastion, message, args) => {
 exports.config = {
   aliases: [],
   enabled: true,
-  musicMasterOnly: true
+  musicMasterOnly: true,
+  argsDefinitions: [
+    { name: 'value', type: Number, defaultOption: true }
+  ]
 };
 
 exports.help = {
   name: 'volume',
-  description: 'Changes/sets the volume of current playback in your Discord.',
+  description: 'Changes the volume of current playback in your server.',
   botPermission: '',
   userTextPermission: '',
   userVoicePermission: '',
-  usage: 'volume < + | - | amount >',
-  example: [ 'volume +', 'volume -', 'volume 25' ]
+  usage: 'volume <VALUE>',
+  example: [ 'volume 25' ]
 };
