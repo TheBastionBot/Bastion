e18393ffabd870730f9402939c0c8e0b9c635e1b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-09-05 09:32:36 +0530

    prototypes(Array): add zip class method

--- prototypes/Array.js
+++ prototypes/Array.js
@@ -5,3 +5,7 @@ Array.intersection = function(arr1, arr2) {
 Array.union = function(arr1, arr2) {
   return [ ...new Set([ ...arr1, ...arr2 ]) ];
 };
+
+Array.zip = function(...arr) {
+  return [...arr[0]].map((_, i) => arr.map(a => a[i]));
+};
