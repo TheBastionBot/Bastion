5cd4bae982d8e2e8b7c80ea7632bec9c7b4a53a3
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-11-29 11:24:00 +0530

    methods: add makeExtractionRequest

---
+++ methods/makeExtractionRequest.js
@@ -0,0 +1,25 @@
+/**
+ * @file makeExtractionRequest
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license GPL-3.0
+ */
+
+const ExtractionAPI = require('ex.traction');
+const extraction = new ExtractionAPI({
+  headers: {
+    'User-Agent': 'Bastion Discord Bot (https://bastionbot.org)'
+  }
+});
+
+module.exports = (path, options) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let response = await extraction.request(path, options);
+
+      resolve(response);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
