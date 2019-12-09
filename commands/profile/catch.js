5c73838b32c7a6dc044c93f152ca63884c2cd12e
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-12-09 16:11:56 +0530

    commands: add catch command

---
+++ commands/profile/catch.js
@@ -0,0 +1,37 @@
+/**
+ * @file catch command
+ * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
+ * @license GPL-3.0
+ */
+
+exports.exec = async (Bastion, message, args) => {
+  if (message.channel.robbers && message.channel.robbers.length) {
+    // Remove all the robbers.
+    delete message.channel.robbers;
+
+    // Reward the catcher.
+    Bastion.emit('userDebit', message.member, 1000);
+
+    await message.channel.send({
+      embed: {
+        color: Bastion.colors.RED,
+        description: 'Thank you for catching the robber. You\'ve been awarded!'
+      }
+    }).catch(() => {});
+  }
+};
+
+exports.config = {
+  aliases: [],
+  enabled: true
+};
+
+exports.help = {
+  name: 'catch',
+  description: 'Catch any robbers, who are stealing %currency.name_plural%, in the channel.',
+  botPermission: '',
+  userTextPermission: '',
+  userVoicePermission: '',
+  usage: 'catch',
+  example: []
+};
