5cdfcbbd616ccb194615f52b0630f500c1b70ba2
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-11 22:02:58 +0530

    rewrite begins!
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- methods/intersect.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file intersect
- * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
- * @license GPL-3.0
- */
-
-module.exports = function(...a) {
-  return [ ...a ].reduce((p, c) => p.filter(e => c.includes(e)));
-};
--- prototypes/Number.js
+++
@@ -1,6 +0,0 @@
-Number.random = (lower = Number.MIN_SAFE_INTEGER, upper = Number.MAX_SAFE_INTEGER) => {
-  lower = Math.ceil(lower);
-  upper = Math.floor(upper);
-
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-};
