9fd6b41c7b369d4266cd243bc8ad0a24eb508bd6
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 00:01:46 +0530

    models: add Role model
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ models/Role.ts
@@ -0,0 +1,58 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import * as mongoose from "mongoose";
+
+export interface Role {
+    _id: string;
+    id?: string;
+    guild: string;
+    selfAssignable?: boolean;
+    autoAssignable?: {
+        forBots: boolean;
+        forUsers: boolean;
+    };
+    level?: number;
+    price?: number;
+    blacklisted?: boolean;
+    disabledCommands?: string[];
+}
+
+export default mongoose.model<Role & mongoose.Document>("Role", new mongoose.Schema<Role>({
+    _id: {
+        type: String,
+        required: true,
+    },
+    guild: {
+        type: String,
+        required: true,
+        ref: "Guild",
+    },
+    selfAssignable: {
+        type: Boolean,
+    },
+    autoAssignable: {
+        type: {
+            forBots: {
+                type: Boolean,
+            },
+            forUsers: {
+                type: Boolean,
+            },
+        },
+    },
+    level: {
+        type: Number,
+    },
+    price: {
+        type: Number,
+    },
+    blacklisted: {
+        type: Boolean,
+    },
+    disabledCommands: {
+        type: [ String ],
+    },
+}));
