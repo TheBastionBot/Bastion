5cf6f7001e862e3002bb997861008135a5468841
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 00:04:22 +0530

    listeners: add listener for voiceStateUpdate event
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ listeners/voiceStateUpdate.ts
@@ -0,0 +1,18 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Listener, Constants } from "tesseract";
+import { VoiceState } from "discord.js";
+
+export = class VoiceStateUpdateListener extends Listener {
+    constructor() {
+        super("voiceStateUpdate", {
+            mode: Constants.LISTENER_MODE.ON,
+        });
+    }
+
+    exec = async (oldVoiceState: VoiceState, newVoiceState: VoiceState): Promise<void> => {
+    }
+}
