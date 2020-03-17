a873d5848df5d19bd33da5aaaefaffa3fd1f2c49
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 00:01:49 +0530

    models: add Trigger model
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ models/Trigger.ts
@@ -0,0 +1,35 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import * as mongoose from "mongoose";
+
+export interface Trigger {
+    guild: string;
+    trigger: string;
+    responseMessage?: unknown;
+    responseReaction?: string;
+}
+
+const triggerSchema = new mongoose.Schema<Trigger>({
+    guild: {
+        type: String,
+        required: true,
+        ref: "Guild",
+    },
+    trigger: {
+        type: String,
+        required: true,
+    },
+    responseMessage: {
+        type: mongoose.SchemaTypes.Mixed,
+        required: true,
+    },
+    responseReaction: {
+        type: String,
+        required: true,
+    },
+});
+
+export default mongoose.model<Trigger & mongoose.Document>("Trigger", triggerSchema);
