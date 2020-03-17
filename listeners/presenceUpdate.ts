c95c9db2f889e172dbe8def5ecd009975919aa98
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 00:04:16 +0530

    listeners: add listener for presenceUpdate event
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ listeners/presenceUpdate.ts
@@ -0,0 +1,18 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Listener, Constants } from "tesseract";
+import { Presence } from "discord.js";
+
+export = class PresenceListener extends Listener {
+    constructor() {
+        super("presenceUpdate", {
+            mode: Constants.LISTENER_MODE.ON,
+        });
+    }
+
+    exec = async (oldPresence: Presence, newPresence: Presence): Promise<void> => {
+    }
+}
