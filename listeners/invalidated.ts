d5b9bf5c3d44cb0f9569c904e8a94ea79e9ca616
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 00:04:11 +0530

    listeners: add listener for invalidated event
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ listeners/invalidated.ts
@@ -0,0 +1,17 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Listener, Constants } from "tesseract";
+
+export = class InvalidatedListener extends Listener {
+    constructor() {
+        super("invalidated", {
+            mode: Constants.LISTENER_MODE.ONCE,
+        });
+    }
+
+    exec = async (): Promise<void> => {
+    }
+}
