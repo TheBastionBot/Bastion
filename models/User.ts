cdcf990f8c0fd88cbcbfd4322d15f680df789494
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 00:01:49 +0530

    models: add User model
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ models/User.ts
@@ -0,0 +1,46 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import * as mongoose from "mongoose";
+
+export interface User {
+    _id: string;
+    id?: string;
+    avatar?: string;
+    info?: string;
+    cakeDay?: Date;
+    location?: string;
+    color?: number;
+    afk?: string;
+}
+
+export default mongoose.model<User & mongoose.Document>("User", new mongoose.Schema<User>({
+    _id: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        trim: true,
+    },
+    info: {
+        type: String,
+        trim: true,
+    },
+    cakeDay: {
+        type: Date,
+    },
+    location: {
+        type: String,
+        trim: true,
+    },
+    color: {
+        type: Number,
+    },
+    afk: {
+        type: String,
+        trim: true,
+    },
+}));
