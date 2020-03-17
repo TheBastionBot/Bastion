41bd8f08674216323f483d12d7884cfa826bfea9
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 00:01:47 +0530

    models: add TextChannel model
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ models/TextChannel.ts
@@ -0,0 +1,58 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import * as mongoose from "mongoose";
+
+export interface TextChannel {
+    _id: string;
+    id?: string;
+    guild: string;
+    language?: string;
+    blacklisted?: boolean;
+    ignoredFilters?: {
+        inviteFilter?: boolean;
+        linkFilter?: boolean;
+        mentionFilter?: boolean;
+        messageFilter?: boolean;
+    };
+    disabledCommands?: string[];
+}
+
+export default mongoose.model<TextChannel & mongoose.Document>("TextChannel", new mongoose.Schema<TextChannel>({
+    _id: {
+        type: String,
+        required: true,
+    },
+    guild: {
+        type: String,
+        required: true,
+        ref: "Guild",
+    },
+    language: {
+        type: String,
+    },
+    blacklisted: {
+        type: Boolean,
+    },
+    ignoredFilters: {
+        type: {
+            inviteFilter: {
+                type: Boolean,
+            },
+            linkFilter: {
+                type: Boolean,
+            },
+            mentionFilter: {
+                type: Boolean,
+            },
+            messageFilter: {
+                type: Boolean,
+            },
+        },
+    },
+    disabledCommands: {
+        type: [ String ],
+    },
+}));
