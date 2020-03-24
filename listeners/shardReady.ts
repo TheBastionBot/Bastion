42ceacb7149d2585b0a8c4ab9b52929d5151ab2f
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-25 03:30:20 +0530

    listeners(shardReady): implement the listener
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/shardReady.ts
+++ listeners/shardReady.ts
@@ -13,5 +13,11 @@ export = class ShardReadyListener extends Listener {
     }
 
     exec = async (shardId: number, unavailableGuilds?: Set<string>): Promise<void> => {
+        this.client.log.info("Shard " + shardId + " - Ready in " + process.uptime() * 1000 + "ms");
+
+        if (this.client.shard.count === shardId + 1) {
+            this.client.log.info("Systems Ready in " + (Date.now() - parseInt(process.env.BASTION_BOOT_TIME)) + "ms");
+            await this.client.shard.broadcastEval("process.env.BASTION_SHARDS_READY = true");
+        }
     }
 }
