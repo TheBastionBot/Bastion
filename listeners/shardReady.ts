d91dff01a47301f1aaa1c47ddc002d4eb1ce1e90
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-27 00:07:45 +0530

    listeners(shardReady): minor changes
    * Remove time took to become ready
    * Show systems ready only if the last shard is being ready for the first time
    * Linting
    
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/shardReady.ts
+++ listeners/shardReady.ts
@@ -12,11 +12,11 @@ export = class ShardReadyListener extends Listener {
         });
     }
 
-    exec = async (shardId: number, unavailableGuilds?: Set<string>): Promise<void> => {
-        this.client.log.info("Shard " + shardId + " - Ready in " + process.uptime() * 1000 + "ms");
+    exec = async (shardId: number): Promise<void> => {
+        this.client.log.info("Shard " + shardId + " - Ready");
 
-        if (this.client.shard.count === shardId + 1) {
-            this.client.log.info("Systems Ready in " + (Date.now() - parseInt(process.env.BASTION_BOOT_TIME)) + "ms");
+        if (!process.env.BASTION_SHARDS_READY && this.client.shard.count === shardId + 1) {
+            this.client.log.info("Systems Ready!");
             await this.client.shard.broadcastEval("process.env.BASTION_SHARDS_READY = true");
         }
     }
