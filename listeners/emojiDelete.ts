e814b9023b4e365c217fb60af007b481eeb60c9f
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-28 23:00:13 +0530

    listeners(emojiDelete): log emojiDelete event
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/emojiDelete.ts
+++ listeners/emojiDelete.ts
@@ -6,6 +6,8 @@
 import { Listener, Constants } from "tesseract";
 import { GuildEmoji } from "discord.js";
 
+import Guild = require("../structures/Guild");
+
 export = class EmojiDeleteListener extends Listener {
     constructor() {
         super("emojiDelete", {
@@ -14,5 +16,24 @@ export = class EmojiDeleteListener extends Listener {
     }
 
     exec = async (emoji: GuildEmoji): Promise<void> => {
+        const guild = emoji.guild as Guild;
+
+        guild.createLog({
+            event: "emojiDelete",
+            fields: [
+                {
+                    name: "Emoji Name",
+                    value: emoji.name,
+                    inline: true,
+                },
+                {
+                    name: "Emoji Identifier",
+                    value: emoji.identifier,
+                    inline: true,
+                },
+            ],
+            footer: emoji.managed ? "Managed" : undefined,
+            timestamp: emoji.createdTimestamp,
+        });
     }
 }
