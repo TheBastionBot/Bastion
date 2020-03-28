2785877f74ca8ad5862ad6ad0a3cf98b13142fa3
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-29 01:28:42 +0530

    listeners(guildMemberAdd): log guild member join events
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/guildMemberAdd.ts
+++ listeners/guildMemberAdd.ts
@@ -6,6 +6,8 @@
 import { Listener, Constants } from "tesseract";
 import { GuildMember } from "discord.js";
 
+import Guild = require("../structures/Guild");
+
 export = class GuildMemberAddListener extends Listener {
     constructor() {
         super("guildMemberAdd", {
@@ -14,5 +16,33 @@ export = class GuildMemberAddListener extends Listener {
     }
 
     exec = async (member: GuildMember): Promise<void> => {
+        const guild = member.guild as Guild;
+
+        guild.createLog({
+            event: "guildMemberAdd",
+            fields: [
+                {
+                    name: "Member",
+                    value: member.user.tag,
+                    inline: true,
+                },
+                {
+                    name: "Member ID",
+                    value: member.id,
+                    inline: true,
+                },
+                {
+                    name: "Member Type",
+                    value: member.user.bot ? "Bot" : "Human",
+                    inline: true,
+                },
+                {
+                    name: "Joined Discord",
+                    value: member.user.createdAt.toUTCString(),
+                    inline: true,
+                },
+            ],
+            timestamp: member.joinedTimestamp,
+        });
     }
 }
