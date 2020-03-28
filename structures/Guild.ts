d9de348eccd7fb93ccd26638f70391efa936f54d
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-28 19:22:33 +0530

    structures(Guild): add createLog method
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- structures/Guild.ts
+++ structures/Guild.ts
@@ -4,16 +4,59 @@
  */
 
 import { Client } from "tesseract";
-import { Guild } from "discord.js";
+import { Constants, EmbedFieldData, Guild, Message, NewsChannel, TextChannel } from "discord.js";
 
 import GuildModel from "../models/Guild";
 
+
+interface GuildCreateLogOptions {
+    event: string
+    fields: EmbedFieldData[]
+    footer?: string
+    timestamp?: number | Date
+}
+
 export = class BastionGuild extends Guild {
+    client: Client;
+
     constructor(client: Client, data: object) {
         super(client, data);
     }
 
     public async getDocument() {
         return await GuildModel.findById(this.id);
+    }
+
+    public createLog(options: GuildCreateLogOptions): Promise<Message> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const document = await this.getDocument();
+
+                const channelsPool = this.client.channels.cache.filter(c => c.type === "text" || c.type === "news");
+
+                if (document.serverLogChannelId && channelsPool.has(document.serverLogChannelId)) {
+                    const serverLogChannel = channelsPool.get(document.serverLogChannelId);
+
+                    if (serverLogChannel instanceof NewsChannel || serverLogChannel instanceof TextChannel) {
+                        resolve(await serverLogChannel.send({
+                            embed: {
+                                color: Constants.Colors.DARK_BUT_NOT_BLACK,
+                                title: this.client.locale.getString(document.language, "events", options.event),
+                                fields: options.fields,
+                                footer: {
+                                    text: options.footer,
+                                },
+                                timestamp: options.timestamp || new Date(),
+                            },
+                        }));
+                    }
+
+                    resolve(null);
+                }
+            } catch (e) {
+                this.client.log.error(e);
+                reject(null);
+            }
+        });
     }
 }
