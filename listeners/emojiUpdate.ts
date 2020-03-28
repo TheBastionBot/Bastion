642981197d05552b0e913f4ab60da9418075f79b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-28 23:00:38 +0530

    listeners(emojiUpdate): log emojiUpdate event
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/emojiUpdate.ts
+++ listeners/emojiUpdate.ts
@@ -6,6 +6,8 @@
 import { Listener, Constants } from "tesseract";
 import { GuildEmoji } from "discord.js";
 
+import Guild = require("../structures/Guild");
+
 export = class EmojiUpdateListener extends Listener {
     constructor() {
         super("emojiUpdate", {
@@ -14,5 +16,31 @@ export = class EmojiUpdateListener extends Listener {
     }
 
     exec = async (oldEmoji: GuildEmoji, newEmoji: GuildEmoji): Promise<void> => {
+        if (oldEmoji.name === newEmoji.name) return;
+
+        const guild = oldEmoji.guild as Guild;
+
+        guild.createLog({
+            event: "emojiUpdate",
+            fields: [
+                {
+                    name: "Emoji ID",
+                    value: oldEmoji.id,
+                    inline: true,
+                },
+                {
+                    name: "Old Emoji Name",
+                    value: oldEmoji.name,
+                    inline: true,
+                },
+                {
+                    name: "New Emoji Name",
+                    value: newEmoji.name,
+                    inline: true,
+                },
+            ],
+            footer: oldEmoji.managed ? "Managed" : undefined,
+            timestamp: oldEmoji.createdTimestamp,
+        });
     }
 }
