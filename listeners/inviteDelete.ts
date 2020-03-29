349019c147173154756e5c211d5da29018fe3c54
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-29 16:42:03 +0530

    listeners(inviteDelete): log invite delete events
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/inviteDelete.ts
+++ listeners/inviteDelete.ts
@@ -6,6 +6,8 @@
 import { Listener, Constants } from "tesseract";
 import { Invite } from "discord.js";
 
+import Guild = require("../structures/Guild");
+
 export = class InviteDeleteListener extends Listener {
     constructor() {
         super("inviteDelete", {
@@ -14,5 +16,41 @@ export = class InviteDeleteListener extends Listener {
     }
 
     exec = async (invite: Invite): Promise<void> => {
+        const guild = invite.guild as Guild;
+
+        const fields = [
+            {
+                name: "Invite Code",
+                value: invite.code,
+                inline: true,
+            },
+            {
+                name: "Invite Channel",
+                value: invite.channel,
+                inline: true,
+            },
+        ];
+
+        if (invite.inviter) {
+            fields.push({
+                name: "Inviter",
+                value: invite.inviter.tag,
+                inline: true,
+            });
+        }
+
+        if (invite.uses) {
+            fields.push({
+                name: "Uses",
+                value: invite.uses + (invite.maxUses ? " / " + invite.maxUses : ""),
+                inline: true,
+            });
+        }
+
+        guild.createLog({
+            event: "inviteDelete",
+            fields,
+            footer: invite.temporary ? "Temporary Membership" : undefined,
+        });
     }
 }
