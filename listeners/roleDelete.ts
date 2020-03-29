121ac7ad9d7f2441de5823316ecdc14a2968c528
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-29 21:43:32 +0530

    listeners(guildDelete): log role delete events
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/roleDelete.ts
+++ listeners/roleDelete.ts
@@ -6,6 +6,8 @@
 import { Listener, Constants } from "tesseract";
 import { Role } from "discord.js";
 
+import Guild = require("../structures/Guild");
+
 export = class RoleDeleteListener extends Listener {
     constructor() {
         super("roleDelete", {
@@ -14,5 +16,23 @@ export = class RoleDeleteListener extends Listener {
     }
 
     exec = async (role: Role): Promise<void> => {
+        const guild = role.guild as Guild;
+
+        guild.createLog({
+            event: "roleDelete",
+            fields: [
+                {
+                    name: "Role Name",
+                    value: role.name,
+                    inline: true,
+                },
+                {
+                    name: "Created",
+                    value: role.createdAt.toUTCString(),
+                    inline: true,
+                },
+            ],
+            footer: role.managed ? "Managed" : undefined,
+        });
     }
 }
