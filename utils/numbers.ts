b23d2453374fa899fdf06cca1c4af12b634afc57
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-29 22:45:24 +0530

    utils: add numbers util functions
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ utils/numbers.ts
@@ -0,0 +1,24 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+const abbreviate = (number: number): string => {
+    const SI_PREFIX_SYMBOL = [ "", "k", "M", "G", "T", "P", "E", "Z", "Y" ];
+
+    // determine SI symbol
+    const symbolIndex = Math.log10(Math.abs(number)) / 3 | 0;
+
+    if (symbolIndex === 0) return number.toString();
+
+    // scale the number
+    const scaled = number / Math.pow(10, symbolIndex * 3);
+
+    // format number and add si symbol as suffix
+    return scaled.toFixed(3).replace(/(?:\.0+|0+)$/, "") + SI_PREFIX_SYMBOL[symbolIndex];
+};
+
+
+export {
+    abbreviate,
+};
