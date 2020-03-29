59777f85951d81b25f19b45a629254800731da3f
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-29 16:41:32 +0530

    listeners(inviteCreate): log invite create events
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/inviteCreate.ts
+++ listeners/inviteCreate.ts
@@ -6,6 +6,9 @@
 import { Listener, Constants } from "tesseract";
 import { Invite } from "discord.js";
 
+import Guild = require("../structures/Guild");
+import * as duration from "../utils/duration";
+
 export = class InviteCreateListener extends Listener {
     constructor() {
         super("inviteCreate", {
@@ -14,5 +17,50 @@ export = class InviteCreateListener extends Listener {
     }
 
     exec = async (invite: Invite): Promise<void> => {
+        const guild = invite.guild as Guild;
+
+        const fields = [
+            {
+                name: "Invite Code",
+                value: invite.code,
+                inline: true,
+            },
+            {
+                name: "Invite Channel",
+                value: invite.channel,
+                inline: true,
+            },
+        ];
+
+        if (invite.inviter) {
+            fields.push({
+                name: "Inviter",
+                value: invite.inviter.tag,
+                inline: true,
+            });
+        }
+
+        if (invite.maxUses) {
+            fields.push({
+                name: "Max Uses",
+                value: invite.maxUses.toString(),
+                inline: true,
+            });
+        }
+
+        if (invite.maxAge) {
+            fields.push({
+                name: "Expires",
+                value: duration.humanize(duration.between(invite.maxAge)),
+                inline: true,
+            });
+        }
+
+        guild.createLog({
+            event: "inviteCreate",
+            fields,
+            footer: invite.temporary ? "Temporary Membership" : undefined,
+            timestamp: invite.createdTimestamp,
+        });
     }
 }
