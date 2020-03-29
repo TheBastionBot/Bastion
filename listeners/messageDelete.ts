d7522a7d1e3679aae1725b55c03ad28fb2ef7ba5
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-29 17:52:59 +0530

    listeners(messageDelete): log message delete events
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/messageDelete.ts
+++ listeners/messageDelete.ts
@@ -4,7 +4,9 @@
  */
 
 import { Listener, Constants } from "tesseract";
-import { Message } from "discord.js";
+import { DMChannel, Message } from "discord.js";
+
+import Guild = require("../structures/Guild");
 
 export = class MessageDeleteListener extends Listener {
     constructor() {
@@ -14,6 +16,41 @@ export = class MessageDeleteListener extends Listener {
     }
 
     exec = async (message: Message): Promise<void> => {
-        console.log(message.content);
+        if (message.channel instanceof DMChannel) return;
+
+        const guild = message.guild as Guild;
+
+        if (message.type !== "DEFAULT") return;
+
+        guild.createLog({
+            event: "messageDelete",
+            fields: [
+                {
+                    name: "Channel",
+                    value: message.channel.name,
+                    inline: true,
+                },
+                {
+                    name: "Channel ID",
+                    value: message.channel.id,
+                    inline: true,
+                },
+                {
+                    name: "Author",
+                    value: message.author.tag,
+                    inline: true,
+                },
+                {
+                    name: "Author ID",
+                    value: message.author.id,
+                    inline: true,
+                },
+                {
+                    name: "Sent",
+                    value: message.createdAt.toUTCString(),
+                    inline: true,
+                },
+            ],
+        });
     }
 }
