9623621a2850732753a5c0be89e8478d921b8089
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-02 23:51:56 +0530

    commands(moderation): add voiceMute command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/moderation/voiceMute.ts
@@ -0,0 +1,84 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments } from "tesseract";
+import { Constants, Message } from "discord.js";
+
+import * as errors from "../../utils/errors";
+
+import BastionGuildMember = require("../../structures/GuildMember");
+
+export = class VoiceMute extends Command {
+    constructor() {
+        super("voiceMute", {
+            description: "",
+            triggers: [],
+            arguments: {
+                configuration: {
+                    "negation-prefix": "un",
+                },
+                alias: {
+                    user: "u",
+                },
+                boolean: [ "set" ],
+                string: [ "user" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [ "MUTE_MEMBERS" ],
+            userPermissions: [ "MUTE_MEMBERS" ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<unknown> => {
+        // Resolve member
+        const member = this.client.resolver.resolveGuildMember(message.guild, argv.user);
+
+        // Command Syntax Validation
+        if (!member) throw new errors.CommandSyntaxError(this.name);
+
+        // Check command user's permission over target member
+        if (message.author.id !== message.guild.ownerID && !(message.member as BastionGuildMember).canManage(member)) {
+            return await message.channel.send({
+                embed: {
+                    color: Constants.Colors.RED,
+                    title: this.client.locale.getString("en_us", "errors", "unauthorized"),
+                    description: this.client.locale.getString("en_us", "errors", "rolePosition", message.author.tag, member.user.tag),
+                },
+            }).catch(() => {
+                // This error can be ignored.
+            });
+        }
+
+        const reason = argv._.join(" ") || "-";
+
+        // Set Voice Mute
+        await member.voice.setMute(!!argv.set, reason);
+
+        // Acknowledgement
+        await message.channel.send({
+            embed: {
+                color: Constants.Colors.DARK_BUT_NOT_BLACK,
+                description: argv.set
+                    ? this.client.locale.getString("en_us", "info", "memberSetVoiceMute", message.author.tag, member.user.tag)
+                    : this.client.locale.getString("en_us", "info", "memberUnsetVoiceMute", message.author.tag, member.user.tag),
+                fields: [
+                    {
+                        name: "Reason",
+                        value: reason,
+                    },
+                ],
+                footer: {
+                    text: member.id,
+                },
+            },
+        }).catch(() => {
+            // This error can be ignored.
+        });
+    }
+}
