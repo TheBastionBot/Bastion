437e9dcbe44557c0bfc405753e7e435e5fbc825b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-10 05:33:26 +0530

    events: add commandHelp event
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ events/commandHelp.ts
@@ -0,0 +1,54 @@
+import { Command, Constants, ModuleManagerEvent } from "tesseract";
+import { Message } from "discord.js";
+
+import * as arrays from "../utils/arrays";
+import * as strings from "../utils/strings";
+
+export = class CommandHelpEvent extends ModuleManagerEvent {
+    constructor() {
+        super("commandHelp");
+    }
+
+    exec = async (message: Message, command: Command): Promise<void> => {
+        message.channel.send({
+            embed: {
+                color: Constants.COLORS.YELLOW,
+                fields: [
+                    {
+                        name: "Command",
+                        value: `\`${command.name}\``,
+                        inline: true
+                    },
+                    {
+                        name: "Aliases",
+                        value: command.triggers.length ? arrays.wrap(command.triggers, "`").join("\n") : "-",
+                        inline: true
+                    },
+                    {
+                        name: "Category",
+                        value: strings.snakeToTitleCase(command.category),
+                        inline: true
+                    },
+                    {
+                        name: "Description",
+                        value: command.description || "-",
+                    },
+                    {
+                        name: "Bastion Permissions",
+                        value: command.clientPermissions.length ? command.clientPermissions.map(c => strings.snakeToTitleCase(c.toString())).join("\n") : "-",
+                        inline: true
+                    },
+                    {
+                        name: "Human Permissions",
+                        value: command.owner ? "Bot Owner" : command.userPermissions.length ? command.userPermissions.map(c => strings.snakeToTitleCase(c.toString())).join("\n") : "-",
+                        inline: true
+                    },
+                    {
+                        name: "Syntax",
+                        value: command.syntax.length ? arrays.wrap(command.syntax, "```").join("") : "```" + command.name  + "```",
+                    },
+                ],
+            },
+        });
+    }
+}
