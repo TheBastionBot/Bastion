6ef7a672177012e7c9651ed5159ae18ca128bd69
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-12 04:40:24 +0530

    listeners(message): implement DM relay
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/message.ts
+++ listeners/message.ts
@@ -4,7 +4,9 @@
  */
 
 import { Listener, Constants } from "tesseract";
-import { Message } from "discord.js";
+import { ClientApplication, DMChannel, Message, Team, User } from "discord.js";
+
+import ConfigModel from "../models/Config";
 
 export = class MessageListener extends Listener {
     constructor() {
@@ -14,6 +16,30 @@ export = class MessageListener extends Listener {
     }
 
     exec = async (message: Message): Promise<void> => {
-        console.log(message.content);
+        if (message.channel instanceof DMChannel) {
+            const config = await ConfigModel.findById(this.client.user.id);
+
+            if (config.relayDirectMessages && message.content) {
+                // get the application owner
+                const app: ClientApplication = await this.client.fetchApplication();
+                const owner: User = app.owner instanceof Team ? app.owner.owner.user : app.owner;
+
+                const channel: DMChannel = await owner.createDM();
+
+                // relay the message to the application owner
+                await channel.send({
+                    embed: {
+                        color: Constants.COLORS.IRIS,
+                        author: {
+                            name: message.author.tag
+                        },
+                        description: message.content,
+                        footer: {
+                            text: message.author.id,
+                        },
+                    },
+                });
+            }
+        }
     }
 }
