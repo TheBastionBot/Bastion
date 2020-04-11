d701ca04a290dfeeb9d8677339a48ce7e6f7d8e1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-11 21:03:32 +0530

    commands(bot_owner): add announce command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/bot_owner/announce.ts
@@ -0,0 +1,81 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message, NewsChannel, TextChannel } from "discord.js";
+
+import GuildModel from "../../models/Guild";
+import * as errors from "../../utils/errors";
+
+export = class Announce extends Command {
+    constructor() {
+        super("announce", {
+            description: "It allows you to send announcements to the announcement channels in the servers where it has been set.",
+            triggers: [],
+            arguments: {
+                array: [ "message", "title" ],
+                string: [ "message", "title" ],
+            },
+            scope: "guild",
+            owner: true,
+            typing: false,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "announce --message MESSAGE",
+                "announce --title TITLE --message MESSAGE",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const announcementTitle = argv.title && argv.title.join(" ");
+        const announcementMessage = argv.message && argv.message.join(" ");
+
+        // command syntax validation
+        if (!announcementMessage) throw new errors.CommandSyntaxError(this.name);
+
+        const guilds = await GuildModel.find({ announcementsChannelId: { $ne: null } });
+
+        for (const guild of guilds) {
+            if (this.client.channels.cache.has(guild.announcementsChannelId)) {
+                const announcementChannel = this.client.channels.cache.get(guild.announcementsChannelId) as TextChannel | NewsChannel;
+
+                // announce
+                announcementChannel.send({
+                    embed: {
+                        color: Constants.COLORS.IRIS,
+                        title: announcementTitle,
+                        description: announcementMessage,
+                        footer: {
+                            text: "📣 " + message.author.tag + " / " + message.author.id,
+                        },
+                    },
+                }).catch(() => {
+                    // this error can be ignored.
+                });
+            }
+        }
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.PUPIL,
+                author: {
+                    name: "Announced",
+                },
+                title: announcementTitle,
+                description: announcementMessage,
+                footer: {
+                    text: "📣 " + message.author.tag + " / " + message.author.id,
+                },
+            },
+        }).catch(() => {
+            // this error can be ignored.
+        });
+    }
+}
