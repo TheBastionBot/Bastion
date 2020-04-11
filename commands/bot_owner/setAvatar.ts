c86b301d366291b5c83049abab6c19975e027887
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-11 21:03:34 +0530

    commands(bot_owner): add setAvatar command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/bot_owner/setAvatar.ts
@@ -0,0 +1,59 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import * as errors from "../../utils/errors";
+
+export = class SetAvatar extends Command {
+    constructor() {
+        super("setAvatar", {
+            description: "It allows you to update Bastion's avatar directly from Discord.",
+            triggers: [],
+            arguments: {
+                coerce: {
+                    image: Constants.ArgumentTypes.URL,
+                },
+            },
+            scope: "guild",
+            owner: true,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "setAvatar --image LINK",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const image = message.attachments.first();
+
+        // command syntax validation
+        if (!argv.image || !(image.height && image.width)) throw new errors.CommandSyntaxError(this.name);
+
+        // update avatar
+        await this.client.user.setAvatar(argv.image);
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.PUPIL,
+                description: this.client.locale.getString("en_us", "info", "updateAvatar", message.author.tag),
+                image: {
+                    url: this.client.user.displayAvatarURL({
+                        dynamic: true,
+                        size: 256,
+                    }),
+                },
+            },
+        }).catch(() => {
+            // this error can be ignored.
+        });
+    }
+}
