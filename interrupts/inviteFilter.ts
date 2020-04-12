b6b0444ab450c7170cf26b7b5dbc417dd625b48b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-12 22:50:03 +0530

    interrupts: add inviteFilter interrupt
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ interrupts/inviteFilter.ts
@@ -0,0 +1,52 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Interrupt } from "tesseract";
+import { Message } from "discord.js";
+
+import BastionGuild = require("../structures/Guild");
+
+import * as regex from "../utils/regex";
+
+export = class InviteFilter extends Interrupt {
+    constructor() {
+        super("inviteFilter");
+    }
+
+    deleteInvite = (message: Message): void => {
+        // delete the message
+        if (message.deletable) message.delete().catch(() => {
+            // this error can be ignored
+        });
+    }
+
+    exec = async (message: Message): Promise<boolean> => {
+        if (!message.content || !message.content.length) return false;
+
+        const guild = message.guild as BastionGuild;
+
+        // check whether the member has permission to manage messages
+        if (message.member.hasPermission("MANAGE_MESSAGES")) return false;
+
+        // check whether invite filter is enabled
+        if (!guild.document.filters || !guild.document.filters.inviteFilter || !guild.document.filters.inviteFilter.enabled) return false;
+
+        // check whether any whitelist exists
+        if (guild.document.filters.inviteFilter.whitelist) {
+            // check whether the channel is whitelisted
+            if (guild.document.filters.inviteFilter.whitelist.includes(message.channel.id)) return false;
+
+            // check whether any of the member's roles are whitelisted
+            if (guild.document.filters.inviteFilter.whitelist.some(id => message.member.roles.cache.has(id))) return false;
+        }
+
+        // TODO: follow redirects if it's there's an URL
+        // check whether the message has invite
+        if (regex.SERVER_INVITE.test(message.content)) {
+            this.deleteInvite(message);
+            return true;
+        }
+    }
+}
