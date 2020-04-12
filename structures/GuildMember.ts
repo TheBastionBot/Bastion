415da7193e05082932ba0a0ff20c7a11a8c95216
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-13 01:06:46 +0530

    structures(GuildMember): add addInfraction method
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- structures/GuildMember.ts
+++ structures/GuildMember.ts
@@ -3,7 +3,7 @@
  * @copyright 2020 - The Bastion Bot Project
  */
 
-import { Client } from "tesseract";
+import { Client, Constants } from "tesseract";
 import { Guild, GuildMember } from "discord.js";
 import * as mongoose from "mongoose";
 
@@ -36,6 +36,65 @@ export = class BastionGuildMember extends GuildMember {
     public isMusicMaster(): boolean {
         if ((this.user as BastionUser).isOwner()) return true;
         return (this.guild as BastionGuild).document.music && (this.guild as BastionGuild).document.music.roleId && this.roles.cache.has((this.guild as BastionGuild).document.music.roleId);
+    }
+
+    public async addInfraction(message: string): Promise<IGuildMember & mongoose.Document> {
+        const member = this.document ? this.document : await this.getDocument();
+
+        // add infraction to member
+        if (member.infractions) {
+            member.infractions = [ message ];
+        } else {
+            member.infractions.push(message);
+        }
+
+
+        const guild = (this.guild as BastionGuild).document
+            ? (this.guild as BastionGuild).document
+            : await (this.guild as BastionGuild).getDocument();
+
+        // check guild infraction thresholds and take action
+        if (guild.infractions) {
+            const count = member.infractions.length;
+            let action: string;
+
+            // check whether member's infractions require kick
+            if (this.kickable && member.infractions.length === guild.infractions.kickThreshold) {
+                action = "Kick";
+
+                await this.kick(member.infractions.length + " infractions.");
+            }
+
+            // check whether member's infractions require ban
+            if (this.bannable && member.infractions.length === guild.infractions.banThreshold) {
+                action = "Ban";
+
+                await this.ban({
+                    reason: member.infractions.length + " infractions.",
+                });
+
+                // clear all infractions once member is banned
+                member.infractions = undefined;
+                delete member.infractions;
+            }
+
+
+            if (action) {
+                // message the user regarding the infraction action
+                await this.send({
+                    embed: {
+                        color: Constants.COLORS.ORANGE,
+                        description: (this.client as Client).locale.getString(guild.language, "info", "memberInfractions" + action, this.guild.name, count),
+                    },
+                }).catch(() => {
+                    // this error can be ignored
+                });
+            }
+        }
+
+
+        // save member
+        return member.save();
     }
 
     public async clearInfractions(): Promise<IGuildMember & mongoose.Document> {
