bd010f7b7705badbbf5d87a79f33c6546037ec4a
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-12 22:51:43 +0530

    interrupts: add link filter interrupt
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ interrupts/linkFilter.ts
@@ -0,0 +1,51 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Interrupt } from "tesseract";
+import { Message } from "discord.js";
+
+import BastionGuild = require("../structures/Guild");
+
+import * as regex from "../utils/regex";
+
+export = class LinkFilter extends Interrupt {
+    constructor() {
+        super("linkFilter");
+    }
+
+    deleteLink = (message: Message): void => {
+        // delete the message
+        if (message.deletable) message.delete().catch(() => {
+            // this error can be ignored
+        });
+    }
+
+    exec = async (message: Message): Promise<boolean> => {
+        if (!message.content || !message.content.length) return false;
+
+        const guild = message.guild as BastionGuild;
+
+        // check whether the member has permission to manage messages
+        if (message.member.hasPermission("MANAGE_MESSAGES")) return false;
+
+        // check whether link filter is enabled
+        if (!guild.document.filters || !guild.document.filters.linkFilter || !guild.document.filters.linkFilter.enabled) return false;
+
+        // check whether any whitelist exists
+        if (guild.document.filters.linkFilter.whitelist) {
+            // check whether the channel is whitelisted
+            if (guild.document.filters.linkFilter.whitelist.includes(message.channel.id)) return false;
+
+            // check whether any of the member's roles are whitelisted
+            if (guild.document.filters.linkFilter.whitelist.some(id => message.member.roles.cache.has(id))) return false;
+        }
+
+        // check whether the message has an uri
+        if (regex.URI.test(message.content)) {
+            this.deleteLink(message);
+            return true;
+        }
+    }
+}
