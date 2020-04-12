aeba0309f01c0e379c4756837ccf7a5910e8c12e
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-13 05:10:10 +0530

    utils(strings): add toTitleCase & toSnakeCase functions
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- utils/strings.ts
+++ utils/strings.ts
@@ -3,11 +3,23 @@
  * @copyright 2020 - The Bastion Bot Project
  */
 
+const toTitleCase = (string: string, splitSeparator: string = " ", joinSeprator: string = " "): string => {
+    return string.split(splitSeparator).map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()).join(joinSeprator);
+};
+
+const toSnakeCase = (string: string, upperCase?: boolean): string => {
+    const snakeString = string.split(" ").join("_");
+    return upperCase ? snakeString.toUpperCase() : snakeString.toLowerCase();
+};
+
 const snakeToTitleCase = (string: string, separator = " "): string => {
-    return string.split("_").map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()).join(separator);
+    return toTitleCase(string, "_", separator);
+    // return string.split("_").map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()).join(separator);
 };
 
 
 export {
+    toTitleCase,
+    toSnakeCase,
     snakeToTitleCase,
 };
