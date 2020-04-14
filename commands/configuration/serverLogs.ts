980f3d2d268bd8cdae15ac37e2d30b8b03263f80
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-14 11:41:28 +0530

    commands(configurations): add serverLogs command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/configuration/serverLogs.ts
@@ -0,0 +1,61 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import BastionGuild = require("../../structures/Guild");
+
+
+export = class ServerLogs extends Command {
+    constructor() {
+        super("serverLogs", {
+            description: "It allows you to enable (and disable) logging of the server events. It sets the channel as the Server Log Channel that will log the server events.",
+            triggers: [],
+            arguments: {
+                alias: {
+                    disable: [ "d" ],
+                },
+                boolean: [ "disable" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [ "MANAGE_GUILD" ],
+            syntax: [
+                "serverLogs",
+                "serverLogs --disable",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const guild = (message.guild as BastionGuild);
+
+        // update the server logs channel
+        if (argv.disable) {
+            guild.document.serverLogChannelId = undefined;
+            delete guild.document.serverLogChannelId;
+        } else {
+            guild.document.serverLogChannelId = message.channel.id;
+        }
+
+        // save document
+        await guild.document.save();
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: guild.document.serverLogChannelId ? Constants.COLORS.GREEN : Constants.COLORS.RED,
+                description: this.client.locale.getString("en_us", "info", guild.document.serverLogChannelId ? "serverLogsEnable" : "serverLogsDisable", message.author.tag),
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
