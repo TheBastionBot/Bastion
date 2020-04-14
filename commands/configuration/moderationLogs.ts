dbc29d5e1893940c5fe59b2fc7eae5a35f5abb50
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-14 11:54:25 +0530

    commands(configurations): add moderationLogs command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/configuration/moderationLogs.ts
@@ -0,0 +1,61 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import BastionGuild = require("../../structures/Guild");
+
+
+export = class ModerationLogs extends Command {
+    constructor() {
+        super("moderationLogs", {
+            description: "It allows you to enable (and disable) logging of the moderation events in the server. It sets the channel as the Moderation Log Channel that will log the moderation events in the server.",
+            triggers: [ "modLogs" ],
+            arguments: {
+                alias: {
+                    disable: [ "d" ],
+                },
+                boolean: [ "disable" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [ "MANAGE_GUILD" ],
+            syntax: [
+                "moderationLogs",
+                "moderationLogs --disable",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const guild = (message.guild as BastionGuild);
+
+        // update the moderation logs channel
+        if (argv.disable) {
+            guild.document.moderationLogChannelId = undefined;
+            delete guild.document.moderationLogChannelId;
+        } else {
+            guild.document.moderationLogChannelId = message.channel.id;
+        }
+
+        // save document
+        await guild.document.save();
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: guild.document.moderationLogChannelId ? Constants.COLORS.GREEN : Constants.COLORS.RED,
+                description: this.client.locale.getString("en_us", "info", guild.document.moderationLogChannelId ? "moderationLogsEnable" : "moderationLogsDisable", message.author.tag),
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
