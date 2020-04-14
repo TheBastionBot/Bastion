b2ea0b32e25614d130c5a6854b391b8b6e715c38
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-14 22:50:55 +0530

    commands(configurations): add languages command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/configuration/languages.ts
@@ -0,0 +1,86 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import * as arrays from "../../utils/arrays";
+
+import BastionGuild = require("../../structures/Guild");
+
+export = class Languages extends Command {
+    constructor() {
+        super("languages", {
+            description: "It allows you to see the languages available in Bastion and set one of them as the default language for your server.",
+            triggers: [],
+            arguments: {
+                alias: {
+                    set: [ "s" ],
+                },
+                string: [ "set" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [ "MANAGE_GUILD" ],
+            syntax: [
+                "languages",
+                "languages --set LANGUAGE",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<unknown> => {
+        const guild = (message.guild as BastionGuild);
+
+        // get the available locales
+        const locales = this.client.locale.getLocales().map(l => l.toUpperCase());
+
+        if (argv.set) {
+            const language: string = argv.set.toLowerCase();
+
+            // check whether the language is available
+            if (!locales.includes(language)) {
+                throw new Error(this.client.locale.getString("en_us", "errors", "languageUnavailable"));
+            }
+
+            // set guild language
+            guild.document.language = language;
+
+            // save document
+            await guild.document.save();
+
+            // acknowledge
+            return await message.channel.send({
+                embed: {
+                    color: Constants.COLORS.GREEN,
+                    description: this.client.locale.getString("en_us", "info", "guildLanguageUpdate", message.author.tag, language.toUpperCase()),
+                },
+            }).catch(() => {
+                // this error can be ignored
+            });
+        }
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                title: "Available Languages",
+                description: this.client.locale.getString("en_us", "info", "availableLocales"),
+                fields: [
+                    {
+                        name: "Languages",
+                        value: arrays.toBulletList(locales),
+                    },
+                ],
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
