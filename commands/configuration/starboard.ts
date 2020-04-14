f6e829dc732ae22a97babbd3072c38486b776a30
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-14 23:53:50 +0530

    commands(configurations): add starboard command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/configuration/starboard.ts
@@ -0,0 +1,61 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import BastionGuild = require("../../structures/Guild");
+
+
+export = class Starboard extends Command {
+    constructor() {
+        super("starboard", {
+            description: "It allows you to enable (and disable) Starboard in the server. It sets the channel as the Starboard Channel that will log the starred messages of the server.",
+            triggers: [],
+            arguments: {
+                alias: {
+                    disable: [ "d" ],
+                },
+                boolean: [ "disable" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [ "MANAGE_GUILD" ],
+            syntax: [
+                "starboard",
+                "starboard --disable",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const guild = (message.guild as BastionGuild);
+
+        // update the starboard channel
+        if (argv.disable) {
+            guild.document.starboardChannelId = undefined;
+            delete guild.document.starboardChannelId;
+        } else {
+            guild.document.starboardChannelId = message.channel.id;
+        }
+
+        // save document
+        await guild.document.save();
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: guild.document.starboardChannelId ? Constants.COLORS.GREEN : Constants.COLORS.RED,
+                description: this.client.locale.getString("en_us", "info", guild.document.starboardChannelId ? "starboardEnable" : "starboardDisable", message.author.tag),
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
