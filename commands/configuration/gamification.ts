024be771673706833b52380cb28ef896eeac59e9
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-14 10:53:09 +0530

    commands(gamification): refactor
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/configuration/gamification.ts
+++ commands/configuration/gamification.ts
@@ -45,7 +45,7 @@ export = class Gamification extends Command {
             // enable level up messages if specified, otherwise keep the old value based on whether gamification is enabled
             messages: argv.messages ? true : status ? guild.document.gamification && guild.document.gamification.messages : undefined,
             // set the level up modifier if specified, otherwise keep the old value based on whether gamification is enabled
-            modifier: argv.modifier ? Number(argv.modifier) : status ? guild.document.gamification && guild.document.gamification.modifier : undefined,
+            modifier: typeof argv.modifier === "number" ? argv.modifier : status ? guild.document.gamification && guild.document.gamification.modifier : undefined,
         };
 
         // save document
@@ -57,22 +57,22 @@ export = class Gamification extends Command {
                 color: guild.document.gamification.enabled ? Constants.COLORS.GREEN : Constants.COLORS.RED,
                 description: this.client.locale.getString("en_us", "info", guild.document.gamification.enabled ? "gamificationEnable" : "gamificationDisable", message.author.tag),
                 fields: guild.document.gamification.enabled
-                ? [
-                    {
-                        name: "Level-Up Messages",
-                        value: guild.document.gamification.messages ? "Enabled" : "Disabled",
-                        inline: true,
-                    },
-                    {
-                        name: "Level-Up Modifier",
-                        value: guild.document.gamification.modifier ? guild.document.gamification.modifier.toString() : gamification.DEFAUL_LEVELUP_MODIFIER.toString(),
-                        inline: true,
-                    },
-                  ]
-                : [],
+                    ? [
+                        {
+                            name: "Level-Up Messages",
+                            value: guild.document.gamification.messages ? "Enabled" : "Disabled",
+                            inline: true,
+                        },
+                        {
+                            name: "Level-Up Modifier",
+                            value: guild.document.gamification.modifier ? guild.document.gamification.modifier.toString() : gamification.DEFAUL_LEVELUP_MODIFIER.toString(),
+                            inline: true,
+                        },
+                    ]
+                    : [],
             },
         }).catch(() => {
-            // This error can be ignored.
+            // this error can be ignored
         });
     }
 }
