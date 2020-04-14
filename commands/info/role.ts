f681dabc6b2fd6de66d286592caf82a539f4e86e
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-15 04:52:57 +0530

    commands(info): add role command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/info/role.ts
@@ -0,0 +1,76 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments } from "tesseract";
+import { Message } from "discord.js";
+
+import * as strings from "../../utils/strings";
+import * as errors from "../../utils/errors";
+
+export = class RoleCommand extends Command {
+    constructor() {
+        super("role", {
+            description: "It allows you see the information of a role.",
+            triggers: [],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "role -- ROLE",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const identifier: string = argv._.join(" ");
+
+        // identify the role
+        const role = this.client.resolver.resolveRole(message.guild, identifier);
+
+        if (!role) throw new errors.RoleNotFound(this.client.locale.getString("en_us", "errors", "roleNotFound"));
+
+        // acknowledge
+        message.channel.send({
+            embed: {
+                color: role.color,
+                author: {
+                    name: role.name,
+                },
+                title: (role.managed ? "Managed" : "") + " Role",
+                fields: [
+                    {
+                        name: "Position",
+                        value: (message.guild.roles.cache.size - role.rawPosition).toString(),
+                        inline: true,
+                    },
+                    {
+                        name: "Members",
+                        value: role.members.size + " Members",
+                        inline: true,
+                    },
+                    {
+                        name: "Created",
+                        value: role.createdAt.toUTCString(),
+                        inline: true,
+                    },
+                    {
+                        name: "Permissions",
+                        value: role.permissions.bitfield ? role.permissions.toArray().map(p => strings.snakeToTitleCase(p)).join(", ") : "-",
+                    },
+                ],
+                footer: {
+                    text: (role.hoist ? "Hoisted • " : "") + role.id,
+                },
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
