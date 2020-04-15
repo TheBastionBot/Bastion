c7462f8ef9a4dd79d374e71ef6f3f5ad1615662a
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-15 09:47:06 +0530

    commands(configurations): add inviteFilter command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/configuration/inviteFilter.ts
@@ -0,0 +1,69 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import BastionGuild = require("../../structures/Guild");
+
+export = class InviteFilterCommand extends Command {
+    constructor() {
+        super("inviteFilter", {
+            description: "It allows you to enable (and disable) Invite Filter in the server. And it also allows you to configure if violation of this filter should be considered an infraction.",
+            triggers: [],
+            arguments: {
+                alias: {
+                    disable: [ "d" ],
+                    enable: [ "e" ],
+                    infraction: [ "i" ],
+                },
+                boolean: [ "disable", "enable", "infraction" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [ "MANAGE_GUILD" ],
+            syntax: [
+                "inviteFilter",
+                "inviteFilter --enable",
+                "inviteFilter --disable",
+                "inviteFilter --infraction",
+                "inviteFilter --no-infraction",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const guild = (message.guild as BastionGuild);
+
+        // update invite filter settings
+        guild.document.filters = {
+            ...guild.document.filters,
+            inviteFilter: {
+                enabled: argv.disable ? undefined : argv.enable || guild.document.filters && guild.document.filters.inviteFilter && guild.document.filters.inviteFilter.enabled ? true : undefined,
+                infraction: argv.infraction === false ? undefined : argv.infraction || guild.document.filters && guild.document.filters.inviteFilter && guild.document.filters.inviteFilter.infraction ? true : undefined,
+            },
+        };
+
+        // save document
+        await guild.document.save();
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: argv.enable ? Constants.COLORS.GREEN : argv.disable ? Constants.COLORS.RED : Constants.COLORS.IRIS,
+                description: this.client.locale.getString("en_us", "info", argv.enable ? "inviteFilterEnable" : argv.disable ? "inviteFilterDisable" : guild.document.filters.inviteFilter.enabled ? "inviteFilterEnabled" : "inviteFilterDisabled", message.author.tag),
+                footer: {
+                    text: guild.document.filters.inviteFilter.enabled ? this.client.locale.getString("en_us", "info", guild.document.filters.inviteFilter.infraction ? "filterInfractionEnabled" : "filterInfractionDisabled") : "",
+                },
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
