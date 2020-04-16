ce7376a4c8286f9da5bfe5910aa27fe78dc94706
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-16 09:54:59 +0530

    utils: add progress util function
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ utils/progress.ts
@@ -0,0 +1,19 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import * as numbers from "./numbers";
+
+export default (percentage = 0, steps = 10): string => {
+    percentage = numbers.clamp(percentage, 0, 100);
+
+    const markers = {
+        filled: "#",
+        unfilled: "-",
+    };
+
+    const progress = Math.round((percentage / 100) * steps);
+
+    return "[" + markers.filled.repeat(progress) + markers.unfilled.repeat(steps - progress) + "]";
+};
