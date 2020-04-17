c89aa66e1d411cde5fe173959a9c61938e2cb2de
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-18 05:28:13 +0530

    commands(mentionSpam): fix message embed color
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/configuration/mentionSpam.ts
+++ commands/configuration/mentionSpam.ts
@@ -48,7 +48,7 @@ export = class MentionSpamCommand extends Command {
         // acknowledge
         await message.channel.send({
             embed: {
-                color: argv.enable ? Constants.COLORS.GREEN : argv.disable ? Constants.COLORS.RED : Constants.COLORS.IRIS,
+                color: guild.document.mentionSpam.threshold ? Constants.COLORS.GREEN : Constants.COLORS.RED,
                 description: this.client.locale.getString("en_us", "info", guild.document.mentionSpam.threshold ? "mentionSpamInfractionsEnable" : "mentionSpamInfractionsDisable", message.author.tag, guild.document.mentionSpam.threshold),
             },
         }).catch(() => {
