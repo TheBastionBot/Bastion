4f573f5d7ac6ed87c39768e012fd4a63069115a0
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-18 03:24:14 +0530

    commands(configurations): add mentionSpam command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/configuration/mentionSpam.ts
@@ -0,0 +1,58 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import BastionGuild = require("../../structures/Guild");
+
+export = class MentionSpamCommand extends Command {
+    constructor() {
+        super("mentionSpam", {
+            description: "It allows you to set (and unset) the threshold for mention spam in the server. If set, Bastion will warn the users who violate this and add it to their infractions.",
+            triggers: [],
+            arguments: {
+                alias: {
+                    threshold: [ "t" ],
+                },
+                number: [ "threshold" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [ "MANAGE_GUILD" ],
+            syntax: [
+                "mentionSpam",
+                "mentionSpam --threshold NUMBER",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        const guild = (message.guild as BastionGuild);
+
+        // update mention spam settings
+        guild.document.mentionSpam = {
+            ...guild.document.mentionSpam,
+            threshold: typeof argv.threshold === "number" && argv.threshold > 1 ? argv.threshold : undefined,
+        };
+
+        // save document
+        await guild.document.save();
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: argv.enable ? Constants.COLORS.GREEN : argv.disable ? Constants.COLORS.RED : Constants.COLORS.IRIS,
+                description: this.client.locale.getString("en_us", "info", guild.document.mentionSpam.threshold ? "mentionSpamInfractionsEnable" : "mentionSpamInfractionsDisable", message.author.tag, guild.document.mentionSpam.threshold),
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
