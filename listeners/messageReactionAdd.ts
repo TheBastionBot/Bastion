3cd70c43cc4137d5dca01c4e380f8246c1eab52b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-19 01:12:50 +0530

    listeners(messageReactionAdd): implement reaction role assignment
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/messageReactionAdd.ts
+++ listeners/messageReactionAdd.ts
@@ -4,7 +4,11 @@
  */
 
 import { Listener, Constants } from "tesseract";
-import { MessageReaction } from "discord.js";
+import { MessageReaction, User } from "discord.js";
+
+import ReactionRoleGroup from "../models/ReactionRoleGroup";
+import RoleModel from "../models/Role";
+import * as emojis from "../utils/emojis";
 
 export = class MessageReactionAddListener extends Listener {
     constructor() {
@@ -13,6 +17,41 @@ export = class MessageReactionAddListener extends Listener {
         });
     }
 
-    exec = async (messageReaction: MessageReaction): Promise<void> => {
+    exec = async (messageReaction: MessageReaction, user: User): Promise<void> => {
+        if (!messageReaction.message.guild) return;
+
+        // identify the member
+        const member = messageReaction.message.guild.member(user);
+
+        // identify the reaction roles group for the reaction
+        const reactionRolesGroup = await ReactionRoleGroup.findById(messageReaction.message.id);
+
+        if (!reactionRolesGroup) return;
+
+        // build the reaction emoji
+        const emoji = messageReaction.emoji.id
+            ? "<" + (messageReaction.emoji.animated ? "a" : "") + ":" + messageReaction.emoji.name + ":" + messageReaction.emoji.id + ">"
+            : messageReaction.emoji.name;
+
+        const emojiObject = emojis.parseEmoji(emoji);
+
+        if (emojiObject) {
+            // find the role for this emoji in the reaction roles group
+            const reactionRole = await RoleModel.findOne({
+                $or: reactionRolesGroup.roles.map(id => ({ _id: id })),
+                guild: messageReaction.message.guild.id,
+                emoji: emojiObject.value,
+            });
+
+            if (!reactionRole) return;
+
+            // remove all other roles in this reaction roles group, if the roles are mutually exclusive
+            if (reactionRolesGroup.exclusive) {
+                await member.roles.remove(reactionRolesGroup.roles, "Auto Removed with Reaction Roles");
+            }
+
+            // add the reaction role
+            await member.roles.add(reactionRole._id, "Added with Reaction Roles");
+        }
     }
 }
