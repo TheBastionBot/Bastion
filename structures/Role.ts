d2e891edb6b9f0511177c9efb11f15143575844d
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-18 22:59:28 +0530

    structures(Role): add {fetch,create}Document method & remove getDocument
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- structures/Role.ts
+++ structures/Role.ts
@@ -10,11 +10,24 @@ import * as mongoose from "mongoose";
 import RoleModel, { Role as IRole } from "../models/Role";
 
 export = class BastionRole extends Role {
+    public document: IRole & mongoose.Document;
+
     constructor(client: Client, data: object, guild: Guild) {
         super(client, data, guild);
     }
 
-    public async getDocument(): Promise<IRole & mongoose.Document> {
-        return await RoleModel.findById(this.id);
+    public async fetchDocument(): Promise<IRole & mongoose.Document> {
+        this.document = await RoleModel.findById(this.id);
+        return this.document;
+    }
+
+    public async createDocument(role?: IRole): Promise<IRole & mongoose.Document> {
+        return await RoleModel.findByIdAndUpdate(this.id, {
+            guild: this.guild.id,
+            ...role,
+        }, {
+            upsert: true,
+            new: true,
+        }) as IRole & mongoose.Document;
     }
 }
