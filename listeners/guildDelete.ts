ec7ef454274922cfc0eb0ca0512e01f9a98057d5
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-19 18:56:06 +0530

    listeners(guildDelete): log guild delete events
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- listeners/guildDelete.ts
+++ listeners/guildDelete.ts
@@ -6,6 +6,8 @@
 import { Listener, Constants } from "tesseract";
 import { Guild } from "discord.js";
 
+import { BastionCredentials } from "../typings/settings";
+
 export = class GuildDeleteListener extends Listener {
     constructor() {
         super("guildDelete", {
@@ -14,5 +16,39 @@ export = class GuildDeleteListener extends Listener {
     }
 
     exec = async (guild: Guild): Promise<void> => {
+        const credentials = (this.client.credentials as BastionCredentials);
+
+        // log guild info when leaving a guild
+        if (credentials.bastion && credentials.bastion.webhookID && credentials.bastion.webhookToken) {
+            const bastionHook = await this.client.fetchWebhook(credentials.bastion.webhookID, credentials.bastion.webhookToken);
+
+            bastionHook.send({
+                username: "Bastion",
+                embeds: [
+                    {
+                        color: Constants.COLORS.RED,
+                        description: "I have left the " + guild.name + " server.",
+                        fields: [
+                            {
+                                name: "Server Owner",
+                                value: guild.owner.user.tag + " / " + guild.owner.user.id,
+                            },
+                        ],
+                        footer: {
+                            text: guild.id
+                        },
+                        thumbnail: {
+                            url: guild.icon ? guild.iconURL({
+                                dynamic: true,
+                                size: 512,
+                            }) : "",
+                        },
+                        timestamp: new Date(),
+                    }
+                ],
+            }).catch(() => {
+                // this error can be ignored
+            });
+        }
     }
 }
