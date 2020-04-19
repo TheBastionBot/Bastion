48c052eaba1d1dc715f67b2d5ae65d81c13d6c9b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-19 18:51:29 +0530

    typings(settings): update BastionCredentials
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- typings/settings.d.ts
+++ typings/settings.d.ts
@@ -1,6 +1,10 @@
 import { TesseractCredentials } from "tesseract/typings/utils/settings";
 
 interface BastionCredentials extends TesseractCredentials {
+    bastion: {
+        webhookID: string;
+        webhookToken: string;
+    };
     twitch: {
         clientId: string;
         clientSecret: string;
