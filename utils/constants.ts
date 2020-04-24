8b174297883fd2c66188370a94bf9718712f7725
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-25 01:41:34 +0530

    utils(constants): add COLORS constant
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,11 +5,19 @@
 
 import { User } from "discord.js";
 
+
+const COLORS = {
+    TWITCH: 0x9146ff,
+    YOUTUBE: 0xff0000,
+    MIXER: 0x1fbaed,
+};
+
 const isPublicBastion = (user: User): boolean => {
     return user.id === "267035345537728512";
 };
 
 
 export {
+    COLORS,
     isPublicBastion,
 };
