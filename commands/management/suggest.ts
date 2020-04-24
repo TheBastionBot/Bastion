e070529386643937382f12353ace18f7a01fd68b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-25 02:57:13 +0530

    commands(management): add suggest command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/management/suggest.ts
@@ -0,0 +1,77 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message, TextChannel } from "discord.js";
+
+import BastionGuild = require("../../structures/Guild");
+import * as errors from "../../utils/errors";
+
+export = class SuggestCommand extends Command {
+    constructor() {
+        super("suggest", {
+            description: "It allows you send a suggestion to the suggestion staff, provided a Suggestion Channel has been set in the server.",
+            triggers: [],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "suggest SUGGESTION",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        if (!argv._.length) throw new errors.CommandSyntaxError(this.name);
+
+        const guild = message.guild as BastionGuild;
+
+        // check whether suggestions channel is set and is available
+        if (!guild.document.suggestionsChannelId || !message.guild.channels.cache.has(guild.document.suggestionsChannelId)) throw new Error("NO_SUGGESTIONS_CHANNEL");
+
+        // identify the suggestion channel
+        const suggestionChannel = message.guild.channels.cache.get(guild.document.suggestionsChannelId) as TextChannel;
+
+        // send the suggestion
+        const suggestion = await suggestionChannel.send({
+            embed: {
+                color: Constants.COLORS.INDIGO,
+                author: {
+                    name: message.author.tag + " / " + message.author.id,
+                    iconURL: message.author.displayAvatarURL({
+                        dynamic: true,
+                        size: 64,
+                    }),
+                },
+                title: "Suggestion",
+                description: argv._.join(" "),
+            },
+        });
+
+        // add reaction to accept/reject the suggestion
+        suggestion.react("✅").catch(() => {
+            // this error can be ignored
+        });
+        suggestion.react("❎").catch(() => {
+            // this error can be ignored
+        });
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                description: "We've received your suggestion. We'll get back to you with updates.",
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+
+    }
+}
