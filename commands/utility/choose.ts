3fa9c3c4229d1bcc3ce23dc74eb8f08fed0a7c41
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-30 01:34:49 +0530

    commands(utility): add choose command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/utility/choose.ts
@@ -0,0 +1,45 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import * as errors from "../../utils/errors";
+
+export = class ChooseCommand extends Command {
+    constructor() {
+        super("choose", {
+            description: "It allows you to ask Bastion to choose an option from the given set of options. Let Bastion make a decision for you.",
+            triggers: [ "decide" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "choose -- OPTIONS...",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        // command syntax validation
+        if (!argv._.length) throw new errors.CommandSyntaxError(this.name);
+
+        const choice: string = argv._[Math.floor(Math.random() * argv._.length)];
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                title: "My choice would be...",
+                description: choice,
+            },
+        });
+    }
+}
