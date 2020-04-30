9298068e8c4143efd67c7f13bc6cda76736652c0
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-04-30 13:14:12 +0530

    models: add Giveaway model
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ models/Giveaway.ts
@@ -0,0 +1,42 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import * as mongoose from "mongoose";
+
+export interface Giveaway {
+    _id: string;
+    id?: string;
+    channel: string;
+    guild: string;
+    winners: number;
+    ends: Date;
+}
+
+const giveawaySchema = new mongoose.Schema<Giveaway>({
+    _id: {
+        type: String,
+        required: true,
+    },
+    channel: {
+        type: String,
+        required: true,
+        ref: "TextChannel",
+    },
+    guild: {
+        type: String,
+        required: true,
+        ref: "Guild",
+    },
+    winners: {
+        type: Number,
+    },
+    ends: {
+        type: Date,
+        required: true,
+        expires: 864e2,
+    },
+});
+
+export default mongoose.model<Giveaway & mongoose.Document>("Giveaway", giveawaySchema);
