d99b131bfd3b54d5d32f507c3779a1dff673f995
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-02 03:43:59 +0530

    commands(profile): add leaderboard command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/profile/leaderboard.ts
@@ -0,0 +1,80 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import MemberModel from "../../models/Member";
+
+export = class LeaderboardCommand extends Command {
+    constructor() {
+        super("leaderboard", {
+            description: "It allows you to see the leaderboard of your server. You're ranked based on your level, experience, karma, and Bastion Coins; in that exact order.",
+            triggers: [ "lb" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [],
+        });
+    }
+
+    exec = async (message: Message): Promise<void> => {
+        // fetch top 5 members
+        const members = await MemberModel.find({
+            guild: message.guild.id,
+        }, null, {
+            sort: {
+                level: -1,
+                experience: -1,
+                karma: -1,
+                balance: -1,
+            },
+            limit: 5,
+        });
+
+        // acknowledge
+        message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                author: {
+                    name: message.guild.name,
+                },
+                title: "Leaderboard",
+                fields: members.map((member, i) => [
+                    {
+                        name: "#" + (i + 1) + " - " + (this.client.users.cache.has(member.user) ? this.client.users.cache.get(member.user).tag + " / " : "") + member.user,
+                        value: member.balance + " Bastion Coins",
+                    },
+                    {
+                        name: "Level",
+                        value: member.level,
+                        inline: true,
+                    },
+                    {
+                        name: "XP",
+                        value: member.experience,
+                        inline: true,
+                    },
+                    {
+                        name: "Karma",
+                        value: member.karma,
+                        inline: true,
+                    },
+                ]),
+                thumbnail: {
+                    url: "https://i.imgur.com/Kzt8Ldk.png",
+                },
+                footer: {
+                    text: "Bastion Profiles - Leaderboard",
+                },
+            },
+        });
+    }
+}
