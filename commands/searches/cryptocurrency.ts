1e6b61c9dde77727cf0f11a8faea594e3734095b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-04 07:51:03 +0530

    commands(searches): add cryptocurrency command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/searches/cryptocurrency.ts
@@ -0,0 +1,77 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import * as errors from "../../utils/errors";
+import * as omnic from "../../utils/omnic";
+
+interface Currency {
+    name: string;
+    symbol: string;
+    description: string;
+    logo: string;
+    date_added: string;
+}
+
+export = class CryptocurrencyCommand extends Command {
+    constructor() {
+        super("cryptocurrency", {
+            description: "It allows you to see some information on the specified cryptocurrency.",
+            triggers: [],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "cryptocurrency SYMBOL",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        // Command Syntax Validation
+        if (!argv._.length) throw new errors.CommandSyntaxError(this.name);
+
+        // identify the cryptocurrency symbol
+        const symbol = argv._.join(" ");
+
+        // fetch data on the cryptocurrency
+        const response = await omnic.makeRequest("/currencies/crypto/" + encodeURIComponent(symbol));
+        const { status, data }: {
+            status: { error_code: number; error_message: string };
+            data: { [key: string]: Currency };
+        } = await response.json();
+
+        // check for errors
+        if (status.error_code !== 0) throw new Error(status.error_message);
+
+        const currency: Currency = Object.values(data)[0];
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                author: {
+                    name: "Cryptocurrency",
+                },
+                title: currency.name + " (" + currency.symbol + ")",
+                description: currency.description,
+                thumbnail: {
+                    url: currency.logo,
+                },
+                footer: {
+                    text: "Powered by CoinMarketCap • Added",
+                },
+                timestamp: new Date(currency.date_added),
+            },
+        });
+    }
+}
