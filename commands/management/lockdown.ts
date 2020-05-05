0e39c646fd3d2f9e7ba34d66280a49cc77aabb7e
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-05 21:41:13 +0530

    commands(managemnt): add lockdown command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/management/lockdown.ts
@@ -0,0 +1,56 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { GuildChannel, Message } from "discord.js";
+
+export = class LockdownCommand extends Command {
+    constructor() {
+        super("lockdown", {
+            description: "It allows you lockdown a channel in the server from users who don't have permission to manage messages or the channel.",
+            triggers: [],
+            arguments: {
+                alias: {
+                    remove: [ "r" ],
+                },
+                boolean: [ "remove" ],
+            },
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [ "MANAGE_CHANNELS" ],
+            userPermissions: [ "MANAGE_GUILD" ],
+            syntax: [
+                "lockdown",
+                "lockdown --remove",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        for (const permissionOverwrite of (message.channel as GuildChannel).permissionOverwrites.values()) {
+            // check whether the overwrite has permission to manage messages or the channel
+
+            if (permissionOverwrite.allow.has("MANAGE_MESSAGES") || permissionOverwrite.allow.has("MANAGE_CHANNELS")) continue;
+
+            // deny permission to send messages in the channel
+            permissionOverwrite.update({
+                SEND_MESSAGES: argv.remove ? null : false,
+            });
+        }
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: argv.remove ? Constants.COLORS.GREEN : Constants.COLORS.ORANGE,
+                description: this.client.locale.getString("en_us", "info", argv.remove ? "lockdownChannel" : "lockdownChannelRemove", message.author.tag),
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
