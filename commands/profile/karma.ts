c410af78f760b036897f731a12932a3c46391a9b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-05 17:09:51 +0530

    commands(profile): add karma command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/profile/karma.ts
@@ -0,0 +1,63 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import MemberModel from "../../models/Member";
+import * as errors from "../../utils/errors";
+
+export = class KarmaCommand extends Command {
+    constructor() {
+        super("karma", {
+            description: "It allows you to give someone the sweet little internet points, we call karma, if you think they're worth it. You can give up to 3 karmas every 6 hours, so spend it wisely.",
+            triggers: [ "rep" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 216e2,
+            ratelimit: 3,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "karma USER",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        if (!argv._.length) throw new errors.CommandSyntaxError(this.name);
+
+        // check whether the specified user is a server member
+        const member = this.client.resolver.resolveGuildMember(message.guild, argv._.join(" "));
+        if (!member) throw new Error(this.client.locale.getString("en_us", "errors", "memberNotFound"));
+
+        // check whether author is giving karma to themselves
+        if (message.author.id === member.id) throw new Error("NO_SELF_KARMA");
+
+        // get member's profile data
+        const memberProfile = await MemberModel.findOne({ user: member.id, guild: message.guild.id });
+
+        // check whether member profile exists
+        if (!memberProfile) throw new Error(this.client.locale.getString("en_us", "errors", "profileNotFound"));
+
+        // give karma to the member
+        memberProfile.karma += 1;
+
+        // save the member document
+        await memberProfile.save();
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                description: this.client.locale.getString("en_us", "info", "karma", message.author.tag, member.user.tag),
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
