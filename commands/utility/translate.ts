b21046afadebd8183d2a51bdd5f2f8e8b8c12d6c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-07 23:28:39 +0530

    commands: add translate command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/utility/translate.ts
@@ -0,0 +1,72 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import translate = require("@k3rn31p4nic/google-translate-api");
+
+import * as errors from "../../utils/errors";
+
+export = class TranslateCommand extends Command {
+    constructor() {
+        super("translate", {
+            description: "It allows you to translate the specified text from one language to another.",
+            triggers: [],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "translate -- TEXT",
+                "translate --to LANGUAGE -- TEXT",
+                "translate --from LANGUAGE --to LANGUAGE -- TEXT",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        // command syntax validation
+        if (!argv._.length) throw new errors.CommandSyntaxError(this.name);
+
+        // identify the text to translate
+        const text: string = argv._.join(" ");
+        // identify the source language
+        const from: string = argv.from ? argv.from.toLowerCase() : "auto";
+        // identify the source language
+        const to: string = argv.to ? argv.to.toLowerCase() : "en";
+
+        // fetch the translation
+        const response = await translate(text, { from, to });
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                title: "Translation",
+                description: response.text,
+                fields: [
+                    {
+                        name: "Source Language",
+                        value: response.from.language.iso.toUpperCase(),
+                        inline: true,
+                    },
+                    {
+                        name: "Target Language",
+                        value: to.toUpperCase(),
+                        inline: true,
+                    },
+                ],
+                footer: {
+                    text: "Powered by Google",
+                },
+            },
+        });
+    }
+}
