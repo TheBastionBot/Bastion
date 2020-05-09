a5125fa5f3df2908cb2aa6f07d671bc5b8fa86b0
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-09 13:12:14 +0530

    commands(help): add changelog command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/help/changelog.ts
@@ -0,0 +1,81 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import * as yaml from "yaml";
+import { promises as fs } from "fs";
+import { Command, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import { version } from "../../package.json";
+
+interface ChangeLog {
+    date: string;
+    image: string;
+    changes: {
+        [section: string]: {
+            title: string;
+            description: string;
+        }[];
+    };
+}
+
+export = class ChangeLogCommand extends Command {
+    constructor() {
+        super("changelog", {
+            description: "It allows you to see the changes that have been introduced in the current version of Bastion.",
+            triggers: [ "changes" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [],
+        });
+    }
+
+    exec = async (message: Message): Promise<void> => {
+        // get the changelog
+        const changelog: ChangeLog = yaml.parse(await fs.readFile("./changelog.yaml", "utf-8"));
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                author: {
+                    name: this.client.locale.getConstant("bastion.name"),
+                    url: this.client.locale.getConstant("bastion.website"),
+                },
+                title: "v" + version,
+                url: this.client.locale.getConstant("bastion.project.url") + "/Bastion/releases",
+                fields: Object.keys(changelog.changes).map(section => ({
+                    name: section,
+                    value: changelog.changes[section].map(change => (change.title ? "**" + change.title + "**\n" : "") + change.description).join("\n\n"),
+                    inline: false,
+                })).concat([
+                    {
+                        name: "Missed an update?",
+                        value: "[Check out our previous change logs](" + this.client.locale.getConstant("bastion.project.url") + "/Bastion/releases)."
+                          + "\nJoin **" + this.client.locale.getConstant("bastion.server.name") + "** and never miss an update: " + this.client.locale.getConstant("bastion.server.invite"),
+                        inline: true,
+                    },
+                    {
+                        name: "Support " + this.client.locale.getConstant("bastion.name") + "'s Development",
+                        value: "[Donate to " + this.client.locale.getConstant("bastion.project") + "](" + this.client.locale.getConstant("bastion.website") + "/donate) and get an enhanced Bastion experience!",
+                        inline: true,
+                    },
+                ]),
+                image: {
+                    url: changelog.image,
+                },
+                footer: {
+                    text: changelog.date + " • " + this.client.locale.getString("en_us", "info", "didYouKnowEarlyReleases"),
+                },
+            },
+        });
+    }
+}
