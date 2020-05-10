b758876157d1a5d214c7e6f49691334e9dbb4ea8
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-10 19:29:50 +0530

    events: implement commandExecute event
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ events/commandExecute.ts
@@ -0,0 +1,45 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, Constants, Logger, ModuleManagerEvent } from "tesseract";
+import { Message } from "discord.js";
+
+import * as errors from "../utils/errors";
+
+export = class CommandExecuteEvent extends ModuleManagerEvent {
+    constructor() {
+        super("commandExecute");
+    }
+
+    exec = async (command: Command, message: Message, status: Constants.MODULE_EXECUTE_STATUS, error?: Error): Promise<unknown> => {
+        switch (status) {
+        case "success":
+            return;
+
+        case "failed":
+            if (error instanceof errors.DiscordError) {
+                return message.channel.send({
+                    embed: {
+                        color: Constants.COLORS.RED,
+                        title: error.name,
+                        description: error.name === errors.BASTION_ERROR_TYPE.INVALID_COMMAND_SYNTAX
+                            ? "That's not how you use the `" + command.name + "` command."
+                            : error.message,
+                        fields: error.name === errors.BASTION_ERROR_TYPE.INVALID_COMMAND_SYNTAX
+                            ? [
+                                {
+                                    name: "Need help?",
+                                    value: "```html\n" + command.name + " --help```\nYou can join [Bastion HQ](https://discord.gg/fzx8fkt) to get help from our amazing support staff.",
+                                }
+                            ]
+                            : [],
+                    },
+                }).catch(Logger.error);
+            }
+
+            return Logger.error(error);
+        }
+    }
+}
