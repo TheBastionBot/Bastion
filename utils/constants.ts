3ef7dcbbfa2f99447407e5475bd7bf94a8be57e3
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-17 12:53:47 +0530

    utils(constants): add badge values
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,6 +6,38 @@
 import { User } from "discord.js";
 
 
+const BADGES = {
+    BASTION_DEVELOPER: 1 << 0,
+    BASTION_VERIFIED_USER: 1 << 1,
+    BASTION_VERIFIED_GUILD: 1 << 2,
+    BASTION_PARTNER_USER: 1 << 3,
+    BASTION_PARTNER_GUILD: 1 << 4,
+    BASTION_LEAP_OF_FAITH: 1 << 5,
+    BASTION_GOLD_USER: 1 << 6,
+    BASTION_GOLD_GUILD: 1 << 7,
+    BASTION_SUPPORTER: 1 << 8,
+    BASTION_WHITE_HAT: 1 << 9,
+    BASTION_BUG_HUNTER: 1 << 10,
+    BASTION_OPEN_SORCERER: 1 << 11,
+    BASTION_ARTISAN: 1 << 12,
+    BASTION_TRANSLATOR: 1 << 13,
+    BASTION_PLUGIN_DEVELOPER: 1 << 14,
+    BASTION_PLUGIN_SUPPORT_GUILD: 1 << 15,
+
+    GUILD_OWNER: 1 << 16,
+    GUILD_STAFF: 1 << 17,
+    GUILD_NEW_MEMBER: 1 << 18,
+    GUILD_1Y_CLUB: 1 << 19,
+    GUILD_2Y_CLUB: 1 << 20,
+    GUILD_3Y_CLUB: 1 << 21,
+    GUILD_4Y_CLUB: 1 << 22,
+    GUILD_5Y_CLUB: 1 << 23,
+    GUILD_6Y_CLUB: 1 << 24,
+    GUILD_7Y_CLUB: 1 << 25,
+    GUILD_8Y_CLUB: 1 << 26,
+    GUILD_9Y_CLUB: 1 << 27,
+};
+
 const COLORS = {
     TWITCH: 0x9146ff,
     YOUTUBE: 0xff0000,
@@ -31,6 +63,7 @@ const isPublicBastion = (user: User): boolean => {
 
 
 export {
+    BADGES,
     COLORS,
     isPublicBastion,
 };
