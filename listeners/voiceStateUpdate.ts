17bbc94ae127792774d4431d3545fd16855fe0a6
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-24 11:36:33 +0530

    listeners(voiceStateUpdate): implement voice session
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ listeners/voiceStateUpdate.ts
@@ -0,0 +1,70 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Constants, Listener } from "tesseract";
+import { VoiceState } from "discord.js";
+
+import GuildModel from "../models/Guild";
+import * as constants from "../utils/constants";
+import * as omnic from "../utils/omnic";
+
+export = class VoiceStateUpdateListener extends Listener {
+    private newSessionChannelPrefix: string;
+
+    constructor() {
+        super("voiceStateUpdate", {
+            mode: Constants.LISTENER_MODE.ON,
+        });
+
+        this.newSessionChannelPrefix = "➕ New";
+    }
+
+    exec = async (oldState: VoiceState, newState: VoiceState): Promise<void> => {
+        if (newState.channelID === oldState.channelID) return;
+        if (!(oldState.channel && oldState.channel.parent) && !(newState.channel && newState.channel.parent)) return;
+
+        // get guild document
+        const guild = await GuildModel.findById(oldState.guild ? oldState.guild.id : newState.guild.id);
+        if (!guild.voiceSessions || !guild.voiceSessions.categories) return;
+
+
+        // check for premium membership
+        if (constants.isPublicBastion(this.client.user)) {
+            // fetch the premium tier
+            const tier = await omnic.fetchPremiumTier(oldState.guild || newState.guild).catch(() => {
+                // this error can be ignored
+            });
+            if (!tier) return;
+        }
+
+
+        // check whether all members left the old channel
+        if (oldState.channel && oldState.channel.parent && guild.voiceSessions.categories.includes(oldState.channel.parentID) && !oldState.channel.name.startsWith(this.newSessionChannelPrefix) && oldState.channel.members.size === 0 && oldState.channel.deletable) {
+            oldState.channel.delete("Members Left");
+        }
+
+
+        // member joined the channel
+        if (newState.channel && newState.channel.parent && guild.voiceSessions.categories.includes(newState.channel.parentID)) {
+            // check whether member is requesting a new session channel to be created
+            if (newState.channel.name.startsWith(this.newSessionChannelPrefix)) {
+                // HACK: channel number might have concurrency problem
+                const sessionChannelName = newState.channel.name.replace(this.newSessionChannelPrefix, "").trim();
+
+                const newSessionChannel = await newState.channel.guild.channels.create(sessionChannelName + " #" + (newState.channel.parent.children.filter(c => c.type === "voice" && c.name.startsWith(sessionChannelName + " #")).size + 1), {
+                    type: "voice",
+                    bitrate: newState.channel.guild.premiumTier ? newState.channel.guild.premiumTier * 128e3 : 96e3,
+                    parent: newState.channel.parent,
+                    // TODO: Add member limit
+                    // userLimit: ,
+                    reason: "Requested by " + (newState.member.user ? newState.member.user.tag : newState.member.id),
+                });
+
+                // move member to the new channel
+                await newState.member.voice.setChannel(newSessionChannel).catch(console.error);
+            }
+        }
+    }
+}
