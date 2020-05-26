9f963218547f02f6887ca549f4f487794f21a5e6
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-26 19:00:19 +0530

    commands(utility): add lmstfy command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/utility/lmstfy.ts
@@ -0,0 +1,53 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, CommandArguments, Constants } from "tesseract";
+import { Message } from "discord.js";
+
+import * as errors from "../../utils/errors";
+
+export = class LMSTFYCommand extends Command {
+    constructor() {
+        super("lmstfy", {
+            description: "It allows you to send a LMGTFY link for the specified search query. Use it to teach people how to do an internet search and get answers to their questions.",
+            triggers: [ "lmgtfy" ],
+            arguments: {
+                alias: {
+                    duckduckgo: [ "ddg", "d" ],
+                    google: [ "g" ],
+                },
+                boolean: [ "duckduckgo", "google" ],
+            },
+            scope: "guild",
+            owner: false,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [
+                "lmstfy QUERY",
+                "lmstfy --duckduckgo QUERY",
+                "lmstfy --google QUERY",
+            ],
+        });
+    }
+
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        // command syntax validation
+        if (!argv._.length) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.INVALID_COMMAND_SYNTAX, this.name);
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                title: "Let me search that for you...",
+                description: "https://lmgtfy.com/?s=" + (argv.google ? "g" : "d") + "&q=" + encodeURIComponent(argv._.join(" ")),
+                footer: {
+                    text: "Powered by LMGTFY"
+                },
+            },
+        });
+    }
+}
