089f9b8476d52f2b1d265086b417e92e8de425bf
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-27 23:57:11 +0530

    events: add insufficientUserPermissions handler
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ events/insufficientUserPermissions.ts
@@ -0,0 +1,28 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, Constants, ModuleManagerEvent } from "tesseract";
+import { Message } from "discord.js";
+
+export = class InsufficientUserPermissionsEvent extends ModuleManagerEvent {
+    constructor() {
+        super("insufficientUserPermissions");
+    }
+
+    exec = async (command: Command, message: Message): Promise<unknown> => {
+        return message.channel.send({
+            embed: {
+                color: Constants.COLORS.RED,
+                title: "Insufficient Permissions",
+                description: message.author.tag + " you don't have the requried permissions in this channel to execute this command.",
+                footer: {
+                    text: "Use the `" + command.name + " --help` command to get more information on the command.",
+                },
+            },
+        }).catch(() => {
+            // this error can be ignored
+        });
+    }
+}
