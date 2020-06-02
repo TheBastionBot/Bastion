92c5cc5d0061f7b48d94ed5ee695ac3374cbf7fb
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-06-02 22:11:23 +0530

    scripts: add scripts required to manage bastion installation
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ scripts/powershell/Start.ps1
@@ -0,0 +1,18 @@
+Write-Host "[Bastion]: Checking Bastion system..."
+If (Test-Path ".\index.js") {
+  Write-Host "[Bastion]: System check successful."
+  Write-Host
+
+  Write-Host "[Bastion]: Booting up..."
+  Write-Host
+
+  yarn start
+}
+Else {
+  Write-Host "[Bastion]: System check failed."
+  Write-Host
+
+  Write-Host "[Bastion]: Check if you have installed Bastion correctly."
+  Write-Host "[Bastion]: You can get help at Bastion HQ: https://discord.gg/fzx8fkt"
+}
+Write-Host
