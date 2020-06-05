f27968a6dacbe2c355a2c20de73d97ad720301f9
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-06-06 03:53:49 +0530

    monitors(sanity): add error log for constants
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- monitors/sanity.ts
+++ monitors/sanity.ts
@@ -55,6 +55,8 @@ export = class SanityMonitor extends Monitor {
 
         // check whether the constants are modified
         if (this.a !== SanityMonitor.e) {
+            process.stderr.write("\n\n" + Buffer.from("RkFUQUwgRVJST1I6IDB4QkFBREIwMDIKCllvdSBzZWVtIHRvIGhhdmUgbW9kaWZpZWQgQmFzdGlvbidzIGNvbnN0YW50cy4gVGhhdCBjb250YWlucyB2YWx1ZXMgdGhhdCBhcmUgcmVxdWlyZWQgdG8gcnVuIEJhc3Rpb24uCgpQbGVhc2UgY29udGFjdCB0aGUgc3VwcG9ydCB0ZWFtIGF0IEJhc3Rpb24gSFEgLSBodHRwczovL2Rpc2NvcmQuZ2cvZnp4OGZrdCAtIGZvciBtb3JlIGluZm9ybWF0aW9uLg", "base64").toString("utf-8") + "\n\n\n");
+
             if (this.client.shard) {
                 await this.client.shard.broadcastEval("this.destroy();process.exitCode = " + this.errorCode + ";");
             } else {
