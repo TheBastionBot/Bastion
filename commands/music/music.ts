85be2a84c2dd7382c8500e954fa5ebe737920426
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-06-05 14:03:59 +0530

    commands(music): support for toggling status for diff. guilds
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/music/music.ts
+++ commands/music/music.ts
@@ -3,18 +3,25 @@
  * @copyright 2020 - The Bastion Bot Project
  */
 
-import { Command, Constants } from "@bastion/tesseract";
+import { Command, CommandArguments, Constants } from "@bastion/tesseract";
 import { Message } from "discord.js";
 
+import * as errors from "../../utils/errors";
+import GuildModel from "../../models/Guild";
 import BastionGuild = require("../../structures/Guild");
 
 
-export = class Music extends Command {
+export = class MusicCommand extends Command {
     constructor() {
         super("music", {
-            description: "It toggles Bastion's music support in the server. Once enabled, you can use all music commands in the server.",
+            description: "It toggles Bastion's music support in the specified server. Once enabled, you can use all music commands in the server.",
             triggers: [],
-            arguments: {},
+            arguments: {
+                alias: {
+                    server: [ "s" ],
+                },
+                string: [ "server" ],
+            },
             scope: "guild",
             owner: true,
             cooldown: 0,
@@ -24,26 +31,34 @@ export = class Music extends Command {
         });
     }
 
-    exec = async (message: Message): Promise<void> => {
-        const guild = (message.guild as BastionGuild);
+    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+        // identify the guild
+        const guildDocument = await GuildModel.findById(argv.server || message.guild.id);
+        const guild = this.client.guilds.resolve(argv.server || message.guild.id);
 
-        const status = guild.document.music && guild.document.music.enabled;
+        // check whether the guild exist
+        if (!guildDocument) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.ERROR, this.client.locale.getString((message.guild as BastionGuild).document.language, "errors", "guildNotFound"));
 
-        // Toggle music in the server
-        guild.document.music = {
-            ...guild.document.music,
+        // check the status of music in the guild
+        const status = guildDocument.music && guildDocument.music.enabled;
+
+        // toggle music in the server
+        guildDocument.music = {
+            ...guildDocument.music,
             enabled: !status,
         };
 
-        guild.document.save();
+        // save the guild
+        await guildDocument.save();
 
+        // acknowledge
         await message.channel.send({
             embed: {
                 color: status ? Constants.COLORS.RED : Constants.COLORS.GREEN,
-                description: this.client.locale.getString((message.guild as BastionGuild).document.language, "info", status ? "musicDisable" : "musicEnable", message.author.tag),
+                description: this.client.locale.getString((message.guild as BastionGuild).document.language, "info", status ? "musicDisable" : "musicEnable", message.author.tag, guild ? guild.name : argv.server),
             },
         }).catch(() => {
-            // This error can be ignored.
+            // this error can be ignored
         });
     }
 }
