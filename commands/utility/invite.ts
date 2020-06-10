f0466850674d15ac070b8d6a7631104b0ea8f4aa
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-06-10 06:28:07 -0700

    commands(invite): execute the command even without instant invite perms
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/utility/invite.ts
+++ commands/utility/invite.ts
@@ -4,7 +4,7 @@
  */
 
 import { Command, CommandArguments } from "@bastion/tesseract";
-import { Message, TextChannel } from "discord.js";
+import { Invite, Message, TextChannel } from "discord.js";
 
 import * as constants from "../../utils/constants";
 
@@ -35,17 +35,18 @@ export = class InviteCommand extends Command {
     exec = async (message: Message, argv: CommandArguments): Promise<void> => {
         const channel = (message.guild.widgetChannel || message.channel) as TextChannel;
 
+        let invite: Invite;
         // check whether member has permission to create instant invites
-        if (!channel.permissionsFor(message.member).has("CREATE_INSTANT_INVITE")) throw new Error("NO_PERMISSION");
-
-        const invite = await channel.createInvite({
-            reason: "Requested by " + message.author.tag,
-            temporary: argv.temporary,
-        });
+        if (channel.permissionsFor(message.member).has("CREATE_INSTANT_INVITE")) {
+            invite = await channel.createInvite({
+                reason: "Requested by " + message.author.tag,
+                temporary: argv.temporary,
+            });
+        }
 
         // acknowledge
         await message.channel.send(
-            "Hello. Beep. Boop.\nIf you wanna invite friends to this server, share the following invite link with your friends.\nBeep!\n" + invite.url
+            (invite ? "Hello. Beep. Boop.\nIf you wanna invite friends to this server, share the following invite link with your friends.\nBeep!\n" + invite.url : "You don't have perms to create an instant invite for the channel.")
             + ((constants.isPublicBastion(this.client.user) || this.client.credentials.owners.includes(message.author.id)) ? "\n\nAnd if you wanna invite me to your server, use the following link.\nBeep.\n" + this.client.locale.getConstant("bastion.bot.invite").replace(this.client.locale.getConstant("bastion.bot.id"), this.client.user.id) : "")
         );
     }
