8e8b443610012e62b6f02c01480ff80c67cd0144
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-06-10 07:10:59 -0700

    commands(flip): implement gambling
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/games/flip.ts
+++ commands/games/flip.ts
@@ -8,18 +8,23 @@ import { Message } from "discord.js";
 
 import * as numbers from "../../utils/numbers";
 
+import BastionGuild = require("../../structures/Guild");
+import BastionGuildMember = require("../../structures/GuildMember");
+
 export = class FlipCommand extends Command {
     private outcomes: string[];
 
     constructor() {
         super("flip", {
-            description: "It allows you to flip any number of coins and see the result.",
+            description: "It allows you to flip any number of coins and see the result. It also supports gambling.",
             triggers: [],
             arguments: {
                 alias: {
                     coins: [ "c" ],
                 },
+                array: [ "bet" ],
                 number: [ "coins" ],
+                string: [ "bet" ],
                 coerce: {
                     coins: (arg): number => Math.floor(numbers.clamp(arg, 1, 256)),
                 },
@@ -36,6 +41,7 @@ export = class FlipCommand extends Command {
             syntax: [
                 "flip",
                 "flip --coins NUMBER",
+                "flip --bet OUTCOME",
             ],
         });
 
@@ -49,6 +55,19 @@ export = class FlipCommand extends Command {
         const outcomes: string[] = [];
         for (let index = 0; index < coins; index++) {
             outcomes.push(this.outcomes[Math.floor(Math.random() * this.outcomes.length)]);
+        }
+
+        // gambling
+        if (argv.bet && argv.bet.length) {
+            if ((message.guild as BastionGuild).document.gambling && (message.guild as BastionGuild).document.gambling.enabled) {
+                if (argv.bet.join(" / ").toLowerCase() === outcomes.join(" / ").toLowerCase()) {
+                    (message.member as BastionGuildMember).credit(13 * ((message.guild as BastionGuild).document.gambling.multiplier || 1), "Won the bet in Flip.");
+                } else {
+                    (message.member as BastionGuildMember).debit(13 * ((message.guild as BastionGuild).document.gambling.multiplier || 1), "Lost the bet in Flip.");
+                }
+            } else {
+                throw new Error("GAMBLING_DISABLED");
+            }
         }
 
         // acknowledge
