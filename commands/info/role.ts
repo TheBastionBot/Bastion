a01e40964d016e6091eb875bb56a2047a4f9072c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-06-10 01:11:15 -0700

    commands(role): add members option to get role members
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/info/role.ts
+++ commands/info/role.ts
@@ -4,18 +4,28 @@
  */
 
 import { Command, CommandArguments } from "@bastion/tesseract";
-import { Message } from "discord.js";
+import { GuildMember, Message } from "discord.js";
 
-import * as strings from "../../utils/strings";
 import * as errors from "../../utils/errors";
+import * as pagination from "../../utils/pagination";
+import * as strings from "../../utils/strings";
+
 import BastionGuild = require("../../structures/Guild");
 
 export = class RoleCommand extends Command {
     constructor() {
         super("role", {
-            description: "It allows you see the information of a role.",
+            description: "It allows you see the information of a role. It also allows you to see the members in a role.",
             triggers: [ "roleInfo" ],
-            arguments: {},
+            arguments: {
+                alias: {
+                    members: [ "m" ],
+                },
+                default: {
+                    members: 1,
+                },
+                number: [ "members" ],
+            },
             scope: "guild",
             owner: false,
             cooldown: 0,
@@ -24,11 +34,12 @@ export = class RoleCommand extends Command {
             userPermissions: [],
             syntax: [
                 "role ROLE",
+                "role ROLE --members PAGE",
             ],
         });
     }
 
-    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+    exec = async (message: Message, argv: CommandArguments): Promise<unknown> => {
         const identifier: string = argv._.join(" ");
 
         // identify the role
@@ -36,8 +47,32 @@ export = class RoleCommand extends Command {
 
         if (!role) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.ROLE_NOT_FOUND, this.client.locale.getString((message.guild as BastionGuild).document.language, "errors", "roleNotFound"));
 
+        // return the members in the role
+        if (argv.members) {
+            const members = pagination.paginate([ ...role.members.values() ], argv.members);
+
+
+            // acknowledge
+            return await message.channel.send({
+                embed: {
+                    color: role.color,
+                    author: {
+                        name: role.name,
+                    },
+                    title: "Role Members",
+                    fields: members.items.map((member: GuildMember) => ({
+                        name: member.displayName,
+                        value: member.user.tag + " / " + member.id,
+                    })),
+                    footer: {
+                        text: `Page ${members.page} of ${members.pages}`
+                    },
+                },
+            });
+        }
+
         // acknowledge
-        message.channel.send({
+        return await message.channel.send({
             embed: {
                 color: role.color,
                 author: {
@@ -69,8 +104,6 @@ export = class RoleCommand extends Command {
                     text: (role.hoist ? "Hoisted • " : "") + role.id,
                 },
             },
-        }).catch(() => {
-            // this error can be ignored
         });
     }
 }
