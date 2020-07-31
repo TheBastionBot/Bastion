6bf20b680fbcd9eaceb5a3c884aec3803d4fca5c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-08-01 01:40:52 +0530

    interrupts: add blacklist interrupt
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ interrupts/blacklist.ts
@@ -0,0 +1,37 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Interrupt } from "@bastion/tesseract";
+import { Message, TextChannel } from "discord.js";
+
+import ConfigModel from "../models/Config";
+
+
+export = class Blacklist extends Interrupt {
+    constructor() {
+        super("blacklist", {
+            type: 0,
+        });
+    }
+
+    exec = async (message: Message): Promise<boolean> => {
+        if (!message.guild) return;
+
+        if (!(message.channel instanceof TextChannel)) return;
+
+        if (!message.content || !message.content.length) return false;
+
+        // get config document
+        const config = await ConfigModel.findById(this.client.user.id);
+
+        // check whether the guild is blacklisted
+        if (config.blacklistedGuildIds && config.blacklistedGuildIds.includes(message.guild.id)) return true;
+
+        // check whether the user is blacklisted
+        if (config.blacklistedUserIds && config.blacklistedUserIds.includes(message.author.id)) return true;
+
+        return false;
+    }
+}
