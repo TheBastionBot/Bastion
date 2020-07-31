783db7403b1fb9a7574e28bd2a6a849e2b4bc2bb
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-08-01 02:24:26 +0530

    commands(bot_owner): refactor command
    * show all blacklists when command is used
    * fix guilds not being blacklisted
    
    Closes https://github.com/TheBastionBot/Bastion/issues/894
    
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/bot_owner/blacklist.ts
+++ commands/bot_owner/blacklist.ts
@@ -8,7 +8,6 @@ import { Message, EmbedField } from "discord.js";
 
 import ConfigModel from "../../models/Config";
 import * as arrays from "../../utils/arrays";
-import * as errors from "../../utils/errors";
 import * as snowflake from "../../utils/snowflake";
 import BastionGuild = require("../../structures/Guild");
 
@@ -43,37 +42,40 @@ export = class Blacklist extends Command {
         const servers: Snowflake[] = argv.servers ? argv.servers.filter((id: string) => snowflake.isValid(id)) : [];
         const users: Snowflake[] = argv.users ? argv.users.filter((id: string) => snowflake.isValid(id)) : [];
 
-        // command syntax validation
-        if (!servers.length && !users.length) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.INVALID_COMMAND_SYNTAX, this.name);
-
         const fields: EmbedField[] = [];
 
         // update blacklisted guilds
-        if (servers.length) {
-            if (argv.remove) config.blacklistedGuildIds = config.blacklistedGuildIds.filter(id => !servers.includes(id));
-            else config.blacklistedGuildIds = [ ...users, ...config.blacklistedGuildIds ];
+        if (argv.remove) config.blacklistedGuildIds = config.blacklistedGuildIds.filter(id => !servers.includes(id));
+        else config.blacklistedGuildIds = [ ...servers, ...config.blacklistedGuildIds ];
 
-            config.blacklistedGuildIds = [ ...new Set(config.blacklistedGuildIds) ];
+        config.blacklistedGuildIds = [ ...new Set(config.blacklistedGuildIds) ];
 
+        const resultingServers = arrays.toBulletList(config.blacklistedGuildIds);
+
+        if (resultingServers) {
             fields.push({
                 name: "Servers",
-                value: arrays.toBulletList(servers),
+                value: resultingServers,
                 inline: true,
             });
         }
+
         // update blacklisted users
-        if (users.length) {
-            if (argv.remove) config.blacklistedUserIds = config.blacklistedUserIds.filter(id => !users.includes(id));
-            else config.blacklistedUserIds = [ ...users, ...config.blacklistedUserIds ];
+        if (argv.remove) config.blacklistedUserIds = config.blacklistedUserIds.filter(id => !users.includes(id));
+        else config.blacklistedUserIds = [ ...users, ...config.blacklistedUserIds ];
 
-            config.blacklistedUserIds = [ ...new Set(config.blacklistedUserIds) ];
+        config.blacklistedUserIds = [ ...new Set(config.blacklistedUserIds) ];
 
+        const resultingUsers = arrays.toBulletList(config.blacklistedUserIds);
+
+        if (resultingUsers) {
             fields.push({
                 name: "Users",
-                value: arrays.toBulletList(users),
+                value: resultingUsers,
                 inline: true,
             });
         }
+
 
         // save config document
         await config.save();
@@ -84,14 +86,12 @@ export = class Blacklist extends Command {
             embed: {
                 color: Constants.COLORS.PUPIL,
                 title: "Bastion's Blacklist",
-                description: this.client.locale.getString((message.guild as BastionGuild).document.language, "info", servers.length || users.length ? "bastionBlacklistUpdate" : "bastionBlacklistUnchanged", message.author.tag),
+                description: this.client.locale.getString((message.guild as BastionGuild).document.language, "info", (servers.length || users.length) ? "bastionBlacklistUpdate" : "bastionBlacklistUnchanged", message.author.tag),
                 fields,
                 footer: {
                     text: `${config.blacklistedGuildIds.length} Blacklisted Servers / ${config.blacklistedUserIds.length} Blacklisted Users`,
                 },
             },
-        }).catch(() => {
-            // this error can be ignored.
         });
     }
 }
