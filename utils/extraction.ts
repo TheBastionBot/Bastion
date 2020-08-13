f9a4783cfbaa642b56ad7f78f72569ae9eccd736
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-08-13 16:10:56 +0530

    utils: add extraction func
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ utils/extraction.ts
@@ -0,0 +1,24 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import fetch, { RequestInit as RequestOptions, Response } from "node-fetch";
+
+export const BASE_PATH = "https://ex.traction.one/";
+
+export const resolvePath = (path: string): string => BASE_PATH + (BASE_PATH.endsWith("/") ? path.replace(/^\//g, "") : path);
+
+export const makeRequest = (path: string, options?: RequestOptions): Promise<Response> => {
+    const requestOptions: RequestOptions = {
+        method: "GET",
+        headers: {
+            "Accepts": "application/json",
+            "Content-Type": "application/json",
+            "User-Agent": "Bastion (Discord Bot; https://bastion.traction.one)",
+        },
+        ...options,
+    };
+
+    return fetch(resolvePath(path), requestOptions);
+};
