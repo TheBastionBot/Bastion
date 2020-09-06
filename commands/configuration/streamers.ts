13403a4e5e5a9a8d5b45781ea41c343910a864f4
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-09-06 06:03:01 +0530

    commands(streamers): fixed streamer data not being saved after complete deletion
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/configuration/streamers.ts
+++ commands/configuration/streamers.ts
@@ -6,6 +6,7 @@
 import { Command, CommandArguments, Constants } from "@bastion/tesseract";
 import { Message } from "discord.js";
 
+import { Guild } from "../../models/Guild";
 import * as constants from "../../utils/constants";
 import * as errors from "../../utils/errors";
 import * as omnic from "../../utils/omnic";
@@ -42,40 +43,42 @@ export = class InviteFilterCommand extends Command {
         const guild = (message.guild as BastionGuild);
 
         if (argv.twitch) {
+            const guildObject: Guild = guild.document.toObject();
+
             // update twitch streamers
-            guild.document.streamers = {
-                ...guild.document.streamers,
+            guildObject.streamers = {
+                ...guildObject.streamers,
                 twitch: {
                     channelId: message.channel.id,
-                    users: guild.document.streamers && guild.document.streamers.twitch && guild.document.streamers.twitch.users && guild.document.streamers.twitch.users.length && !guild.document.streamers.twitch.users.includes(argv.twitch)
-                        ? guild.document.streamers.twitch.users.concat(argv.twitch)
+                    users: guildObject.streamers && guildObject.streamers.twitch && guildObject.streamers.twitch.users && guildObject.streamers.twitch.users.length && !guildObject.streamers.twitch.users.includes(argv.twitch)
+                        ? guildObject.streamers.twitch.users.concat(argv.twitch)
                         : [ argv.twitch as string ],
                 },
             };
 
             // remove the streamer, if specified
             if (argv.remove) {
-                guild.document.streamers.twitch.users = guild.document.streamers.twitch.users.filter(streamer => streamer !== argv.twitch);
+                guildObject.streamers.twitch.users = guildObject.streamers.twitch.users.filter(streamer => streamer !== argv.twitch);
 
                 // remove the entire object if no users are left
-                if (guild.document.streamers.twitch.users.length === 0) {
-                    guild.document.streamers.twitch = undefined;
-                    delete guild.document.streamers.twitch;
+                if (guildObject.streamers.twitch.users.length === 0) {
+                    guildObject.streamers.twitch = undefined;
+                    delete guildObject.streamers.twitch;
                 }
             }
 
 
             // check for premium membership limits
-            if (guild.document.streamers.twitch && guild.document.streamers.twitch.users && guild.document.streamers.twitch.users.length > constants.LIMITS.STREAMERS_PER_SERVICE && constants.isPublicBastion(this.client.user)) {
+            if (guildObject.streamers.twitch && guildObject.streamers.twitch.users && guildObject.streamers.twitch.users.length > constants.LIMITS.STREAMERS_PER_SERVICE && constants.isPublicBastion(this.client.user)) {
                 // fetch the premium tier
                 const tier = await omnic.fetchPremiumTier(message.guild).catch(() => {
                     // this error can be ignored
                 });
 
                 if (tier) { // check for premium membership limits
-                    if (tier === omnic.PremiumTier.GOLD && guild.document.streamers.twitch.users.length > constants.LIMITS.GOLD.STREAMERS_PER_SERVICE) {
+                    if (tier === omnic.PremiumTier.GOLD && guildObject.streamers.twitch.users.length > constants.LIMITS.GOLD.STREAMERS_PER_SERVICE) {
                         throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.LIMITED_PREMIUM_MEMBERSHIP, this.client.locale.getString((message.guild as BastionGuild).document.language, "errors", "membershipLimitStreamers", constants.LIMITS.GOLD.STREAMERS_PER_SERVICE));
-                    } else if (tier === omnic.PremiumTier.PLATINUM && guild.document.streamers.twitch.users.length > constants.LIMITS.PLATINUM.STREAMERS_PER_SERVICE) {
+                    } else if (tier === omnic.PremiumTier.PLATINUM && guildObject.streamers.twitch.users.length > constants.LIMITS.PLATINUM.STREAMERS_PER_SERVICE) {
                         throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.LIMITED_PREMIUM_MEMBERSHIP, this.client.locale.getString((message.guild as BastionGuild).document.language, "errors", "membershipLimitStreamers", constants.LIMITS.PLATINUM.STREAMERS_PER_SERVICE));
                     }
                 } else {    // no premium membership
@@ -83,6 +86,9 @@ export = class InviteFilterCommand extends Command {
                 }
             }
 
+
+            // update document
+            guild.document.overwrite(guildObject);
 
             // save document
             await guild.document.save();
