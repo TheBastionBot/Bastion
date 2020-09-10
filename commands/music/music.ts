093b96f38869d73185eea7b74ac63d9f21db62f8
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-09-10 07:04:20 +0530

    commands(music): allow premium users to toggle music in their servers
    and prevent non-bot owners from enabling it in other servers and see
    which servers it's enabled in.
    
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- commands/music/music.ts
+++ commands/music/music.ts
@@ -6,7 +6,9 @@
 import { Command, CommandArguments, Constants } from "@bastion/tesseract";
 import { Message } from "discord.js";
 
+import * as constants from "../../utils/constants";
 import * as errors from "../../utils/errors";
+import * as omnic from "../../utils/omnic";
 import GuildModel from "../../models/Guild";
 import BastionGuild = require("../../structures/Guild");
 
@@ -29,38 +31,55 @@ export = class MusicCommand extends Command {
             cooldown: 0,
             ratelimit: 1,
             clientPermissions: [],
-            userPermissions: [],
+            userPermissions: [ "MANAGE_GUILD" ],
             syntax: [
                 "music",
                 "music --server SERVER_ID",
+                "music --servers",
             ],
         });
     }
 
     exec = async (message: Message, argv: CommandArguments): Promise<unknown> => {
-        if (argv.servers) {
-            // find the guilds
-            const guildDocuments = await GuildModel.find({
-                music: {
-                    enabled: true,
-                },
+        // check for premium membership
+        if (constants.isPublicBastion(this.client.user)) {
+            // fetch the premium tier
+            const tier = await omnic.fetchPremiumTier(message.guild).catch(() => {
+                // this error can be ignored
             });
 
-            // acknowledge
-            return await message.channel.send({
-                embed: {
-                    color: Constants.COLORS.IRIS,
-                    title: "Music Servers",
-                    description: "Music is enabled in the following servers.",
-                    fields: [
-                        {
-                            name: guildDocuments.length + " Servers",
-                            value: guildDocuments.map(guild => guild.id).join("\n") || "-",
-                        },
-                    ],
-                },
-            });
+            if (!tier) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.PREMIUM_MEMBERSHIP_REQUIRED, this.client.locale.getString((message.guild as BastionGuild).document.language, "errors", "premiumMusic"));
         }
+
+        // allow only bot owner to enable music for other servers & see servers where music is enabled
+        if (this.client.credentials.owners.includes(message.author.id)) {
+            if (argv.servers) {
+                // find the guilds
+                const guildDocuments = await GuildModel.find({
+                    music: {
+                        enabled: true,
+                    },
+                });
+
+                // acknowledge
+                return await message.channel.send({
+                    embed: {
+                        color: Constants.COLORS.IRIS,
+                        title: "Music Servers",
+                        description: "Music is enabled in the following servers.",
+                        fields: [
+                            {
+                                name: guildDocuments.length + " Servers",
+                                value: guildDocuments.map(guild => guild.id).join("\n") || "-",
+                            },
+                        ],
+                    },
+                });
+            }
+        } else {
+            argv.server = message.guild.id;
+        }
+
 
         // identify the guild
         const guildDocument = await GuildModel.findById(argv.server || message.guild.id);
