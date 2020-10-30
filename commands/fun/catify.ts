36078a694fc89ea4ea1e78996a9423b150f18f37
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-10-29 17:19:02 -0700

    commands(fun): add catify command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/fun/catify.ts
@@ -0,0 +1,31 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command } from "@bastion/tesseract";
+import { Message } from "discord.js";
+
+
+export = class CatifyCommand extends Command {
+    constructor() {
+        super("catify", {
+            description: "It allows you to generate a cat that's unique to you.",
+            triggers: [],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [],
+        });
+    }
+
+    exec = async (message: Message): Promise<void> => {
+        await message.channel.send({
+            files: [ { name: "avatar.png", attachment: "https://robohash.org/" + encodeURIComponent(message.author.tag) + "?set=set4" } ]
+        });
+    }
+}
