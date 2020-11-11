7c11a2bef347908faa8e0b3d17e4a8ee0378dc33
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-11-11 21:04:12 +0530

    commands(fun): add roast command
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ commands/fun/roast.ts
@@ -0,0 +1,31 @@
+/*!
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command } from "@bastion/tesseract";
+import { Message } from "discord.js";
+
+import * as roasts from "../../assets/roasts.json";
+
+export = class RoastCommand extends Command {
+    constructor() {
+        super("roast", {
+            description: "It allows you to get roasted. The thicker the skin, the better the roast.",
+            triggers: [],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [],
+        });
+    }
+
+    exec = async (message: Message): Promise<void> => {
+        // acknowledge
+        await message.channel.send(roasts[Math.floor(Math.random() * roasts.length)]);
+    }
+}
