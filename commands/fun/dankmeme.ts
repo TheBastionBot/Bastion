295a180e90ba438628ed9d3a370869314fe4b412
Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>
2020-12-03 10:15:35 +0530

    commands(fun): add dankmeme command
    Signed-off-by: Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>

---
+++ commands/fun/dankmeme.ts
@@ -0,0 +1,49 @@
+/*!
+ * @author Sankarsan Kampa (iamtraction)
+ * @copyright 2020 - The Bastion Bot Project
+ */
+
+import { Command, Constants } from "@bastion/tesseract";
+import { Message } from "discord.js";
+
+import * as omnic from "../../utils/omnic";
+
+export = class DankMemeCommand extends Command {
+    constructor() {
+        super("dankmeme", {
+            description: "Posts a random dank meme collected from the r/dankmemes subreddit.",
+            triggers: [ "dankmemes" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            cooldown: 0,
+            ratelimit: 1,
+            clientPermissions: [],
+            userPermissions: [],
+            syntax: [],
+        });
+    }
+
+    exec = async (message: Message): Promise<void> => {
+        // fetch a random meme
+        const response = await omnic.makeRequest("/reddit/dankmemes/random");
+        const [ post ] = await response.json();
+
+        if (!post || !post.data || !post.data.children || !post.data.children.length || !post.data.children[0].data) return;
+
+        // acknowledge
+        await message.channel.send({
+            embed: {
+                color: Constants.COLORS.IRIS,
+                title: post.data.children[0].data.title.slice(0, 256),
+                url: "https://reddit.com" + post.data.children[0].data.permalink,
+                image: {
+                    url: post.data.children[0].data.url,
+                },
+                footer: {
+                    text: "Powered by r/dankmemes",
+                },
+            },
+        });
+    }
+}
