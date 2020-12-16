577a84bc42b1fed33eb57cdbfe29eb144c6c936e
Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>
2020-12-16 09:34:46 +0530

    listeners(inviteDelete): delete invite data from cache on delete
    Signed-off-by: Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>

--- listeners/inviteDelete.ts
+++ listeners/inviteDelete.ts
@@ -7,6 +7,7 @@ import { Listener, Constants } from "@bastion/tesseract";
 import { Invite } from "discord.js";
 
 import Guild = require("../structures/Guild");
+import * as constants from "../utils/constants";
 
 export = class InviteDeleteListener extends Listener {
     constructor() {
@@ -18,6 +19,13 @@ export = class InviteDeleteListener extends Listener {
     exec = async (invite: Invite): Promise<void> => {
         const guild = invite.guild as Guild;
 
+        // delete the invite data from invite cache
+        // TODO: add public bot support (with premium membership)
+        if (!constants.isPublicBastion(this.client.user)) {
+            delete (guild as Guild).invites[invite.code];
+        }
+
+        // server log
         const fields = [
             {
                 name: "Invite Code",
