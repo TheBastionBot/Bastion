792657f71d3dff5c28cae9a1b360c0c436a2bb93
Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>
2021-03-27 17:36:21 +0530

    commands(commandModule): add support for enabling/disabling cateogry
    Signed-off-by: Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>

--- commands/bot_owner/commandModule.ts
+++ commands/bot_owner/commandModule.ts
@@ -13,14 +13,15 @@ import BastionGuild = require("../../structures/Guild");
 export = class CommandModuleCommand extends Command {
     constructor() {
         super("commandModule", {
-            description: "It allows you to temporarily enable or disable a command module until the next restart. It also allows you to check whether the command is currently enabled.",
+            description: "It allows you to temporarily enable or disable a command module or cateogry until the next restart. It also allows you to check whether the command is currently enabled.",
             triggers: [ "command" ],
             arguments: {
                 alias: {
+                    category: [ "c" ],
                     disable: [ "d" ],
                     enable: [ "e" ],
                 },
-                boolean: [ "disable", "enable" ],
+                boolean: [ "category", "disable", "enable" ],
             },
             scope: "guild",
             owner: true,
@@ -36,9 +37,36 @@ export = class CommandModuleCommand extends Command {
         });
     }
 
-    exec = async (message: Message, argv: CommandArguments): Promise<void> => {
+    exec = async (message: Message, argv: CommandArguments): Promise<any> => {
         // command syntax validation
         if (!argv._.length) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.INVALID_COMMAND_SYNTAX, this.name);
+
+        if (argv.category && (argv.enable || argv.disable)) {
+            // find the commands in the specified category
+            const commands = (this.manager.modules as Collection<string, Command>).filter(command => command.category.toLowerCase() === argv._.join("").toLowerCase());
+
+            // toggle commands' active status
+            for (const [ , command ] of commands) {
+                command.enabled = argv.enable ? true : argv.disable ? false : command.enabled;
+            }
+
+            // check whether the module exists
+            if (!commands.size) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.ERROR, this.client.locale.getString((message.guild as BastionGuild).document.language, "errors", "commandNotFound"));
+
+            // acknowledge
+            return await message.channel.send({
+                embed: {
+                    color: argv.enable ? Constants.COLORS.GREEN : argv.disable ? Constants.COLORS.RED : Constants.COLORS.PUPIL,
+                    author: {
+                        name: commands.first().category,
+                    },
+                    title: "Command Category",
+                    description: this.client.locale.getString((message.guild as BastionGuild).document.language, "info", argv.enable ? "volatileCommandCateogoryEnable" : "volatileCommandCateogoryDisable", message.author.tag, commands.first().category),
+                },
+            }).catch(() => {
+                // this error can be ignored.
+            });
+        }
 
         // find the specified command
         const command = (this.manager.modules as Collection<string, Command>).find(command => command.name.toLowerCase() === argv._.join("").toLowerCase());
@@ -57,7 +85,7 @@ export = class CommandModuleCommand extends Command {
                     name: command.name,
                 },
                 title: "Command Module",
-                description: this.client.locale.getString((message.guild as BastionGuild).document.language, "info", argv.enable ? "volatileCommandEnable" : argv.disable ? "volatileCommandDisable" : command.enabled ? "commandEnabled" : "commandDisabled", message.author.tag, command.name),
+                description: this.client.locale.getString((message.guild as BastionGuild).document.language, "info", argv.enable ? "volatileCommandEnable" : argv.disable ? "volatileCommandDisable" : command.enabled ? "commandEnabled" : "commandDisable", message.author.tag, command.name),
             },
         }).catch(() => {
             // this error can be ignored.
