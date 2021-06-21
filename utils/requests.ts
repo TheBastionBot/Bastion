2669edd7ac1ec37b1ee69ac7f951a8251ceaa97c
Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>
2021-06-21 09:58:12 +0530

    utils: add requests module
    Signed-off-by: Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>

---
+++ utils/requests.ts
@@ -0,0 +1,18 @@
+/*!
+ * @author TRACTION (iamtraction)
+ * @copyright 2021 - Sankarsan Kampa
+ */
+
+import fetch, { RequestInit, Response } from "node-fetch";
+
+export const get = (url: string, headers?: { [key: string]: string }, options?: RequestInit): Promise<Response> => {
+    return fetch(url, {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "User-Agent": "Ommic (The Bastion Bot Project; https://bastion.traction.one)",
+            ...headers,
+        },
+        ...options,
+    });
+};
