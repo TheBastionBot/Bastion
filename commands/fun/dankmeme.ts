2e104b2f59d54df0a120ca5da0dc7ad1c2e7ee4e
Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>
2021-06-21 09:59:58 +0530

    commands(dankmeme): use reddit module to get data
    Signed-off-by: Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>

--- commands/fun/dankmeme.ts
+++ commands/fun/dankmeme.ts
@@ -6,7 +6,7 @@
 import { Command, Constants } from "@bastion/tesseract";
 import { Message } from "discord.js";
 
-import * as omnic from "../../utils/omnic";
+import { getRandomPost } from "../../utils/reddit";
 
 export = class DankMemeCommand extends Command {
     constructor() {
@@ -26,8 +26,7 @@ export = class DankMemeCommand extends Command {
 
     exec = async (message: Message): Promise<void> => {
         // fetch a random meme
-        const response = await omnic.makeRequest("/reddit/dankmemes/random");
-        const [ post ] = await response.json();
+        const [ post ] = await getRandomPost("dankmemes");
 
         if (!post || !post.data || !post.data.children || !post.data.children.length || !post.data.children[0].data) return;
 
