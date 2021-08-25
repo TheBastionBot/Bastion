8ee02c9cd9b1b6a5e6f99b402725b726f3f2ac63
Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>
2021-08-25 14:02:20 +0530

    utils(requests): add post method
    Signed-off-by: Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>

--- utils/requests.ts
+++ utils/requests.ts
@@ -16,3 +16,17 @@ export const get = (url: string, headers?: { [key: string]: string }, options?: 
         ...options,
     });
 };
+
+export const post = (url: string, headers?: { [key: string]: string }, options?: RequestInit, body?: Record<string, unknown>): Promise<Response> => {
+    return fetch(url, {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "User-Agent": "Ommic (The Bastion Bot Project; https://bastion.traction.one)",
+            ...headers,
+        },
+        ...options,
+        method: "POST",
+        body: JSON.stringify(body),
+    });
+};
