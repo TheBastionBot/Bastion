427af466b9f31036e9932b03ee43bdd08c33f569
TRACTION <19631364+iamtraction@users.noreply.github.com>
2021-10-24 14:50:09 +0530

    commands(rocketLeague): call APIs locally
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- commands/player_stats/rocketLeague.ts
+++ commands/player_stats/rocketLeague.ts
@@ -5,14 +5,30 @@
 
 import { Command, CommandArguments } from "@bastion/tesseract";
 import { Message } from "discord.js";
+import { BastionCredentials } from "../../typings/settings";
 
 import * as constants from "../../utils/constants";
 import * as errors from "../../utils/errors";
-import * as omnic from "../../utils/omnic";
+import * as requests from "../../utils/requests";
 import * as strings from "../../utils/strings";
+import * as xml from "../../utils/xml";
+
+interface Skills {
+    player_skills?: unknown[];
+    season_rewards?: {
+        level?: number;
+        wins?: number;
+    };
+    user_name?: string;
+    user_id?: string;
+    stats?: {
+        [key: string]: unknown;
+    };
+}
 
 export = class RocketLeagueCommand extends Command {
     private platforms: string[];
+    private skillTypes: string[];
 
     constructor() {
         super("rocketLeague", {
@@ -37,6 +53,7 @@ export = class RocketLeagueCommand extends Command {
         });
 
         this.platforms = [ "steam", "ps4", "xboxone" ];
+        this.skillTypes = [ "assists", "goals", "mvps", "saves", "shots", "wins" ];
     }
 
     exec = async (message: Message, argv: CommandArguments): Promise<void> => {
@@ -44,34 +61,59 @@ export = class RocketLeagueCommand extends Command {
         if (!argv._.length) throw new errors.DiscordError(errors.BASTION_ERROR_TYPE.INVALID_COMMAND_SYNTAX, this.name);
 
         // identify player & platform
-        const player = argv._.join(" ");
+        let player = encodeURIComponent(argv._.join(" "));
         const platform = argv.platform && this.platforms.includes(argv.platform.toLowerCase()) ? argv.platform.toLowerCase() : this.platforms[0];
 
+        // get steam id
+        if (platform === "steam") {
+            const response = await requests.get("https://steamcommunity.com/id/" + player + "?xml=1");
+            const account: SteamProfile = xml.parse(await response.text());
+            player = account?.profile?.steamID64 as string;
+        }
+
         // get stats
-        const rawResponse = await omnic.makeRequest("/playerstats/rocketleague/" + platform + "/" + encodeURIComponent(player));
-        const response = await rawResponse.json();
+        const skillsResponse = await requests.get("https://api.rocketleague.com/api/v1/" + platform + "/playerskills/" + player, {
+            Authorization: "Token " + (this.client.credentials as BastionCredentials).rocketLeagueApiKey,
+        });
+        const skills = await skillsResponse.json();
+
+        const data: Skills = {
+            ...skills[0],
+            stats: {},
+        };
+
+        for (const type of this.skillTypes) {
+            const statsResponse = await requests.get("https://api.rocketleague.com/api/v1/" + platform + "/leaderboard/stats/" + type + "/" + player, {
+                Authorization: "Token " + (this.client.credentials as BastionCredentials).rocketLeagueApiKey,
+            });
+            const stats = await statsResponse.json();
+
+            if (stats?.[0]?.user_id) {
+                data.stats[stats[0].stat_type] = stats[0].value;
+            }
+        }
 
         // check for errors
-        if (!response.user_name) throw new Error("PLAYER_NOT_FOUND");
+        if (!data.user_name) throw new Error("PLAYER_NOT_FOUND");
 
         // acknowledge
         await message.channel.send({
             embed: {
                 color: constants.COLORS.ROCKET_LEAGUE,
                 author: {
-                    name: response.user_name + " / " + player,
+                    name: data.user_name + " / " + player,
                 },
                 title: "Rocket League - Player Stats",
                 fields: [
                     {
                         name: "Season Reward",
-                        value: "Level " + response.season_rewards
-                            ? response.season_rewards.level + (response.season_rewards.wins ? response.season_rewards.wins + " Wins" : "")
+                        value: "Level " + data.season_rewards
+                            ? data.season_rewards?.level + (data.season_rewards?.wins ? data.season_rewards?.wins + " Wins" : "")
                             : "-",
                     },
-                    ...Object.keys(response.stats).map((key: string) => ({
+                    ...Object.keys(data.stats).map((key: string) => ({
                         name: strings.toTitleCase(key),
-                        value: response.stats[key],
+                        value: data.stats[key],
                         inline: true,
                     })),
                 ],
