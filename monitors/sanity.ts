6508a163fbf8878847b0033280e95ae112aa8c46
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-01-22 17:50:28 +0530

    monitors(sanity): update sanity check
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- monitors/sanity.ts
+++ monitors/sanity.ts
@@ -6,7 +6,6 @@
 import * as crypto from "crypto";
 import { promises as fs } from "fs";
 import { Monitor } from "@bastion/tesseract";
-import { Team, User } from "discord.js";
 
 export = class SanityMonitor extends Monitor {
     public static e = "1ead86a47ecab";
@@ -22,32 +21,6 @@ export = class SanityMonitor extends Monitor {
     }
 
     exec = async (): Promise<void> => {
-        // don't do anything if it's my Bastion
-        if (this.client.user.id === "267035345537728512") return;
-
-        const app = await this.client.fetchApplication().catch(() => {
-            // this error can be ignored
-        });
-        if (!app) return;
-
-        const owner: User = app.owner instanceof Team ? app.owner.owner.user : app.owner;
-
-        // if it's one of mine, it can do whatever the hell it wants
-        if (owner.id === "266290969974931457") return;
-
-        // if it's a public bot, don't allow it to run
-        if (app.botPublic) {
-            process.stderr.write("\n\n" + Buffer.from("RkFUQUwgRVJST1I6IDB4QkFBREIwMDIKCkdvIHRvIHlvdXIgQm90J3MgYXBwbGljYXRpb24gcGFnZSBpbiBEaXNjb3JkIERldmVsb3BlcnMgc2l0ZSBhbmQgZGlzYWJsZSB0aGUgIlB1YmxpYyBCb3QiIG9wdGlvbi4KClBsZWFzZSBjb250YWN0IHRoZSBzdXBwb3J0IHRlYW0gYXQgQmFzdGlvbiBIUSAtIGh0dHBzOi8vZGlzY29yZC5nZy9meng4Zmt0IC0gZm9yIG1vcmUgaW5mb3JtYXRpb24u", "base64").toString("utf-8") + "\n\n\n");
-
-            if (this.client.shard) {
-                await this.client.shard.broadcastEval("this.destroy();process.exitCode = " + this.errorCode + ";");
-            } else {
-                this.client.destroy();
-                process.exitCode = this.errorCode;
-                process.exit(this.errorCode);
-            }
-        }
-
         // find the hash of the constants file
         if (typeof this.a !== "string") {
             this.a = crypto.createHash("sha256").update(await fs.readFile("./locales/constants.yaml", "utf-8"), "utf8").digest("hex").slice(-13);
