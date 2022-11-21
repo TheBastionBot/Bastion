f3aef5b2d79be00b7867bfbcf7ee17362a1e7024
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-11-22 04:50:11 +0530

    add bot script
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

---
+++ src/bastion.ts
@@ -0,0 +1,61 @@
+/*!
+ * @author TRACTION (iamtraction)
+ * @copyright 2022
+ */
+import { GatewayIntentBits, Options, Partials } from "discord.js";
+import { Client, Logger } from "@bastion/tesseract";
+import * as dotenv from "dotenv";
+
+// configure dotenv
+dotenv.config();
+
+const bastion = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildBans,
+        // GatewayIntentBits.GuildEmojisAndStickers,
+        // GatewayIntentBits.GuildIntegrations,
+        // GatewayIntentBits.GuildWebhooks,
+        // GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        // GatewayIntentBits.GuildMessageReactions,
+        // GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        // GatewayIntentBits.DirectMessageReactions,
+        // GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.MessageContent,
+        // GatewayIntentBits.GuildScheduledEvents,
+        // GatewayIntentBits.AutoModerationConfiguration,
+        // GatewayIntentBits.AutoModerationExecution,
+    ],
+    partials: [
+        Partials.Message,
+        Partials.Channel,
+        Partials.Reaction,
+    ],
+    makeCache: Options.cacheWithLimits({
+        ...Options.DefaultMakeCacheSettings,
+        MessageManager: {
+            maxSize: 5,
+        },
+    }),
+    sweepers: {
+        ...Options.DefaultSweeperSettings,
+        messages: {
+            interval: 36e2,
+            lifetime: 9e2,
+        },
+    },
+});
+
+bastion.init();
+
+process.on("SIGINT", () => {
+    bastion.destroy();
+    bastion.disconnectMongo()
+    .then(() => process.exit())
+    .catch(Logger.error);
+});
