4421fbefb35227ec0aee06440b5873c04151c485
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-11-24 18:16:59 +0530

    components: add VerificationButton handler
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

---
+++ src/components/VerificationButton.ts
@@ -0,0 +1,41 @@
+/*!
+ * @author TRACTION (iamtraction)
+ * @copyright 2022
+ */
+import { ButtonInteraction, ComponentType, TextInputStyle } from "discord.js";
+import { MessageComponent } from "@bastion/tesseract";
+
+import MessageComponents from "../utils/components";
+
+class VerificationButton extends MessageComponent {
+    constructor() {
+        super({
+            id: MessageComponents.VerificationButton,
+            scope: "guild",
+        });
+    }
+
+    public async exec(interaction: ButtonInteraction<"cached">): Promise<unknown> {
+        return interaction.showModal({
+            custom_id: MessageComponents.VerificationModal,
+            title: "Are you a human?",
+            components: [
+                {
+                    type: ComponentType.ActionRow,
+                    components: [
+                        {
+                            custom_id: MessageComponents.VerificationTextInput,
+                            type: ComponentType.TextInput,
+                            label: "Type \"i am human\" to verify yourself.",
+                            placeholder: "i am human",
+                            required: true,
+                            style: TextInputStyle.Short,
+                        },
+                    ],
+                },
+            ],
+        });
+    }
+}
+
+export = VerificationButton;
