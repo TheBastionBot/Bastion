e7594901671ce74ce3947ee8a11baa10c1c76bc7
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-11-24 18:17:56 +0530

    components: add VerificationModal handler
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

---
+++ src/components/VerificationModal.ts
@@ -0,0 +1,34 @@
+/*!
+ * @author TRACTION (iamtraction)
+ * @copyright 2022
+ */
+import { ModalSubmitInteraction } from "discord.js";
+import { Logger, MessageComponent } from "@bastion/tesseract";
+
+import GuildModel from "../models/Guild";
+import MessageComponents from "../utils/components";
+
+class VerificationModal extends MessageComponent {
+    constructor() {
+        super({
+            id: MessageComponents.VerificationModal,
+            scope: "guild",
+        });
+    }
+
+    public async exec(interaction: ModalSubmitInteraction<"cached">): Promise<void> {
+        const inputValue = interaction.fields.getTextInputValue(MessageComponents.VerificationTextInput)?.toLowerCase();
+
+        if ([ "i'm human", "i am human" ].includes(inputValue)) {
+            await interaction.deferUpdate();
+
+            const guildDocument = await GuildModel.findById(interaction.guildId);
+
+            if (guildDocument.verifiedRole) {
+                await interaction.member.roles.add(guildDocument.verifiedRole, "Verified").catch(Logger.ignore);
+            }
+        }
+    }
+}
+
+export = VerificationModal;
