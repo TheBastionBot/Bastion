7306e6248a7381f5dbea9cb3b06edf568deaca15
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-11-28 11:49:03 +0530

    chore: migrate from yaml lib to yaml util
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -2,10 +2,7 @@
  * @author TRACTION (iamtraction)
  * @copyright 2022
  */
-import fs from "node:fs";
-import path from "node:path";
-import * as yaml from "yaml";
-
+import * as yaml from "./yaml";
 import { bastion } from "../types";
 
-export const get = (): bastion.Settings => yaml.parse(fs.readFileSync(path.resolve("settings.yaml"), "utf8"));
+export const get = (): bastion.Settings => yaml.parse("settings.yaml");
