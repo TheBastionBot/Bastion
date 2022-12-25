13727b731cdae016e00f650dcca5764679579928
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-12-25 18:35:40 +0530

    commands(config): add select-roles list command
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

---
+++ src/commands/config/select-roles/list.ts
@@ -0,0 +1,58 @@
+/*!
+ * @author TRACTION (iamtraction)
+ * @copyright 2022
+ */
+import { ApplicationCommandOptionType, ChannelType, ChatInputCommandInteraction, PermissionFlagsBits } from "discord.js";
+import { Command } from "@bastion/tesseract";
+
+import SelectRoleGroupModel from "../../../models/SelectRoleGroup";
+import { COLORS } from "../../../utils/constants";
+
+class SelectRolesListCommand extends Command {
+    constructor() {
+        super({
+            name: "list",
+            description: "List all the Select Role Groups.",
+            options: [
+                {
+                    type: ApplicationCommandOptionType.Channel,
+                    name: "channel",
+                    description: "List Select Role Groups only from this channel.",
+                    channel_types: [ ChannelType.GuildAnnouncement, ChannelType.GuildText, ChannelType.GuildVoice ],
+                },
+            ],
+            userPermissions: [ PermissionFlagsBits.ManageGuild ],
+        });
+    }
+
+    public async exec(interaction: ChatInputCommandInteraction<"cached">): Promise<unknown> {
+        await interaction.deferReply();
+        const channel = interaction.options.getChannel("channel");
+
+        // get all the select role group documents
+        const queryFilter = channel?.id ? { guild: interaction.guildId, channel: channel.id } : { guild: interaction.guildId };
+        const selectRoleGroupDocuments = await SelectRoleGroupModel.find(queryFilter);
+
+        if (selectRoleGroupDocuments?.length) {
+            return await interaction.editReply({
+                embeds: [
+                    {
+                        color: COLORS.PRIMARY,
+                        title: "Select Role Groups",
+                        fields: selectRoleGroupDocuments.slice(0, 25).map((doc, i) => ({
+                            name: `#${ i + 1 } — ${ doc.id }`,
+                            value: `${ doc.roles?.length } roles / [Source](https://discord.com/channels/${ doc.guild }/${ doc.channel }/${ doc.id }) 🔗`,
+                        })),
+                        footer: {
+                            text: `${ selectRoleGroupDocuments.length } Select Role Groups.`
+                        },
+                    },
+                ],
+            });
+        }
+
+        await interaction.editReply(`There are no Select Role Groups in ${ channel || "this server" }.`);
+    }
+}
+
+export = SelectRolesListCommand;
