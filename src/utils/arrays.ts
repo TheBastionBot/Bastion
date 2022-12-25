c9ad9399f78252e617860a04d9d3ebdc0fee3281
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-12-25 21:28:22 +0530

    utils(arrays): add shuffle func
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -2,6 +2,7 @@
  * @author TRACTION (iamtraction)
  * @copyright 2022
  */
+
 /**
  * Returns an array of elements split into groups of the specified size.
  * If array can't be split evenly, the final chunk will be the remaining elements.
@@ -14,4 +15,16 @@ export const chunks = <T>(array: T[], size: number): T[][] => {
         chunks.push(array.slice(i, i + size));
     }
     return chunks;
+};
+
+/**
+ * Shuffles the array of elements in-place using the Fisher–Yates shuffle.
+ * @param array The array you want to shuffle.
+ */
+export const shuffle = <T>(array: T[]) => {
+    let currentIndex = array.length;
+    while (currentIndex) {
+        let randomIndex = Math.floor(Math.random() * currentIndex--);
+        [ array[currentIndex], array[randomIndex] ] = [ array[randomIndex], array[currentIndex] ];
+    }
 };
