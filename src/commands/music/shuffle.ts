6e1d46273ac9e661c6042c31864c2ccdfcd6e588
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-12-25 21:29:42 +0530

    commands(music): add shuffle command
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

---
+++ src/commands/music/shuffle.ts
@@ -0,0 +1,32 @@
+/*!
+ * @author TRACTION (iamtraction)
+ * @copyright 2022
+ */
+import { ChatInputCommandInteraction } from "discord.js";
+import { Client, Command } from "@bastion/tesseract";
+
+import * as arrays from "../../utils/arrays";
+
+class ShuffleCommand extends Command {
+    constructor() {
+        super({
+            name: "shuffle",
+            description: "Shuffle the current music queue.",
+        });
+    }
+
+    public async exec(interaction: ChatInputCommandInteraction<"cached">): Promise<unknown> {
+        const studio = (interaction.client as Client).studio.get(interaction.guild);
+
+        // shuffle the music queue
+        arrays.shuffle(studio.queue);
+
+        if (studio?.queue?.length) {
+            return await interaction.reply({ content: (interaction.client as Client).locales.getText(interaction.guildLocale, "musicQueueShuffle") });
+        }
+
+        await interaction.reply({ content: (interaction.client as Client).locales.getText(interaction.guildLocale, "musicQueueEmpty"), ephemeral: true });
+    }
+}
+
+export = ShuffleCommand;
