29662817cd56f3ecf57d88ab2b5bb16aac101ac5
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-12-25 18:37:55 +0530

    components: add SelectRolesNoRolesButton handler
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

---
+++ src/components/SelectRolesNoRolesButton.ts
@@ -0,0 +1,41 @@
+/*!
+ * @author TRACTION (iamtraction)
+ * @copyright 2022
+ */
+import { ButtonInteraction, ComponentType, PermissionFlagsBits } from "discord.js";
+import { MessageComponent } from "@bastion/tesseract";
+
+import MessageComponents from "../utils/components";
+
+class SelectRolesNoRolesButton extends MessageComponent {
+    constructor() {
+        super({
+            id: MessageComponents.SelectRolesNoRolesButton,
+            scope: "guild",
+            userPermissions: [ PermissionFlagsBits.ManageGuild ],
+        });
+    }
+
+    public async exec(interaction: ButtonInteraction<"cached">): Promise<void> {
+        await interaction.reply({
+            content: "Select the roles you want to have in this Select Role Group.",
+            components: [
+                {
+                    type: ComponentType.ActionRow,
+                    components: [
+                        {
+                            type: ComponentType.RoleSelect,
+                            customId: MessageComponents.SelectRolesUpdateSelect,
+                            placeholder: "Select Roles",
+                            minValues: 1,
+                            maxValues: 25,
+                        },
+                    ],
+                },
+            ],
+            ephemeral: true,
+        });
+    }
+}
+
+export = SelectRolesNoRolesButton;
