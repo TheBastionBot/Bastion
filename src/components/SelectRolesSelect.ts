251235f9aeb04499fa589c379dfb44b25eac8837
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-12-31 12:15:39 +0530

    chore: refactor
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/SelectRolesSelect.ts
+++ src/components/SelectRolesSelect.ts
@@ -21,27 +21,27 @@ class SelectRolesSelectMenu extends MessageComponent {
         await interaction.deferUpdate();
 
         // get the select role group document
-        const selectRolesGroup = await SelectRoleGroupModel.findById(interaction.message?.id);
+        const selectRoleGroup = await SelectRoleGroupModel.findById(interaction.message?.id);
 
         // get the selected roles
-        const roles = interaction.values.filter(id => selectRolesGroup?.roles?.includes(id));
+        const roles = interaction.values.filter(id => selectRoleGroup?.roles?.includes(id));
 
         // check whether the roles exists in the select role group
         if (roles?.length) {
             // check whether the roles can only be removed
-            if (selectRolesGroup.type === SelectRolesType.RemoveOnly) {
+            if (selectRoleGroup.type === SelectRolesType.RemoveOnly) {
                 // remove the selected roles from the member
                 return await interaction.member.roles.remove(roles).catch(Logger.ignore);
             }
 
             // check whether the roles can only be added
-            if (selectRolesGroup.type === SelectRolesType.AddOnly) {
+            if (selectRoleGroup.type === SelectRolesType.AddOnly) {
                 // add the selected roles to the member
                 return await interaction.member.roles.add(roles).catch(Logger.ignore);
             }
 
             const memberRoles = interaction.member.roles.cache
-            .filter(r => !selectRolesGroup.roles.some(id => id === r.id))   // remove all the roles in the select role group
+            .filter(r => !selectRoleGroup.roles.some(id => id === r.id))   // remove all the roles in the select role group
             .map(r => r.id)
             .concat(roles); // add the selected roles
 
