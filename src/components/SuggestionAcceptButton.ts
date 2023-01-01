ba49b48272386c55f656a7c80c9f7c7aed0f7dd9
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-01-01 05:43:10 +0530

    comp(SuggestionAcceptButton): add change status button
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/SuggestionAcceptButton.ts
+++ src/components/SuggestionAcceptButton.ts
@@ -2,7 +2,7 @@
  * @author TRACTION (iamtraction)
  * @copyright 2022
  */
-import { ButtonInteraction, PermissionFlagsBits } from "discord.js";
+import { ButtonInteraction, ButtonStyle, ComponentType, PermissionFlagsBits } from "discord.js";
 import { MessageComponent } from "@bastion/tesseract";
 
 import MessageComponents from "../utils/components";
@@ -26,7 +26,6 @@ class SuggestionAcceptButton extends MessageComponent {
                     ...interaction.message.embeds[0].toJSON(),
                     color: COLORS.GREEN,
                     fields: [
-                        ...interaction.message.embeds[0].fields,
                         {
                             name: "Status",
                             value: "Accepted",
@@ -40,7 +39,19 @@ class SuggestionAcceptButton extends MessageComponent {
                     ],
                 },
             ],
-            components: [],
+            components: [
+                {
+                    type: ComponentType.ActionRow,
+                    components: [
+                        {
+                            type: ComponentType.Button,
+                            label: "Change Status",
+                            style: ButtonStyle.Secondary,
+                            customId: MessageComponents.SuggestionRejectButton,
+                        },
+                    ],
+                },
+            ],
         });
     }
 }
