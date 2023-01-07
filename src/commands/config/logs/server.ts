b3aa27b86907ece2fb471ce96cec071c1658bdaa
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-01-07 18:46:48 +0530

    commands(logs server): disable content log if server log is disabled
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/commands/config/logs/server.ts
+++ src/commands/config/logs/server.ts
@@ -32,6 +32,8 @@ class LogServerCommand extends Command {
 
         // update server log channel
         guildDocument.serverLogChannel = channel?.id || undefined;
+        // disable message content logs if server logs is disabled
+        guildDocument.serverLogContent = channel?.id ? guildDocument.serverLogContent : undefined;
 
         await guildDocument.save();
         return await interaction.editReply(`I've ${ channel?.id ? "enabled" : "disabled" } server logs${ channel?.id ? ` in the **${ channel }** channel` : "" }.`);
