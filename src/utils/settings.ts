c06e840f7f2dbb7cd855e513db996887939b6164
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-03-03 06:59:05 +0530

    utils(settings): define BastionSettings class
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,8 +1,48 @@
 /*!
  * @author TRACTION (iamtraction)
- * @copyright 2022
+ * @copyright 2023
  */
-import * as yaml from "./yaml.js";
+import { Settings } from "@bastion/tesseract";
+
 import { bastion } from "../types.js";
 
-export const get = (): bastion.Settings => yaml.parse("settings.yaml");
+export default class BastionSettings extends Settings {
+    declare protected data: bastion.Settings;
+
+    constructor() {
+        super();
+    }
+
+    /** Port */
+    public get port(): string | 8377 {
+        return process.env.BASTION_API_PORT || process.env.PORT || 8377;
+    }
+
+    /** Auth */
+    public get auth(): string {
+        return process.env.BASTION_API_AUTH || this.data.auth;
+    }
+
+    /** Music Activity */
+    public get musicActivity(): boolean {
+        if (process.env.BASTION_MUSIC_ACTIVITY) {
+            if (process.env.BASTION_MUSIC_ACTIVITY.toLowerCase() === "true") return true;
+            if (process.env.BASTION_MUSIC_ACTIVITY.toLowerCase() === "false") return false;
+        }
+        return this.data.musicActivity;
+    }
+
+    /** Relay Direct Messages */
+    public get relayDirectMessages(): boolean | string {
+        if (process.env.BASTION_RELAY_DMS) {
+            if (process.env.BASTION_RELAY_DMS.toLowerCase() === "true") return true;
+            if (process.env.BASTION_RELAY_DMS.toLowerCase() === "false") return false;
+            return process.env.BASTION_RELAY_DMS;
+        }
+        return this.data.relayDirectMessages;
+    }
+
+    public get<K extends keyof bastion.Settings>(key: K): bastion.Settings[K] {
+        return this.data[key];
+    }
+}
