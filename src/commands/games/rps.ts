aedef92ff740d79db4d7adbec7e565b55338b13c
TRACTION <19631364+iamtraction@users.noreply.github.com>
2026-09-07 11:47:47 +0530

    refactor(rps): make the april fools' joke a chance rather than the whole day
    Co-authored-by: Sage <264273354+sage-cli@users.noreply.github.com>

--- src/commands/games/rps.ts
+++ src/commands/games/rps.ts
@@ -1,6 +1,6 @@
 /*!
  * @author TRACTION (iamtraction)
- * @copyright 2022
+ * @copyright 2026
  */
 import { ApplicationCommandOptionType, ChatInputCommandInteraction } from "discord.js";
 import { Command } from "@bastion/tesseract";
@@ -39,23 +39,28 @@ class RockPaperScissorCommand extends Command {
     public async exec(interaction: ChatInputCommandInteraction<"cached">): Promise<void> {
         const choice = interaction.options.getString("choice");
 
-        const isAprilFoolsDay = new Date().getMonth() === 3 && new Date().getDate() === 1;
+        const today = new Date();
+        const isAprilFoolsDay = today.getMonth() === 3 && today.getDate() === 1;
+        const isJoking = isAprilFoolsDay && Math.random() < 0.1;
 
-        // find bastion's choice
-        const bastionChoice: string = isAprilFoolsDay ? Choices.Rock : this.choices[Math.floor(Math.random() * this.choices.length)];
+        const bastionChoice: string = isJoking ? Choices.Rock : this.choices[Math.floor(Math.random() * this.choices.length)];
 
-        // find the result
-        const result = choice === bastionChoice
-            ? "Damn! It's a draw, mate."
-            : choice === Choices.Rock && bastionChoice === Choices.Scissor
-                ? "You win, human."
-                : choice === Choices.Paper && bastionChoice === Choices.Rock
-                    ? isAprilFoolsDay
-                        ? "I understand that scissors can beat paper. And I get how rock can beat scissors. But there's no way paper can beat rock. Paper is supposed to magically wrap around rock leaving it immobile? Why can't paper do this to scissors? Forget scissors, why can't paper do this to people? Why aren't sheets of notebook constantly suffocating students as they attempt to take notes in class? I'll tell you why. Because paper can't beat anybody. A rock would tear it up in seconds. When I play rock paper scissor, I always choose rock. And if you claim to have beaten me with your paper I will punch you in the face with my already chenched fist and say, \"oh sorry, i thought paper would protect you.\""
-                        : "You win, human."
-                    : choice === Choices.Scissor && bastionChoice === Choices.Paper
-                        ? "You win, human."
-                        : "I win! Sorry, human. :yum:";
+        const isDraw = choice === bastionChoice;
+        const hasWon = (choice === Choices.Rock && bastionChoice === Choices.Scissor)
+            || (choice === Choices.Paper && bastionChoice === Choices.Rock)
+            || (choice === Choices.Scissor && bastionChoice === Choices.Paper);
+
+        let result: string;
+
+        if (isDraw) {
+            result = "Damn! It's a draw, mate.";
+        } else if (!hasWon) {
+            result = "I win! Sorry, human. :yum:";
+        } else if (isJoking && choice === Choices.Paper) {
+            result = "I understand that scissors can beat paper. And I get how rock can beat scissors. But there's no way paper can beat rock. Paper is supposed to magically wrap around rock leaving it immobile? Why can't paper do this to scissors? Forget scissors, why can't paper do this to people? Why aren't sheets of notebook constantly suffocating students as they attempt to take notes in class? I'll tell you why. Because paper can't beat anybody. A rock would tear it up in seconds. When I play rock paper scissor, I always choose rock. And if you claim to have beaten me with your paper I will punch you in the face with my already chenched fist and say, \"oh sorry, i thought paper would protect you.\"";
+        } else {
+            result = "You win, human.";
+        }
 
         await interaction.reply(`I chose **${ bastionChoice }**, you chose **${ choice }**. ${ result }`);
     }
