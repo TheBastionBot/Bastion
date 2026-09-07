fd490c68a4f374d0a2f0b6fdbe7ffed23b6cd5d5
TRACTION <19631364+iamtraction@users.noreply.github.com>
2026-09-08 02:43:50 +0530

    fix(roles): delete the role's configuration when it's deleted
    Co-authored-by: Sage <264273354+sage-cli@users.noreply.github.com>

--- src/listeners/roleDelete.ts
+++ src/listeners/roleDelete.ts
@@ -5,6 +5,7 @@
 import { Role, time } from "discord.js";
 import { Listener } from "@bastion/tesseract";
 
+import RoleModel from "../models/Role.js";
 import { logGuildEvent } from "../utils/guilds.js";
 
 class RoleDeleteListener extends Listener<"roleDelete"> {
@@ -13,6 +14,8 @@ class RoleDeleteListener extends Listener<"roleDelete"> {
     }
 
     public async exec(role: Role): Promise<void> {
+        await RoleModel.deleteOne({ _id: role.id });
+
         await logGuildEvent(role.guild, {
             color: role.color,
             title: "Role Deleted",
